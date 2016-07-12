6543140fdbda71430f5ebe3cd6c543abd664441a
John Knoll <jpknoll@ucdavis.edu>
2016-07-11 21:51:00 -0700

    new event card and item

---
+++ app/views/eventCard/EventCard.js
@@ -0,0 +1,102 @@
+'use strict'
+
+import React from 'react'
+import {
+	View,
+  ListView,
+	Text,
+	TouchableHighlight,
+} from 'react-native';
+import EventService from '../../services/eventService'
+import Card from '../cards/card'
+
+var css = require('../../styles/css');
+
+export default class EventCard extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.fetchEventsErrorInterval: 15 * 1000,			// Retry every 15 seconds
+  	this.fetchEventsErrorLimit: 3,
+  	this.fetchEventsErrorCounter: 0,
+
+    this.state = {
+      eventsDataFull: [],
+      eventsDataPartial: [],
+      eventsRenderAllRows: false,
+      fetchEventsErrorLimitReached: false
+    }
+  }
+
+  refresh() {
+    EventService.FetchEvents
+			.then((responseData) => {
+
+				var responseDataFull = responseData;
+				var responseDataPartial = responseData.slice(0, this.eventsDefaultResults);
+
+				var dsFull = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+				var dsPartial = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+				this.setState({
+					eventsDataFull: dsFull.cloneWithRows(responseDataFull),
+					eventsDataPartial: dsPartial.cloneWithRows(responseDataPartial),
+					eventsDataLoaded: true
+				});
+			})
+			.catch((error) => {
+				if (this.fetchEventsErrorLimit > this.fetchEventsErrorCounter) {
+					this.fetchEventsErrorCounter++;
+					logger.custom('ERR: fetchEvents1: refreshing again in ' + this.fetchEventsErrorInterval/1000 + ' sec');
+					this.refreshEventsTimer = this.setTimeout( () => { this.refresh() }, this.fetchEventsErrorInterval);
+				} else {
+					logger.custom('ERR: fetchEvents2: Limit exceeded - max limit:' + this.fetchEventsErrorLimit);
+					this.setState({ fetchEventsErrorLimitReached: true });
+				}
+			})
+			.done();
+	}
+
+  render() {
+    var eventsList = [];
+    if (this.state.eventsRenderAllRows){
+      eventsList = this.state.eventsDataFull;
+    } else {
+      eventsList = this.state.eventsDataPartial;
+    }
+
+    return (
+      <Card title='Campus Events'>
+        <View style={css.events_list}>
+          <ListView
+            dataSource={eventsList}
+            renderRow={ (row) => return <EventItem data={row} /> }
+            style={css.wf_listview} />
+
+          {this.state.eventsRenderAllRows === false ? (
+            <TouchableHighlight underlayColor={'rgba(200,200,200,.1)'} onPress={ () => this._setState('eventsRenderAllRows', true) }>
+              <View style={css.events_more}>
+                <Text style={css.events_more_label}>Show More Events &#9660;</Text>
+              </View>
+            </TouchableHighlight>
+          ) : null }
+
+          {this.state.eventsRenderAllRows === true ? (
+            <TouchableHighlight underlayColor={'rgba(200,200,200,.1)'} onPress={ () => this._setState('eventsRenderAllRows', false) }>
+              <View style={css.events_more}>
+                <Text style={css.events_more_label}>Show Less Events &#9650;</Text>
+              </View>
+            </TouchableHighlight>
+          ) : null }
+
+          {this.state.fetchEventsErrorLimitReached ? (
+            <View style={[css.flexcenter, css.pad40]}>
+              <Text>There was a problem loading Student Events</Text>
+            </View>
+          ) : null }
+        </Card>
+      )
+    }
+  }
+}
---
+++ app/views/eventCard/EventItem.js
@@ -0,0 +1,58 @@
+'use strict'
+
+import React from 'react'
+import {
+	StyleSheet,
+	View,
+	Text,
+	Navigator,
+	TouchableHighlight,
+	Image,
+} from 'react-native';
+
+var css = require('../../styles/css');
+
+export default class EventItem extends React.Component {
+
+  gotoEventDetail: function(eventData) {
+		this.props.navigator.push({ component: EventDetail, title: 'Events', eventData: eventData });
+	},
+
+  render: function() {
+    var data = this.props.data;
+    var eventTitleStr = data.EventTitle.replace('&amp;','&');
+		var eventDescriptionStr = data.EventDescription.replace('&amp;','&').replace(/\n.*/g,'').trim();
+
+		if (eventDescriptionStr.length > 0) {
+			if (eventTitleStr.length < 25) {
+				eventDescriptionStr = eventDescriptionStr.substring(0,56) + '...';
+			} else if (eventTitleStr.length < 50) {
+				eventDescriptionStr = eventDescriptionStr.substring(0,28) + '...';
+			} else {
+				eventDescriptionStr = '';
+			}
+		}
+
+		var eventDateDay;
+		if (data.EventDate) {
+			var eventDateDayArray = data.EventDate[0].split(', ');
+			eventDateDay = eventDateDayArray[1] + ', ' + eventDateDayArray[2].substring(5,22).toLowerCase();
+		} else {
+			eventDateDay = 'Ongoing Event';
+		}
+
+    return (
+      <TouchableHighlight underlayColor={'rgba(200,200,200,.1)'} onPress={ () => this.gotoEventDetail(data) }>
+        <View style={css.events_list_row}>
+          <View style={css.events_list_left_container}>
+            <Text style={css.events_list_title}>{eventTitleStr}</Text>
+            {eventDescriptionStr ? (<Text style={css.events_list_desc}>{this.props.description}</Text>) : null }
+            <Text style={css.events_list_postdate}>{eventDateDay}</Text>
+          </View>
+
+          <Image style={css.events_list_image} source={{ uri: data.EventImage }} />
+        </View>
+      </TouchableHighlight>
+    )
+  }
+}
