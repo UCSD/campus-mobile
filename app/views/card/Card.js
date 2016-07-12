34774a91c431b72fe9594bd1a0e0cd67a4b0df84
John Knoll <jpknoll@ucdavis.edu>
2016-07-11 21:50:40 -0700

    stub

--- app/views/card/Card.js
+++ app/views/card/Card.js
@@ -10,7 +10,11 @@ var css = require('../styles/css');
 
 export default class Card extends React.Component {
 
-  render: function() {
+  refresh(refreshType) {
+    return;
+  }
+
+  render() {
     <View style={css.card_main}>
       <CardHeader title={this.props.title} />
       {children}
