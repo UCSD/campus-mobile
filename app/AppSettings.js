84236f0a55b9934fef95d5fa671a59a94b7fddaa
Charles <c3bryant@ucsd.edu>
2016-08-11 11:47:36 -0700

    Reactivated NavigatorIOS for iphone

--- app/AppSettings.js
+++ app/AppSettings.js
@@ -6,7 +6,7 @@ module.exports = {
 	APP_NAME: 					'now@ucsandiego',
 	GOOGLE_ANALYTICS_ID: 		'UA-81825078-1',
 	DEBUG_ENABLED: 				true,				// Disables all logger functions (i.e. logger.log) if set to false (console.log is unaffected)
-	NAVIGATOR_ENABLED: 			true,				// IOS setting only - if true, app uses Navigator instead of Navigator IOS
+	NAVIGATOR_ENABLED: 			false,				// IOS setting only - if true, app uses Navigator instead of Navigator IOS
 													// Navigator will always be used for Android
 	
 	/* CARDS */
