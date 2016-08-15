1d32d360de6e0e98e306729d0dea1d8a0a21e7e8
Ivan Wu <i8wu@act179-154.ucsd.edu>
2016-08-15 13:33:24 -0700

    Removed unused import

--- android/app/src/main/java/com/nowucsandiego/MainApplication.java
+++ android/app/src/main/java/com/nowucsandiego/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.mehcode.reactnative.splashscreen.SplashScreenPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new SplashScreenPackage(),
           new GoogleAnalyticsBridgePackage(),
           new ReactNativePermissionsPackage(),
           new MapsPackage()
