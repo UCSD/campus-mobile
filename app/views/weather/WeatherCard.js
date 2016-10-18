ac5876a4156e9bcac875bbbab35e15e4f89d7d74
Charles <c3bryant@ucsd.edu>
2016-10-18 09:56:11 -0700

    Update formatting on WeatherCard

--- app/views/weather/WeatherCard.js
+++ app/views/weather/WeatherCard.js
@@ -24,117 +24,115 @@ var general = require('../../util/general');
 var AppSettings = require('../../AppSettings');
 
 export default class WeatherCard extends CardComponent {
-  constructor(props) {
-	super(props);
-	this.state = {
-	  weatherData: null,
-	  weatherDataLoaded: false,
+	constructor(props) {
+		super(props);
+		this.state = {
+			weatherData: null,
+			weatherDataLoaded: false,
+		}
 	}
-  }
 
 	componentDidMount() {
 		this.refresh();
 	}
 
-  fetchWeatherData() {
+	fetchWeatherData() {
+		WeatherService.FetchWeather()
+		.then((responseData) => {
 
-	WeatherService.FetchWeather()
-	  .then((responseData) => {
+			responseData.currently.temperature = Math.round(responseData.currently.temperature);
+			responseData.daily.data = responseData.daily.data.slice(0,5);
 
-		responseData.currently.temperature = Math.round(responseData.currently.temperature);
-		responseData.daily.data = responseData.daily.data.slice(0,5);
+			for (var i = 0; responseData.daily.data.length > i; i++) {
+				var data = responseData.daily.data[i];
+				var wf_date = new Date(data.time * 1000);
+				var wf_days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+				var wf_day = wf_date.getDay();
 
-		for (var i = 0; responseData.daily.data.length > i; i++) {
-		  var data = responseData.daily.data[i];
-		  var wf_date = new Date(data.time * 1000);
-		  var wf_days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-		  var wf_day = wf_date.getDay();
+				data.dayofweek = wf_days[wf_day];
+				data.tempMax = Math.round(data.temperatureMax);
+				data.tempMin = Math.round(data.temperatureMin);
+			}
 
-		  data.dayofweek = wf_days[wf_day];
-		  data.tempMax = Math.round(data.temperatureMax);
-		  data.tempMin = Math.round(data.temperatureMin);
+			this.setState({
+				weatherData: responseData,
+				weatherDataLoaded: true,
+			});
+		})
+		.catch((error) => {
+			logger.log('ERR: fetchWeatherData: ' + error);
+		})
+		.done();
+	}
+
+	// TODO: should probably be handled by surf screen
+	fetchSurfData() {
+		WeatherService.FetchSurf()
+		.then((responseData) => {
+			this.setState({
+				surfData: responseData,
+				surfDataLoaded: true,
+			});
+		})
+		.catch((error) => {
+			logger.log('ERR: fetchSurfData: ' + error);
+		})
+		.done();
+	}
+
+	getCurrentSurfData() {
+		if (this.state.surfDataLoaded) {
+			var surfData = this.state.surfData[0].title.replace(/.* \: /g, '').replace(/ft.*/g, '').replace(/^\./g, '').replace(/^ /g, '').replace(/ $/g, '').replace(/Surf\: /g, '').trim() + "'";
+			if (surfData.length <= 6) {
+				return (surfData);
+			} else if (surfData.indexOf('none') >= 0) {
+				return '1-2\'';
+			} else {
+				return ;
+			}
+		} else {
+			return;
 		}
-
-		this.setState({
-		  weatherData: responseData,
-		  weatherDataLoaded: true,
-		});
-	  })
-	  .catch((error) => {
-		logger.log('ERR: fetchWeatherData: ' + error);
-	  })
-	  .done();
-  }
-
-  // TODO: should probably be handled by surf screen
-  fetchSurfData() {
-
-	WeatherService.FetchSurf()
-	  .then((responseData) => {
-		this.setState({
-		  surfData: responseData,
-		  surfDataLoaded: true,
-		});
-	  })
-	  .catch((error) => {
-		logger.log('ERR: fetchSurfData: ' + error);
-	  })
-	  .done();
-  }
-
-  getCurrentSurfData() {
-	if (this.state.surfDataLoaded) {
-	  var surfData = this.state.surfData[0].title.replace(/.* \: /g, '').replace(/ft.*/g, '').replace(/^\./g, '').replace(/^ /g, '').replace(/ $/g, '').replace(/Surf\: /g, '').trim() + "'";
-	  if (surfData.length <= 6) {
-		return (surfData);
-	  } else if (surfData.indexOf('none') >= 0) {
-		return '1-2\'';
-	  } else {
-		return ;
-	  }
-	} else {
-	  return;
-	}
-  }
-
-  refresh() {
-	this.fetchWeatherData();
-	this.fetchSurfData();
 	}
 
-  gotoSurfReport() {
-	this.props.navigator.push({ id: 'SurfReport', component: SurfReport, title: 'Surf Report', surfData: this.state.surfData });
-  }
+	refresh() {
+		this.fetchWeatherData();
+		this.fetchSurfData();
+	}
+
+	gotoSurfReport() {
+		this.props.navigator.push({ id: 'SurfReport', component: SurfReport, title: 'Surf Report', surfData: this.state.surfData });
+	}
 
 	render() {
 		return (
-		<Card title='Weather'>
-			{this.state.weatherDataLoaded ? (
-				<View style={css.wc_main}>
-					<View style={css.wc_toprow}>
-						<View style={css.wc_toprow_left}>
-							<Text style={css.wc_current_temp}>{ this.state.weatherData.currently.temperature }&deg; in San Diego</Text>
-							<Text style={css.wc_current_summary}>{ this.state.weatherData.currently.summary }</Text>
+			<Card title='Weather'>
+				{this.state.weatherDataLoaded ? (
+					<View style={css.wc_main}>
+						<View style={css.wc_toprow}>
+							<View style={css.wc_toprow_left}>
+								<Text style={css.wc_current_temp}>{ this.state.weatherData.currently.temperature }&deg; in San Diego</Text>
+								<Text style={css.wc_current_summary}>{ this.state.weatherData.currently.summary }</Text>
+							</View>
+							<View style={css.wc_toprow_right}>
+								<Image style={css.wc_toprow_icon} source={{ uri: AppSettings.WEATHER_ICON_BASE_URL + this.state.weatherData.currently.icon + '.png' }} />
+							</View>
 						</View>
-						<View style={css.wc_toprow_right}>
-							<Image style={css.wc_toprow_icon} source={{ uri: AppSettings.WEATHER_ICON_BASE_URL + this.state.weatherData.currently.icon + '.png' }} />
-						</View>
+
+						<WeatherWeek weatherData={this.state.weatherData} />
+
+						<TouchableHighlight underlayColor={'rgba(200,200,200,.1)'} onPress={ () => this.gotoSurfReport() }>
+							<View style={css.weathercard_border}>
+								<Text style={css.wc_surfreport_more}>Surf Report &raquo;</Text>
+							</View>
+						</TouchableHighlight>
 					</View>
-
-					<WeatherWeek weatherData={this.state.weatherData} />
-
-					<TouchableHighlight underlayColor={'rgba(200,200,200,.1)'} onPress={ () => this.gotoSurfReport() }>
-						<View style={css.weathercard_border}>
-							<Text style={css.wc_surfreport_more}>Surf Report &raquo;</Text>
-						</View>
-					</TouchableHighlight>
-				</View>
-			) : (
-				<View style={[css.cardcenter, css.weatherccard_loading_height]}>
-					<ActivityIndicator style={css.shuttle_card_aa} size="large" />
-				</View>
-			)}
-		</Card>
+				) : (
+					<View style={[css.cardcenter, css.weatherccard_loading_height]}>
+						<ActivityIndicator style={css.shuttle_card_aa} size="large" />
+					</View>
+				)}
+			</Card>
 		);
 	}
 }
