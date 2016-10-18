aa4121b09806ed99d1bbaba245d5d1bfb11b439c
John Knoll <jpknoll@gmail.com>
2016-10-18 16:58:59 -0700

    add/remove

--- app/reducers/cards.js
+++ app/reducers/cards.js
@@ -9,10 +9,27 @@ function activeCards(state = initialState, action) {
       if (newState[action.id])
         newState[action.id] = true;
       return newState;
+
     case: 'HIDE_CARD':
       var newState = Object.assign({}, state);
       if (newState[action.id])
         newState[action.id] = false;
+      return newState;
+
+    case: 'ADD_CARD':
+      var newState = Object.assign({}, state);
+      // check for duplicate
+      if (newState[action.id])
+        return newState;
+
+      newState[action.id] = true;
+      return newState;
+
+    case: 'DELETE_CARD':
+      var newState = Object.assign({}, state);
+      if (newState[action.id])
+        delete newState[action.id];
+
       return newState;
   }
   return state;
