350376fe90a3a2b24d627e5609f49b4e2e6c3921
John Knoll <jpknoll@gmail.com>
2016-10-18 16:56:05 -0700

    reduce!

--- app/reducers/cards.js
+++ app/reducers/cards.js
@@ -1,9 +1,19 @@
 
-function activeCards(state, action) {
-  switch (action.type) {
-    case: 'HIDE_CARD':
-      return [];
-  }
+var initialState = {
+}
 
+function activeCards(state = initialState, action) {
+  switch (action.type) {
+    case: 'SHOW_CARD':
+      var newState = Object.assign({}, state);
+      if (newState[action.id])
+        newState[action.id] = true;
+      return newState;
+    case: 'HIDE_CARD':
+      var newState = Object.assign({}, state);
+      if (newState[action.id])
+        newState[action.id] = false;
+      return newState;
+  }
   return state;
 }
