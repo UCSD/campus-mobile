bb50aac9e368c9409c5f499b84010278d57bd9f9
John Knoll <jpknoll@ucdavis.edu>
2016-10-19 14:28:46 -0700

    Add id

--- app/views/events/EventCard.js
+++ app/views/events/EventCard.js
@@ -62,7 +62,7 @@ export default class EventCard extends CardComponent {
 
   render() {
 	return (
-		<Card title='Events'>
+		<Card id='events' title='Events'>
 			<View style={css.events_list}>
 				{this.state.eventsDataLoaded ? (
 					<EventList data={this.state.eventsData} navigator={this.props.navigator} />
