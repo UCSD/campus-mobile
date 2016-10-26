5b59a968311bcd71d98a22904133344f78fe441f
Scott Kirkland <srkirkland@gmail.com>
2016-10-26 15:04:12 -0700

    card cleanup

--- app/views/card/Card.js
+++ app/views/card/Card.js
@@ -1,29 +1,26 @@
-'use strict'
-
-import React from 'react'
+import React from 'react';
 import {
 	View,
 } from 'react-native';
-import CardHeader from './CardHeader'
 
-var css = require('../../styles/css');
+import CardHeader from './CardHeader';
+
+const css = require('../../styles/css');
 
 export default class Card extends React.Component {
+	setNativeProps(props) {
+		this._card.setNativeProps(props);
+	}
 
-  refresh(refreshType) {
-    return;
-  }
-
-  setNativeProps(props) {
-    this._card.setNativeProps(props);
-  }
-
-  render() {
-	return (
-		<View style={css.card_main} ref={(i) => this._card = i}>
-			<CardHeader title={this.props.title} cardRefresh={this.props.cardRefresh} isRefreshing={this.props.isRefreshing} />
-			{this.props.children}
-		</View>
+	refresh(refreshType) {
+		return;
+	}
+	render() {
+		return (
+			<View style={css.card_main} ref={(i) => { this._card = i; }}>
+				<CardHeader title={this.props.title} cardRefresh={this.props.cardRefresh} isRefreshing={this.props.isRefreshing} />
+				{this.props.children}
+			</View>
 		);
 	}
 }
