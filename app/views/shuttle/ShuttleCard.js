b7b93adb78f0c81baab567ce65f96b6cede6e616
Scott Kirkland <srkirkland@gmail.com>
2016-10-27 14:56:01 -0700

    shuttle linting

--- app/views/shuttle/ShuttleCard.js
+++ app/views/shuttle/ShuttleCard.js
@@ -1,12 +1,8 @@
-'use strict'
-
-import React from 'react'
+import React from 'react';
 import {
 	View,
-	ListView,
 	Text,
 	ActivityIndicator,
-	TouchableHighlight,
 } from 'react-native';
 
 import Card from '../card/Card';
@@ -15,11 +11,11 @@ import ShuttleService from '../../services/shuttleService';
 import ShuttleOverview from './ShuttleOverview';
 import ShuttleStop from '../ShuttleStop';
 
-var css = require('../../styles/css');
-var logger = require('../../util/logger');
-var shuttle = require('../../util/shuttle');
+const css = require('../../styles/css');
+const logger = require('../../util/logger');
+const shuttle = require('../../util/shuttle');
 
-var shuttle_routes = 	require('../../json/shuttle_routes_master.json');
+const shuttle_routes = 	require('../../json/shuttle_routes_master.json');
 
 export default class ShuttleCard extends CardComponent {
 
@@ -45,7 +41,7 @@ export default class ShuttleCard extends CardComponent {
 
 	componentDidMount() {
 		logger.log('mounted ' + new Date());
-		if (this.props.location){
+		if (this.props.location) {
 			this.refresh();
 		}
 		this.setupAutoRefresh();
@@ -58,11 +54,10 @@ export default class ShuttleCard extends CardComponent {
 					(nextProps.location.coords.latitude !== this.props.location.coords.latitude
 						|| nextProps.location.coords.longitude !== this.props.location.coords.longitude)
 				)) {
-
 			// do not attempt to do refresh location if we are currently in a refresh
 			if (!this.state.isRefreshing) {
 				logger.log(nextProps.location);
-				this.refreshWithLocation('manual', nextProps.location); //refresh with new location
+				this.refreshWithLocation('manual', nextProps.location); // refresh with new location
 			}
 		}
 	}
@@ -98,7 +93,7 @@ export default class ShuttleCard extends CardComponent {
 
 	render() {
 		return (
-			<Card title='Shuttle' cardRefresh={this.refresh} isRefreshing={this.state.isRefreshing}>
+			<Card title="Shuttle" cardRefresh={this.refresh} isRefreshing={this.state.isRefreshing}>
 				{!this.state.closestStop1Loaded && !this.state.closestStop2Loaded && !this.state.closestStop1LoadFailed && !this.state.closestStop2LoadFailed ? (
 					<View style={[css.shuttle_card_row_center, css.shuttle_card_loader]}>
 						<ActivityIndicator style={css.shuttle_card_aa} size="large" />
@@ -112,7 +107,7 @@ export default class ShuttleCard extends CardComponent {
 				{this.state.closestStop2Loaded && !this.state.closestStop2LoadFailed ? (
 					<ShuttleOverview onPress={this.gotoShuttleStop} stopData={this.shuttleClosestStops[1]} shuttleData={this.state.shuttleData[1]} />
 				) : null }
-			
+
 				{this.state.closestStop1LoadFailed && this.state.closestStop2LoadFailed ? (
 					<View style={css.shuttlecard_loading_fail}>
 						<Text style={css.fs18}>No Shuttles en Route</Text>
@@ -126,18 +121,15 @@ export default class ShuttleCard extends CardComponent {
 
 	// SHUTTLE_CARD
 	findClosestShuttleStops = (refreshType, location) => {
-
 		this.shuttleClosestStops[0].dist = 1000000000;
 		this.shuttleClosestStops[1].dist = 1000000000;
 
-		for (var i = 0; shuttle_routes.length > i; i++) {
+		for (let i = 0; shuttle_routes.length > i; i++) {
+			const shuttleRoute = shuttle_routes[i];
 
-			var shuttleRoute = shuttle_routes[i];
-
-			for (var n = 0; shuttleRoute.stops.length > n; n++) {
-
-				var shuttleRouteStop = shuttleRoute.stops[n];
-				var distanceFromStop = shuttle.getDistance(location.coords.latitude, location.coords.longitude, shuttleRouteStop.lat, shuttleRouteStop.lon);
+			for (let n = 0; shuttleRoute.stops.length > n; n++) {
+				const shuttleRouteStop = shuttleRoute.stops[n];
+				const distanceFromStop = shuttle.getDistance(location.coords.latitude, location.coords.longitude, shuttleRouteStop.lat, shuttleRouteStop.lon);
 
 				// Rewrite this later using sortRef from shuttleDetail
 				if (distanceFromStop < this.shuttleClosestStops[0].dist) {
@@ -146,7 +138,7 @@ export default class ShuttleCard extends CardComponent {
 					this.shuttleClosestStops[0].dist = distanceFromStop;
 					this.shuttleClosestStops[0].stopLat = shuttleRouteStop.lat;
 					this.shuttleClosestStops[0].stopLon = shuttleRouteStop.lon;
-				} else if (distanceFromStop < this.shuttleClosestStops[1].dist && this.shuttleClosestStops[0].stopID != shuttleRouteStop.id) {
+				} else if (distanceFromStop < this.shuttleClosestStops[1].dist && this.shuttleClosestStops[0].stopID !== shuttleRouteStop.id) {
 					this.shuttleClosestStops[1].stopID = shuttleRouteStop.id;
 					this.shuttleClosestStops[1].stopName = shuttleRouteStop.name;
 					this.shuttleClosestStops[1].dist = distanceFromStop;
@@ -159,8 +151,8 @@ export default class ShuttleCard extends CardComponent {
 		this.fetchShuttleArrivalsByStop(0, this.shuttleClosestStops[0].stopID);
 		this.fetchShuttleArrivalsByStop(1, this.shuttleClosestStops[1].stopID);
 
-		if (refreshType == 'auto') {
-			//logger.log('Queueing Shuttle Card data refresh in ' + this.shuttleCardRefreshInterval/1000 + ' seconds');
+		if (refreshType === 'auto') {
+			// logger.log('Queueing Shuttle Card data refresh in ' + this.shuttleCardRefreshInterval/1000 + ' seconds');
 		} else {
 			// If manual refresh, reset the Auto refresh timer
 			this.setupAutoRefresh();
@@ -168,68 +160,66 @@ export default class ShuttleCard extends CardComponent {
 	}
 
 	fetchShuttleArrivalsByStop = (closestStopNumber, stopID) => {
-			ShuttleService.FetchShuttleArrivalsByStop(stopID)
-			.then((responseData) => {
-				if (responseData.length > 0 && responseData[0].secondsToArrival) {
+		ShuttleService.FetchShuttleArrivalsByStop(stopID)
+		.then((responseData) => {
+			if (responseData.length > 0 && responseData[0].secondsToArrival) {
+				this.setState({ shuttleData : responseData });
+				let closestShuttleETA = 999999;
 
-					this.setState({shuttleData : responseData});
-					var closestShuttleETA = 999999;
+				for (let i = 0; responseData.length > i; i++) {
+					const shuttleStopArrival = responseData[i];
 
-					for (var i = 0; responseData.length > i; i++) {
+					if (shuttleStopArrival.secondsToArrival < closestShuttleETA ) {
+						closestShuttleETA = shuttleStopArrival.secondsToArrival;
 
-						var shuttleStopArrival = responseData[i];
-
-						if (shuttleStopArrival.secondsToArrival < closestShuttleETA ) {
-							closestShuttleETA = shuttleStopArrival.secondsToArrival;
-
-							this.shuttleClosestStops[closestStopNumber].etaMinutes = shuttle.getMinutesETA(responseData[i].secondsToArrival);
-							this.shuttleClosestStops[closestStopNumber].etaSeconds = shuttleStopArrival.secondsToArrival;
-							this.shuttleClosestStops[closestStopNumber].routeID = shuttleStopArrival.route.id;
-							this.shuttleClosestStops[closestStopNumber].routeName = shuttleStopArrival.route.name;
-							this.shuttleClosestStops[closestStopNumber].routeShortName = shuttleStopArrival.route.shortName;
-							this.shuttleClosestStops[closestStopNumber].routeColor = shuttleStopArrival.route.color;
-						}
+						this.shuttleClosestStops[closestStopNumber].etaMinutes = shuttle.getMinutesETA(responseData[i].secondsToArrival);
+						this.shuttleClosestStops[closestStopNumber].etaSeconds = shuttleStopArrival.secondsToArrival;
+						this.shuttleClosestStops[closestStopNumber].routeID = shuttleStopArrival.route.id;
+						this.shuttleClosestStops[closestStopNumber].routeName = shuttleStopArrival.route.name;
+						this.shuttleClosestStops[closestStopNumber].routeShortName = shuttleStopArrival.route.shortName;
+						this.shuttleClosestStops[closestStopNumber].routeColor = shuttleStopArrival.route.color;
 					}
-
-					if (this.shuttleClosestStops[closestStopNumber].routeShortName == "Campus Loop") {
-						this.shuttleClosestStops[closestStopNumber].routeShortName = "L";
-					}
-					this.shuttleClosestStops[closestStopNumber].routeName = this.shuttleClosestStops[closestStopNumber].routeName.replace(/.*\) /, '').replace(/ - .*/, '');
-
-					if (closestStopNumber == 0) {
-						this.setState({
-							closestStop1Loaded: true,
-							closestStop1LoadFailed: false
-						});
-					} else if (closestStopNumber == 1) {
-						this.setState({
-							closestStop2Loaded: true,
-							closestStop2LoadFailed: false
-						});
-					}
-
-				} else {
-					throw('Invalid response');
 				}
 
-				this.setState({ isRefreshing: false });
-			})
-			.catch((error) => {
-
-				logger.error('ERR: fetchShuttleArrivalsByStop: ' + error + ' (stop: ' + closestStopNumber + ')');
-
-				if (closestStopNumber == 0) {
-					this.setState({ closestStop1LoadFailed: true });
-				} else if (closestStopNumber == 1) {
-					this.setState({ closestStop2LoadFailed: true });
+				if (this.shuttleClosestStops[closestStopNumber].routeShortName === 'Campus Loop') {
+					this.shuttleClosestStops[closestStopNumber].routeShortName = 'L';
 				}
 
-				this.setState({ isRefreshing: false });
-			})
-			.done();
+				this.shuttleClosestStops[closestStopNumber].routeName = this.shuttleClosestStops[closestStopNumber].routeName.replace(/.*\) /, '').replace(/ - .*/, '');
+
+				if (closestStopNumber === 0) {
+					this.setState({
+						closestStop1Loaded: true,
+						closestStop1LoadFailed: false
+					});
+				} else if (closestStopNumber === 1) {
+					this.setState({
+						closestStop2Loaded: true,
+						closestStop2LoadFailed: false
+					});
+				}
+			}
+			else {
+				throw new Error('Invalid response');
+			}
+
+			this.setState({ isRefreshing: false });
+		})
+		.catch((error) => {
+			logger.error('ERR: fetchShuttleArrivalsByStop: ' + error + ' (stop: ' + closestStopNumber + ')');
+
+			if (closestStopNumber === 0) {
+				this.setState({ closestStop1LoadFailed: true });
+			} else if (closestStopNumber === 1) {
+				this.setState({ closestStop2LoadFailed: true });
+			}
+
+			this.setState({ isRefreshing: false });
+		})
+		.done();
 	}
 
 	gotoShuttleStop = (stopData, shuttleData) => {
-		this.props.navigator.push({ id: 'ShuttleStop', name: 'Shuttle Stop', component: ShuttleStop, title: 'Shuttle',stopData: stopData, currentPosition: this.state.currentPosition, shuttleData: shuttleData });
+		this.props.navigator.push({ id: 'ShuttleStop', name: 'Shuttle Stop', component: ShuttleStop, title: 'Shuttle',stopData, currentPosition: this.state.currentPosition, shuttleData });
 	}
 }
