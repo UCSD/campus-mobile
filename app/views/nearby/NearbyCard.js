24db9275f17529a01ddee3f8ae1a6a08ab060821
Scott Kirkland <srkirkland@gmail.com>
2016-10-27 14:50:26 -0700

    cleanup nearby

--- app/views/nearby/NearbyCard.js
+++ app/views/nearby/NearbyCard.js
@@ -1,28 +1,24 @@
-'use strict'
-
-import React from 'react'
+import React from 'react';
 import {
 	View,
 	ListView,
-	Text,
-	TouchableHighlight,
 } from 'react-native';
 
-import Card from '../card/Card'
-import CardComponent from '../card/CardComponent'
+import Card from '../card/Card';
+import CardComponent from '../card/CardComponent';
 import NearbyList from './NearbyList';
 import NearbyMap from './NearbyMap';
 
-var css = require('../../styles/css');
-var logger = require('../../util/logger');
-var shuttle = require('../../util/shuttle');
-var AppSettings = 		require('../../AppSettings');
-var general = require('../../util/general');
-
+const css = require('../../styles/css');
+const logger = require('../../util/logger');
+const shuttle = require('../../util/shuttle');
+const AppSettings = 		require('../../AppSettings');
+const general = require('../../util/general');
 
 // UCSD Nodes
-var ucsd_nodes = 		require('../../json/ucsd_nodes.json');
-var fiveRandomColors = general.getRandomColorArray(5);
+const ucsd_nodes = 		require('../../json/ucsd_nodes.json');
+
+const fiveRandomColors = general.getRandomColorArray(5);
 
 export default class NearbyCard extends CardComponent {
 
@@ -35,12 +31,12 @@ export default class NearbyCard extends CardComponent {
 			nearbyMarkersLoaded: false,
 			nearbyMaxResults: 5,
 			nearbyAnnotations: null,
-			nearbyLatDelta: .02,
-			nearbyLonDelta: .02,
+			nearbyLatDelta: 0.02,
+			nearbyLonDelta: 0.02,
 			nearbyMarkersFull: null,
 			nearbyMarkersPartial: null,
 			nearbyInvalidated: false,
-		}
+		};
 	}
 
 	componentDidMount() {
@@ -49,7 +45,7 @@ export default class NearbyCard extends CardComponent {
 
 	shouldComponentUpdate() {
 		this.refresh();
-		if(this.state.nearbyInvalidated) {
+		if (this.state.nearbyInvalidated) {
 			this.state.nearbyInvalidated = false;
 			return true;
 		}
@@ -60,17 +56,17 @@ export default class NearbyCard extends CardComponent {
 
 	// Updates which predesignated node region the user is in
 	refresh() {
-		var currentLat = this.props.getCurrentPosition('lat');
-		var currentLon = this.props.getCurrentPosition('lon');
+		const currentLat = this.props.getCurrentPosition('lat');
+		const currentLon = this.props.getCurrentPosition('lon');
 
 		// Determine if location has changed since last run, skip if con
 		if ((this.props.getCurrentPosition('lat') !== this.state.nodePreviousLat) ||
 			(this.props.getCurrentPosition('lon') !== this.state.nodePreviousLon)) {
-			var closestNode = 0;
-			var closestNodeDistance = 100000000;
+			let closestNode = 0;
+			let closestNodeDistance = 100000000;
 
-			for (var i = 0; ucsd_nodes.length > i; i++) {
-				var nodeDist = shuttle.getDistance(currentLat, currentLon, ucsd_nodes[i].lat, ucsd_nodes[i].lon);
+			for (let i = 0; ucsd_nodes.length > i; i++) {
+				const nodeDist = shuttle.getDistance(currentLat, currentLon, ucsd_nodes[i].lat, ucsd_nodes[i].lon);
 
 				if (nodeDist < closestNodeDistance) {
 					closestNodeDistance = nodeDist;
@@ -78,57 +74,54 @@ export default class NearbyCard extends CardComponent {
 				}
 			}
 
-			var NODE_MODULES_URL = AppSettings.NODE_MARKERS_BASE_URL + 'ucsd_node_' + closestNode + '.json';
+			const NODE_MODULES_URL = AppSettings.NODE_MARKERS_BASE_URL + 'ucsd_node_' + closestNode + '.json';
 
 			fetch(NODE_MODULES_URL, {
-					method: 'GET',
-				})
-				.then((response) => response.json())
-				.then((responseData) => {
-					this.parseNodeRegion(responseData);
-				})
-				.catch((error) => {
-					logger.log('ERR: loadNodeRegion: ' + error);
-				})
-				.done();
+				method: 'GET',
+			})
+			.then((response) => response.json())
+			.then((responseData) => {
+				this.parseNodeRegion(responseData);
+			})
+			.catch((error) => {
+				logger.log('ERR: loadNodeRegion: ' + error);
+			})
+			.done();
+
 			this.setState({
 				nodePreviousLat: this.props.getCurrentPosition('lat'),
 				nodePreviousLon: this.props.getCurrentPosition('lon')
 			});
 		}
 		else {
-			
+
 		}
-		
 	}
 
 	parseNodeRegion(ucsd_node) {
 		// Calc distance from markers
-		for (var i = 0; ucsd_node.length > i; i++) {
+		for (let i = 0; ucsd_node.length > i; i++) {
 			ucsd_node[i].distance = shuttle.getDistance(this.props.getCurrentPosition('lat'), this.props.getCurrentPosition('lon'), ucsd_node[i].mkrLat, ucsd_node[i].mkrLong);
 		}
 
 		ucsd_node.sort(general.sortNearbyMarkers);
-		var nodeDataFull = ucsd_node;
-		var nodeDataPartial = ucsd_node.slice(0, this.state.nearbyMaxResults);
+		const nodeDataFull = ucsd_node;
+		const nodeDataPartial = ucsd_node.slice(0, this.state.nearbyMaxResults);
 
-		var dsFull = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-		var dsPartial = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		const dsFull = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+		const dsPartial = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
-		var farthestMarkerDist;
-
-		var nearbyAnnotations = [];
-		for (var i = 0; ucsd_node.length > i && this.state.nearbyMaxResults > i; i++) {
+		const nearbyAnnotations = [];
+		for (let i = 0; ucsd_node.length > i && this.state.nearbyMaxResults > i; i++) {
 			if (this.state.nearbyMaxResults === i + 1) {
-				farthestMarkerDist = ucsd_node[i].distance;
-				var distLatLon = Math.sqrt(Math.pow(Math.abs(this.props.getCurrentPosition('lat') - ucsd_node[i].mkrLat), 2) + Math.pow(Math.abs(this.props.getCurrentPosition('lon') - ucsd_node[i].mkrLong), 2));
+				const distLatLon = Math.sqrt(Math.pow(Math.abs(this.props.getCurrentPosition('lat') - ucsd_node[i].mkrLat), 2) + Math.pow(Math.abs(this.props.getCurrentPosition('lon') - ucsd_node[i].mkrLong), 2));
 				this.setState({
 					nearbyLatDelta: distLatLon * 2,
 					nearbyLonDelta: distLatLon * 2
 				});
 			}
 
-			var newAnnotations = {};
+			const newAnnotations = {};
 
 			newAnnotations.coords = {
 				latitude: parseFloat(ucsd_node[i].mkrLat),
@@ -142,7 +135,7 @@ export default class NearbyCard extends CardComponent {
 			nearbyAnnotations.push(newAnnotations);
 		}
 		this.setState({
-			nearbyAnnotations: nearbyAnnotations,
+			nearbyAnnotations,
 			nearbyMarkersFull: dsFull.cloneWithRows(nodeDataFull),
 			nearbyMarkersPartial: dsPartial.cloneWithRows(nodeDataPartial),
 			nearbyMarkersLoaded: true,
@@ -152,10 +145,11 @@ export default class NearbyCard extends CardComponent {
 
 	render() {
 		return (
-			<Card title='Nearby'>
+			<Card title="Nearby">
 				<View>
-					<NearbyMap nearbyAnnotations={this.state.nearbyAnnotations} 
-						updatedGoogle={this.props.updatedGoogle} 
+					<NearbyMap
+						nearbyAnnotations={this.state.nearbyAnnotations}
+						updatedGoogle={this.props.updatedGoogle}
 						getCurrentPosition={(latlon) => this.props.getCurrentPosition(latlon)}
 						nearbyLonDelta={this.state.nearbyLonDelta}
 						nearbyLatDelta={this.state.nearbyLatDelta}
@@ -163,11 +157,12 @@ export default class NearbyCard extends CardComponent {
 					/>
 					<View style={css.events_list}>
 						{this.state.nearbyMarkersLoaded ? (
-							<NearbyList 
-								data={this.state.nearbyMarkersPartial} 
+							<NearbyList
+								data={this.state.nearbyMarkersPartial}
 								colors={fiveRandomColors}
 								getCurrentPosition={(latlon) => this.props.getCurrentPosition(latlon)}
-								navigator={this.props.navigator} />
+								navigator={this.props.navigator}
+							/>
 						) : null}
 					</View>
 				</View>
--- app/views/nearby/NearbyMap.js
+++ app/views/nearby/NearbyMap.js
@@ -1,20 +1,14 @@
-import React from 'react'
+import React from 'react';
 import {
 	View,
-	ListView,
 	Text,
 	TouchableHighlight,
 } from 'react-native';
-import NearbyItem from './NearbyItem';
-import Icon from 'react-native-vector-icons/FontAwesome';
 
-var css = require('../../styles/css');
-var logger = require('../../util/logger');
-var general = require('../../util/general');
+const css = require('../../styles/css');
 
 const GoogleAPIAvailability = 	require('react-native-google-api-availability-bridge');
-var MapView = require('react-native-maps');
-var nearbyCounter;
+const MapView = require('react-native-maps');
 
 export default class NearbyMap extends React.Component {
 
@@ -25,7 +19,7 @@ export default class NearbyMap extends React.Component {
 	}
 
 	render() {
-		return(
+		return (
 			<View style={css.destinationcard_map_container}>
 				{this.props.nearbyAnnotations && this.props.updatedGoogle && this.props.getCurrentPosition('lat') ? (
 
@@ -41,27 +35,28 @@ export default class NearbyMap extends React.Component {
 							longitude: this.props.getCurrentPosition('lon'),
 							latitudeDelta: this.props.nearbyLatDelta,
 							longitudeDelta: this.props.nearbyLonDelta,
-						}}>
-							{this.props.nearbyAnnotations.map((marker, index) => (
-								<MapView.Marker
-									pinColor={this.props.colors[index]}
-									coordinate={marker.coords}
-									title={marker.title}
-									description={marker.description}
-									key={index}
-								/>
-							))}
+						}}
+					>
+						{this.props.nearbyAnnotations.map((marker, index) => (
+							<MapView.Marker
+								pinColor={this.props.colors[index]}
+								coordinate={marker.coords}
+								title={marker.title}
+								description={marker.description}
+								key={index}
+							/>
+						))}
 					</MapView>
 				) : null }
 
 				{!this.props.updatedGoogle ? (
 					<View>
-					<Text>Please update Google Play Services and restart app to view map.</Text>
-					<TouchableHighlight underlayColor={'rgba(200,200,200,.1)'} onPress={() => GoogleAPIAvailability.openGooglePlayUpdate()}>
-						<View style={css.eventdetail_readmore_container}>
-							<Text style={css.eventdetail_readmore_text}>Update</Text>
-						</View>
-					</TouchableHighlight>
+						<Text>Please update Google Play Services and restart app to view map.</Text>
+						<TouchableHighlight underlayColor={'rgba(200,200,200,.1)'} onPress={() => GoogleAPIAvailability.openGooglePlayUpdate()}>
+							<View style={css.eventdetail_readmore_container}>
+								<Text style={css.eventdetail_readmore_text}>Update</Text>
+							</View>
+						</TouchableHighlight>
 					</View>
 					) : null}
 
