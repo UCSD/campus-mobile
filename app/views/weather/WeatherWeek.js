1f98c02d6cb1a82114fd24c39ea299e6b31c4b75
Scott Kirkland <srkirkland@gmail.com>
2016-10-27 15:05:16 -0700

    weather card

--- app/views/weather/WeatherWeek.js
+++ app/views/weather/WeatherWeek.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 import {
 	View,
 } from 'react-native';
 
 import WeatherDay from './WeatherDay';
 
-var css = require('../../styles/css');
+const css = require('../../styles/css');
 
 export default class WeatherWeek extends React.Component {
 	render() {
 		return (
 			<View style={css.wc_botrow}>
-				{this.props.weatherData.daily.data.map((val,index) => {
-					return <WeatherDay key={index} data={val} />;
-				})}
+				{this.props.weatherData.daily.data.map((val,index) =>
+					<WeatherDay key={index} data={val} />
+				)}
 			</View>
 		);
 	}
