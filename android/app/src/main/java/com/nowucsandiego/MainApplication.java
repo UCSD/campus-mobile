dd1acce5dda05490e47f1fea553f21a7b28bac5c
Ivan Wu <i8wu@act-i8wu.local>
2016-10-31 14:17:58 -0700

    Removed duplicate codepush linkage

--- android/app/src/main/java/com/nowucsandiego/MainApplication.java
+++ android/app/src/main/java/com/nowucsandiego/MainApplication.java
@@ -5,7 +5,6 @@ import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.microsoft.codepush.react.CodePush;
-import com.microsoft.codepush.react.CodePush;
 import com.ivanwu.googleapiavailabilitybridge.ReactNativeGooglePlayServicesPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -37,7 +36,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
           new ReactNativeGooglePlayServicesPackage(),
           new GoogleAnalyticsBridgePackage(),
