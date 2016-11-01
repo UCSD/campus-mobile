5103e17bc4125200bd3badb7bacbff6aacc9d133
John Knoll <jpknoll@ucdavis.edu>
2016-11-01 16:06:52 -0700

    get current location

--- app/services/locationService.js
+++ app/services/locationService.js
@@ -1,5 +1,12 @@
 
+export function getLocation() {
+	return new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(
+				(position) => { resolve(position); },
+				(error) => { reject(error); }
+			);
+	});
+}
 
-export default class LocationService {
-
+export function getPermission() {
 }
