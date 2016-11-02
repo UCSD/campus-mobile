b8157b00c1a8fa2db8995289805e9829e2f75a35
John Knoll <jpknoll@ucdavis.edu>
2016-11-02 14:01:59 -0700

    disable cache

--- app/services/locationService.js
+++ app/services/locationService.js
@@ -1,4 +1,12 @@
 
+const logger = require('../util/logger');
+
+const positionOptions = {
+	enableHighAccuracy: false,
+	timeout: 1000,
+	maximumAge: 0
+};
+
 export function getPosition() {
 	return new Promise((resolve, reject) => {
 		navigator.geolocation.getCurrentPosition(
@@ -6,6 +14,7 @@ export function getPosition() {
 					logger.log(position);
 					resolve(position); },
 				(error) => { reject(error); },
+				positionOptions
 			);
 	});
 }
