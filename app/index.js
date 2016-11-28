42bf26839832eb514cbf08ff705d7da168506e36
Charles <c3bryant@ucsd.edu>
2016-11-28 12:08:43 -0800

    change preferences title to settings

--- app/index.js
+++ app/index.js
@@ -116,7 +116,7 @@ var nowucsandiego = React.createClass({
 		this.refs.navRef.push({
 			id: 'PreferencesView',
 			component: PreferencesView,
-			title: 'Preferences'
+			title: 'Settings'
 		});
 	},
 
@@ -168,7 +168,7 @@ var nowucsandiego = React.createClass({
 	renderScene(route, navigator, index, navState) {
 		switch (route.id) {
 		case 'Home': 				return (<Home route={route} navigator={navigator} new_timeout={this.newTimeout} do_timeout={this.doTimeout} />);
-		case 'PreferencesView': return (<PreferencesView route={route} navigator={navigator} />);
+		case 'PreferencesView': 	return (<PreferencesView route={route} navigator={navigator} />);
 		case 'ShuttleStop': 		return (<ShuttleStop route={route} navigator={navigator} />);
 		case 'SurfReport': 			return (<SurfReport route={route} navigator={navigator} />);
 		case 'DiningListView': 		return (<DiningListView route={route} navigator={navigator} />);
