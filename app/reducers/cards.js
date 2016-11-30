7c79bea211f5b7bf7f9c30d46812d672e7f52e50
Charles <c3bryant@ucsd.edu>
2016-11-30 13:15:03 -0800

    card initial state enhancement

--- app/reducers/cards.js
+++ app/reducers/cards.js
@@ -1,28 +1,29 @@
 // NOTE: AppSettings.XX_CARD_ENABLED is being deprecated to pave the way for more dynamic cards
 
-const initialState = {
-	'Weather': true,
-	'Shuttle': true,
-	'Dining': true,
-	'Events': true,
-	'Quick Links': true,
-	'News': true,
-	'Map': true,
+var initialState = {
+	'weather': 		{ active: true, order: 1, name: 'Weather' },
+	'shuttle': 		{ active: true, order: 2, name: 'Shuttle' },
+	'dining': 		{ active: true, order: 3, name: 'Dining' },
+	'events': 		{ active: true, order: 4, name: 'Events' },
+	'quicklinks': 	{ active: true, order: 5, name: 'Quick Links' },
+	'news': 		{ active: true, order: 6, name: 'News' },
+	'map': 			{ active: true, order: 7, name: 'Map' }
 };
 
 function cards(state = initialState, action) {
+
 	switch (action.type) {
 		case 'SHOW_CARD':
 			var newState = {...state};
-			if (!newState[action.id])
-				newState[action.id] = true;
-			return newState;
+			if (!newState[action.id].active)
+				newState[action.id].active = true;
+			return newState[action.id].active;
 
 		case 'HIDE_CARD':
 			var newState = {...state};
-			if (newState[action.id])
-				newState[action.id] = false;
-			return newState;
+			if (newState[action.id].active)
+				newState[action.id].active = false;
+			return newState[action.id].active;
 
 		case 'ADD_CARD':
 			var newState = {...state};
@@ -39,6 +40,7 @@ function cards(state = initialState, action) {
 			return newState;
 	}
 	return state;
+
 }
 
 module.exports = cards
