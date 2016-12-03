33cbb95a2687a3a0715c4ec3b9641888d527a9ed
Charles <c3bryant@ucsd.edu>
2016-12-02 16:06:06 -0800

    update replace_codepush_key script, readme file

--- scripts/README.md
+++ scripts/README.md
@@ -2,17 +2,29 @@
 
 2) In your ".nowapp" folder, create a file "env.js" with the following content:
 
-	var APP_NAME = 'now-mobile';
-	var APP_SHORTNAME = 'now-mobile';	// Text shown under your App Icon
+	// Modify These Values
+	var APP_NAME = 'your-app-name';
+	var APP_SHORTNAME = 'your-app-shortname';	// Text shown under your App Icon
 
 	var CODEPUSH_IOS_KEY_PROD = 'iOS Production Key Here';
 	var CODEPUSH_IOS_KEY_STG = 'iOS Staging Key Here';
-	var CODEPUSH_ANDROID_KEY_STG = 'Android Staging Key Here';
 	var CODEPUSH_ANDROID_KEY_PROD = 'Android Production Key Here';
+	var CODEPUSH_ANDROID_KEY_STG = 'Android Staging Key Here';
 
 	var GOOGLE_ANALYTICS_ID = 'Google Analytics ID Here';
 	var GOOGLE_MAPS_API_KEY = 'Google Maps API Key Here';
+	
 
+	// Placeholder Values (Do Not Modify)
+	var APP_NAME_PH = 'now-mobile';
+	var APP_SHORTNAME_PH = 'nowmobile';
+	var CODEPUSH_IOS_KEY_PH = 'CODEPUSH_IOS_DEPLOYMENT_KEY';
+	var CODEPUSH_ANDROID_KEY_PH = 'CODEPUSH_ANDROID_DEPLOYMENT_KEY';
+	var GOOGLE_ANALYTICS_ID_PH = 'GOOGLE_ANALYTICS_ID';
+	var GOOGLE_MAPS_API_KEY_PH = 'GOOGLE_MAPS_API_KEY';
+
+
+	// Exports (Do Not Modify)
 	exports.APP_NAME = APP_NAME;
 	exports.APP_SHORTNAME = APP_SHORTNAME;
 
@@ -23,3 +35,10 @@
 
 	exports.GOOGLE_ANALYTICS_ID = GOOGLE_ANALYTICS_ID;
 	exports.GOOGLE_MAPS_API_KEY = GOOGLE_MAPS_API_KEY;
+
+	exports.APP_NAME_PH = APP_NAME_PH;
+	exports.APP_SHORTNAME_PH = APP_SHORTNAME_PH;
+	exports.CODEPUSH_IOS_KEY_PH = CODEPUSH_IOS_KEY_PH;
+	exports.CODEPUSH_ANDROID_KEY_PH = CODEPUSH_ANDROID_KEY_PH;
+	exports.GOOGLE_ANALYTICS_ID_PH = GOOGLE_ANALYTICS_ID_PH;
+	exports.GOOGLE_MAPS_API_KEY_PH = GOOGLE_MAPS_API_KEY_PH;
--- scripts/replace_codepush_key.js
+++ scripts/replace_codepush_key.js
@@ -1,41 +1,79 @@
+// Usage: replace_codepush_key.js [Production|Staging|Placeholder]
+
+var REPLACEMENT_TYPE = process.argv[2];
+
 var fs = require('fs');
-var myEnv = require(getUserHome() + '/.nowapp/env');
+var myEnv = require(getUserHome() + '/.nowapp/env.js');
 
 var ANDROID_FILE_PATH = '../android/app/src/main/res/values/strings.xml';
-var ANDROID_STRING_TO_REPLACE = 'CODEPUSH_ANDROID_DEPLOYMENT_KEY';
-
 var IOS_FILE_PATH = '../ios/nowucsandiego/Info.plist';
-var IOS_STRING_TO_REPLACE = 'CODEPUSH_IOS_DEPLOYMENT_KEY';
 
-var REPLACEMENT_STRING = myEnv.CODEPUSH_KEY;
+var CODEPUSH_IOS_KEY_PROD = myEnv.CODEPUSH_IOS_KEY_PROD;
+var CODEPUSH_IOS_KEY_STG = myEnv.CODEPUSH_IOS_KEY_STG;
+var CODEPUSH_IOS_KEY_PH = myEnv.CODEPUSH_IOS_KEY_PH;
 
+var CODEPUSH_ANDROID_KEY_PROD = myEnv.CODEPUSH_ANDROID_KEY_PROD;
+var CODEPUSH_ANDROID_KEY_STG = myEnv.CODEPUSH_ANDROID_KEY_STG;
+var CODEPUSH_ANDROID_KEY_PH = myEnv.CODEPUSH_ANDROID_KEY_PH;
 
-// iOS
-fs.readFile(IOS_FILE_PATH, 'utf8', function(err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    var result = data.replace(IOS_STRING_TO_REPLACE, REPLACEMENT_STRING);
+if (REPLACEMENT_TYPE === 'Production' || REPLACEMENT_TYPE === 'Staging' || REPLACEMENT_TYPE === 'Placeholder') {
 
-    fs.writeFile(IOS_FILE_PATH, result, 'utf8', function(err) {
-        if (err) return console.log(err);
-    });
-});
+	// iOS
+	fs.readFile(IOS_FILE_PATH, 'utf8', function(err, data) {
+		if (err) {
+			return console.log(err);
+		} else {
+			var result;
 
+			if (REPLACEMENT_TYPE === 'Production') {
+				result = data.replace(CODEPUSH_IOS_KEY_PH, CODEPUSH_IOS_KEY_PROD).replace(CODEPUSH_IOS_KEY_STG, CODEPUSH_IOS_KEY_PROD);
+			} else if (REPLACEMENT_TYPE === 'Staging') {
+				result = data.replace(CODEPUSH_IOS_KEY_PH, CODEPUSH_IOS_KEY_STG).replace(CODEPUSH_IOS_KEY_PROD, CODEPUSH_IOS_KEY_STG);
+			} else if (REPLACEMENT_TYPE === 'Placeholder') {
+				result = data.replace(CODEPUSH_IOS_KEY_STG, CODEPUSH_IOS_KEY_PH).replace(CODEPUSH_IOS_KEY_PROD, CODEPUSH_IOS_KEY_PH);
+			}
 
+			fs.writeFile(IOS_FILE_PATH, result, 'utf8', function(err) {
+				if (err) {
+					return console.log(err);
+				} else {
+					console.log('SUCCESS: File ' + IOS_FILE_PATH + ' updated with ' + REPLACEMENT_TYPE + ' values.');
+				}
+			});
+		}
+	});
 
-// Android
-fs.readFile(ANDROID_FILE_PATH, 'utf8', function(err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    var result = data.replace(ANDROID_STRING_TO_REPLACE, REPLACEMENT_STRING);
+	// Android
+	fs.readFile(ANDROID_FILE_PATH, 'utf8', function(err, data) {
+		if (err) {
+			return console.log(err);
+		} else {
+			var result;
 
-    fs.writeFile(ANDROID_FILE_PATH, result, 'utf8', function(err) {
-        if (err) return console.log(err);
-    });
-});
+			if (REPLACEMENT_TYPE === 'Production') {
+				result = data.replace(CODEPUSH_ANDROID_KEY_PH, CODEPUSH_ANDROID_KEY_PROD).replace(CODEPUSH_ANDROID_KEY_STG, CODEPUSH_ANDROID_KEY_PROD);
+			} else if (REPLACEMENT_TYPE === 'Staging') {
+				result = data.replace(CODEPUSH_ANDROID_KEY_PH, CODEPUSH_ANDROID_KEY_STG).replace(CODEPUSH_ANDROID_KEY_PROD, CODEPUSH_ANDROID_KEY_STG);
+			} else if (REPLACEMENT_TYPE === 'Placeholder') {
+				result = data.replace(CODEPUSH_ANDROID_KEY_STG, CODEPUSH_ANDROID_KEY_PH).replace(CODEPUSH_ANDROID_KEY_PROD, CODEPUSH_ANDROID_KEY_PH);
+			}
+
+			fs.writeFile(ANDROID_FILE_PATH, result, 'utf8', function(err) {
+				if (err) {
+					return console.log(err);
+				} else {
+					console.log('SUCCESS: File ' + ANDROID_FILE_PATH + ' updated with ' + REPLACEMENT_TYPE + ' values.');
+				}
+			});
+		}
+	});
+
+} else {
+	console.log('Error: Replacement type not specififed');
+	console.log('Sample Usage: replace_codepush_keys.js [replacement type]');
+	console.log('Replacement Types: Production, Staging, Placeholder');
+}
 
 function getUserHome() {
-    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+	return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
