6b3e70468a955908dd17c5201f7b1a090a52fb6d
Charles <c3bryant@ucsd.edu>
2017-02-08 16:31:23 -0800

    add version tracking to api endpoints

--- app/AppSettings.js
+++ app/AppSettings.js
@@ -1,29 +1,33 @@
+const APP_VERSION = 			'5.0',
+	  APP_NAME = 				'campus-mobile',
+	  API_TRACKING = 			'version=' + APP_NAME + '_' + APP_VERSION;
+
 module.exports = {
 
 	/* APP CONFIG */
-	APP_VERSION: 				'5.0',
+	APP_NAME: 					APP_NAME,
+	APP_VERSION: 				APP_VERSION,
 	APP_CODEPUSH_VERSION: 		'1',
-	APP_NAME: 					'campus-mobile',
 	APP_CAMPUS_NAME: 			'APP_CAMPUS_NAME_PH',
 	GOOGLE_ANALYTICS_ID: 		'GOOGLE_ANALYTICS_ID_PH',
 	DEBUG_ENABLED: 				false, // Disables all logger functions (i.e. logger.log, logger.error).
 									   // Use console.log/error as necessary but do not commit code using console.log/error
 
 	/* APIs / FEEDS */
-	WEATHER_API_URL: 			'https://w3wyps9yje.execute-api.us-west-2.amazonaws.com/prod/forecast',
-	SURF_API_URL: 				'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/surffeed.json',
-	SHUTTLE_STOPS_API_URL: 		'https://ies4wyrlx9.execute-api.us-west-2.amazonaws.com/prod/stops/',
-	DINING_API_URL: 			'https://pg83tslbyi.execute-api.us-west-2.amazonaws.com/prod/v2/dining/locations',
-	EVENTS_API_URL: 			'https://evv6vpvob6.execute-api.us-west-2.amazonaws.com/prod/',
-	QUICKLINKS_API_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/quick_links/ucsd-quicklinks-v2.json',
-	NEWS_API_URL: 				'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/allstories.json',
-	MAP_SEARCH_API_URL: 		'https://xgu9qa7gx4.execute-api.us-west-2.amazonaws.com/prod/map/search?region=0&query=',
+	WEATHER_API_URL: 			'https://w3wyps9yje.execute-api.us-west-2.amazonaws.com/prod/forecast?' + API_TRACKING,
+	SURF_API_URL: 				'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/surffeed.json?' + API_TRACKING,
+	SHUTTLE_STOPS_API_URL: 		'https://ies4wyrlx9.execute-api.us-west-2.amazonaws.com/prod/stops/?' + API_TRACKING,
+	DINING_API_URL: 			'https://pg83tslbyi.execute-api.us-west-2.amazonaws.com/prod/v2/dining/locations?' + API_TRACKING,
+	EVENTS_API_URL: 			'https://evv6vpvob6.execute-api.us-west-2.amazonaws.com/prod/?' + API_TRACKING,
+	QUICKLINKS_API_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/quick_links/ucsd-quicklinks-v2.json?' + API_TRACKING,
+	NEWS_API_URL: 				'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/allstories.json?' + API_TRACKING,
+	MAP_SEARCH_API_URL: 		'https://xgu9qa7gx4.execute-api.us-west-2.amazonaws.com/prod/map/search?' + API_TRACKING + '&region=0&query=',
+	WELCOME_WEEK_API_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/welcome_week_events.json?' + API_TRACKING,
 
 	/* RESOURCES */
 	FEEDBACK_URL: 				'https://eforms.ucsd.edu/view.php?id=175631',
 	NODE_MARKERS_BASE_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/nearby_markers/',
 	WEATHER_ICON_BASE_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/images/v1/weather-icons/',
-	WELCOME_WEEK_API_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/welcome_week_events.json',
 
 	/* API TTL (seconds) */
 	WEATHER_API_TTL: 			3600,
