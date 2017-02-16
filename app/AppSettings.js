19d981f7f16584468d3a0ac5b84b813a32df8dbe
Charles <c3bryant@ucsd.edu>
2017-02-16 11:31:15 -0800

    created ga property campus mobile which can be used by other developers

--- app/AppSettings.js
+++ app/AppSettings.js
@@ -1,31 +1,31 @@
 module.exports = {
 
 	/* APP CONFIG */
-	APP_NAME: 					'Campus Mobile',
-	APP_VERSION: 				'5.0',
-	APP_CODEPUSH_VERSION: 		'1',
-	GOOGLE_ANALYTICS_ID: 		'GOOGLE_ANALYTICS_ID_PH',
-	DEBUG_ENABLED: 				false, 	// Disables all logger functions (i.e. logger.log, logger.error).
-	// Use console.log/error as necessary but do not commit code using console.log/error
+	APP_NAME: 						'Campus Mobile',
+	APP_VERSION: 					'5.0',
+	APP_CODEPUSH_VERSION: 			'1',
+	GOOGLE_ANALYTICS_ID: 			'UA-59591330-4',
+	DEBUG_ENABLED: 					false, 	// Enable or disable logger.log, logger.error from displaying
+											// Use console.log as necessary for testing, but commit code with logger.log
 
 	/* APIs / FEEDS */
-	WEATHER_API_URL: 			'https://w3wyps9yje.execute-api.us-west-2.amazonaws.com/prod/forecast?',
-	SURF_API_URL: 				'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/surffeed.json',
-	SHUTTLE_STOPS_API_URL: 		'https://ies4wyrlx9.execute-api.us-west-2.amazonaws.com/prod/stops/',
+	WEATHER_API_URL: 				'https://w3wyps9yje.execute-api.us-west-2.amazonaws.com/prod/forecast?',
+	SURF_API_URL: 					'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/surffeed.json',
+	SHUTTLE_STOPS_API_URL: 			'https://ies4wyrlx9.execute-api.us-west-2.amazonaws.com/prod/stops/',
 	SHUTTLE_VEHICLES_API_URL: 		'https://87t4myy3wj.execute-api.us-west-2.amazonaws.com/dev?route=',
-	DINING_API_URL: 			'https://pg83tslbyi.execute-api.us-west-2.amazonaws.com/prod/v2/dining/locations',
-	EVENTS_API_URL: 			'https://evv6vpvob6.execute-api.us-west-2.amazonaws.com/prod/',
-	QUICKLINKS_API_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/quick_links/ucsd-quicklinks-v2.json',
-	NEWS_API_URL: 				'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/allstories.json',
-	MAP_SEARCH_API_URL: 		'https://vkil1id5r5.execute-api.us-west-2.amazonaws.com/dev/map/search?region=0&query=',
-	WELCOME_WEEK_API_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/welcome_week_events.json',
+	DINING_API_URL: 				'https://pg83tslbyi.execute-api.us-west-2.amazonaws.com/prod/v2/dining/locations',
+	EVENTS_API_URL: 				'https://evv6vpvob6.execute-api.us-west-2.amazonaws.com/prod/',
+	QUICKLINKS_API_URL: 			'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/quick_links/ucsd-quicklinks-v2.json',
+	NEWS_API_URL: 					'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/allstories.json',
+	MAP_SEARCH_API_URL: 			'https://vkil1id5r5.execute-api.us-west-2.amazonaws.com/dev/map/search?region=0&query=',
+	WELCOME_WEEK_API_URL: 			'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/welcome_week_events.json',
 
 	/* RESOURCES */
-	FEEDBACK_URL: 				'https://eforms.ucsd.edu/view.php?id=175631',
-	NODE_MARKERS_BASE_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/nearby_markers/',
-	WEATHER_ICON_BASE_URL: 		'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/images/v1/weather-icons/',
-	SHUTTLE_ROUTES_MASTER: 'https://s3-us-west-2.amazonaws.com/ucsd-mobile/v1/shuttle_routes_master_map.json',
-	SHUTTLE_STOPS_MASTER: 'https://s3-us-west-2.amazonaws.com/ucsd-mobile/v1/shuttle_stops_master_map.json',
+	FEEDBACK_URL: 					'https://eforms.ucsd.edu/view.php?id=175631',
+	NODE_MARKERS_BASE_URL: 			'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/nearby_markers/',
+	WEATHER_ICON_BASE_URL: 			'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/images/v1/weather-icons/',
+	SHUTTLE_ROUTES_MASTER: 			'https://s3-us-west-2.amazonaws.com/ucsd-mobile/v1/shuttle_routes_master_map.json',
+	SHUTTLE_STOPS_MASTER: 			'https://s3-us-west-2.amazonaws.com/ucsd-mobile/v1/shuttle_stops_master_map.json',
 	SHUTTLE_STOPS_MASTER_NO_ROUTES: 'https://s3-us-west-2.amazonaws.com/ucsd-mobile/v1/shuttle_stops_master_map_no_routes.json',
 
 	/* API TTL (seconds) */
