95c53d7dff9ca1ec9f57ad2ba79bb13cb8e27e53
Ivan Wu <i8wu@ucsd.edu>
2017-02-17 16:53:52 -0800

    Added delta to map zoom

--- app/views/mapsearch/NearbyMapView.js
+++ app/views/mapsearch/NearbyMapView.js
@@ -347,7 +347,7 @@ class NearbyMapView extends React.Component {
 						</ElevatedView>
 						) : (null)
 					}
-				<ShuttleLocationContainer />
+					<ShuttleLocationContainer />
 				</View>
 			);
 		} else {
