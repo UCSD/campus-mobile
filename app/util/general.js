47b425b300d8891683a9c4017d4e9a6402601fe1
Ivan Wu <i8wu@ucsd.edu>
2017-03-16 08:55:49 -0700

    Removed unused functions and minor tweaks

--- app/util/general.js
+++ app/util/general.js
@@ -1,6 +1,4 @@
 import {
-	Animated,
-	Easing,
 	Platform,
 	Linking,
 	Dimensions,
@@ -11,13 +9,12 @@ const logger = require('./logger');
 
 /**
  * A module containing general helper functions
- * @module util/general
+ * @module general
  */
 module.exports = {
 
 	/**
 	 * Gets whether or not the current platform the app is running on is IOS
-	 * @function
 	 * @returns {boolean} True if the platform is IOS, false otherwise
 	 */
 	platformIOS() {
@@ -26,7 +23,7 @@ module.exports = {
 
 	/**
 	 * Gets whether or not the current platform the app is running on is Android
-	 * @function
+	 * @memberOf util/general
 	 * @returns {boolean} True if the platform is Android, false otherwise
 	 */
 	platformAndroid() {
@@ -35,7 +32,6 @@ module.exports = {
 
 	/**
 	 * Gets the current platform ths app is running on
-	 * @function
 	 * @returns {string} The platform name
 	 */
 	getPlatform() {
@@ -44,7 +40,7 @@ module.exports = {
 
 	/**
 	 * Converts a numerical quantity from meters to miles
-	 * @function
+	 * @memberOf util/general
 	 * @param {number} meters The quantity to convert
 	 * @returns {number} The quantity now converted into miles
 	 */
@@ -54,7 +50,6 @@ module.exports = {
 
 	/**
 	 * Gets a string representation of a given quantity of miles (up to and including a single decimal place)
-	 * @function
 	 * @param {number} miles The quantity to convert to a string
 	 * @returns {number} The miles quantity now as a string
 	 */
@@ -64,25 +59,25 @@ module.exports = {
 
 	/**
 	 * Attempts to open the provided URL for displaying to the user
-	 * @function
 	 * @param {string} url The URL to open
-	 * @returns {boolean|undefined} If the URL cannot be opened, this logs to console. Otherwise, returns true.
-	 * False can only be returned if the ability to open a URL changes between the check and the opening itself.
-	 * @todo Develop a more explicit/consistent return for this method
+	 * @returns {boolean} If the URL cannot be opened, this logs to console and returns false. Otherwise, returns true.
 	 */
 	openURL(url) {
 		Linking.canOpenURL(url).then(supported => {
 			if (!supported) {
 				logger.log('ERR: openURL: Unable to handle url: ' + url);
+				return false;
 			} else {
 				return Linking.openURL(url);
 			}
-		}).catch(err => logger.log('ERR: openURL: ' + err));
+		}).catch(err => {
+			logger.log('ERR: openURL: ' + err);
+			return false;
+		});
 	},
 
 	/**
 	 * Gets the URL for obtaining directions to a given location via a given transportation method
-	 * @function
 	 * @param {string} method Can be "walk" or anything else. Anything else will result in a URL for driving.
 	 * @param {string|number} stopLat The latitude of the destination
 	 * @param {string|number} stopLon The longitude of the destination
@@ -113,49 +108,18 @@ module.exports = {
 
 	/**
 	 * Attempts to redirect the device to its navigation app
-	 * @function
-	 * @param {string} method Currently not used, can be anything
 	 * @param {string|number} destinationLat The destination's latitude
 	 * @param {string|number} destinationLon The destination's longitude
+	 * @returns {boolean} true/false depending if navigation app is opened
 	 */
 	gotoNavigationApp(destinationLat, destinationLon) {
 		const destinationURL = module.exports.getDirectionsURL('walk', destinationLat, destinationLon );
-		module.exports.openURL(destinationURL);
-	},
-
-	/**
-	 * Begins the app's reloading animation with specific parameters
-	 * @function
-	 * @param {View|Text|Image|Object} anim The animation to use
-	 * @param {number} toVal The value to approach
-	 * @param {number} duration The duration of the animation
-	 */
-	startReloadAnimation2(anim, toVal, duration) {
-		Animated.timing(anim, { toValue: toVal, duration, easing: Easing.linear }).start();
-	},
-
-	/**
-	 * Begins the app's reloading animation
-	 * @function
-	 * @param {View|Text|Image|Object} anim The animation to use
-	 */
-	startReloadAnimation(anim) {
-		Animated.timing(anim, { toValue: 100, duration: 60000, easing: Easing.linear }).start();
-	},
-
-	/**
-	 * Stops the specified reload animation
-	 * @function
-	 * @param {View|Text|Image|Object} anim The animation to stop
-	 */
-	stopReloadAnimation(anim) {
-		Animated.timing(anim, { toValue: 0, duration: 0 }).start();
+		return module.exports.openURL(destinationURL);
 	},
 
 	/**
 	 * Rounds a number to the nearest integer
 	 * As examples: 2.49 will be rounded down to 2, while 2.5 will be rounded up to 3
-	 * @function
 	 * @param {number} number The number to round
 	 * @returns {number} The rounded number
 	 */
@@ -166,13 +130,11 @@ module.exports = {
 	/**
 	 * Gets the pixel-ratio-modifier needed for this device window
 	 * The modifier is a ratio to be used for scaling GUI elements based on the default sizes
-	 * @function
 	 * @returns {number} The ratio of current window width vs the app's default width
 	 * @todo The variable windowHeight is unused
 	 */
 	getPRM() {
 		const windowWidth = Dimensions.get('window').width;
-		const windowHeight = Dimensions.get('window').height;
 		const appDefaultWidth = 414;
 		return (windowWidth / appDefaultWidth);
 	},
@@ -180,7 +142,6 @@ module.exports = {
 	/**
 	 * Modifies the input value by the pixel-ration-modifier
 	 * This can be used to scale a GUI element's size for the current device
-	 * @function
 	 * @param {number} num The number/size to scale
 	 * @returns {number} The now-scaled number/size
 	 * @todo This method could call this.getPRM() to prevent redundant code
@@ -195,7 +156,6 @@ module.exports = {
 
 	/**
 	 * Gets the maximum width for a card occupying a screen/window
-	 * @function
 	 * @returns {number} The maximum width in pixels
 	 */
 	getMaxCardWidth() {
@@ -207,7 +167,6 @@ module.exports = {
 
 	/**
 	 * Gets the UCSD campus primary color in hexidecimal form
-	 * @function
 	 * @returns {string} The string "#182B49" which represents a dark blue color
 	 */
 	getCampusPrimary() {
@@ -216,7 +175,6 @@ module.exports = {
 
 	/**
 	 * Gets the current timestamp in seconds
-	 * @function
 	 * @returns {number} The number of seconds since midnight Jan 1, 1970
 	 */
 	getCurrentTimestamp() {
@@ -225,7 +183,6 @@ module.exports = {
 
 	/**
 	 * Gets the current timestamp formatted as a string
-	 * @function
 	 * @returns {string} The current time formatted to "yyyymmdd"
 	 */
 	getTimestampNumeric() {
@@ -234,8 +191,7 @@ module.exports = {
 
 	/**
 	 * Gets the current timestamp in a specified format
-	 * @function
-	 * @param {string} The format to use (e.g. "yyyymmdd")
+	 * @param {string} format The format to use (e.g. "yyyymmdd")
 	 * @returns {string} The formatted current time
 	 */
 	getTimestamp(format) {
@@ -244,7 +200,6 @@ module.exports = {
 
 	/**
 	 * Gets the current date
-	 * @function
 	 * @returns {number} The number of milliseconds since midnight Jan 1, 1970
 	 */
 	getDateNow() {
@@ -253,8 +208,7 @@ module.exports = {
 
 	/**
 	 * Converts a time string provided in military form to AM-PM form
-	 * @function
-	 * @param {string} The time in military form
+	 * @param {string} time The time in military form
 	 * @returns {string} The time in AM-PM form
 	 */
 	militaryToAMPM(time) {
@@ -304,7 +258,6 @@ module.exports = {
 
 	/**
 	 * Gets an array containing random colors
-	 * @function
 	 * @param {number} length The length of the array to return
 	 * @returns {number[]} An array with each element containing a randomly generated hex color code
 	 */
@@ -318,7 +271,6 @@ module.exports = {
 
 	/**
 	 * Generates random color hex
-	 * @function
 	 * @returns {string} A randomly generated hex color code
 	 */
 	getRandomColor() {
@@ -327,19 +279,17 @@ module.exports = {
 
 	/**
 	 * Gets a linear sorting function based on a given property of the input objects
-	 * @function
 	 * @param {string} property The property the sorting function should concern itself with
-	 * @returns {function} A sorting function that compares inputs based on one of their properties 
+	 * @returns {function} A sorting function that compares inputs based on one of their properties
 	 */
 	dynamicSort(property) {
-		return function(a, b) {
+		return function (a, b) {
 			return (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-		}
+		};
 	},
 
 	/**
 	 * This is a comparison function used for sorting markers
-	 * @function
 	 * @param {Object} a The first marker to compare
 	 * @param {number} a.distance The first distance
 	 * @param {Object} b The second marker to compare
