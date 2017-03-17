8e62475097404b71c556e7872c15bb7abcad2f9a
Ivan Wu <i8wu@ucsd.edu>
2017-03-17 14:09:26 -0700

    Added TTL logic for weather action

--- app/actions/weather.js
+++ app/actions/weather.js
@@ -1,9 +1,18 @@
 import logger from '../util/logger';
 import WeatherService from '../services/weatherService';
+import { WEATHER_API_TTL } from '../AppSettings';
 
 function updateWeather() {
-	return (dispatch) => {
-		WeatherService.FetchWeather()
+	return (dispatch, getState) => {
+		const { lastUpdated, data } = getState().weather;
+		const nowTime = new Date().getTime();
+		const timeDiff = nowTime - lastUpdated;
+		const weatherTTL = WEATHER_API_TTL * 1000;
+
+		if (timeDiff < weatherTTL && data) {
+			// Do nothing, no need to fetch new data
+		} else {
+			WeatherService.FetchWeather()
 			.then((weather) => {
 				dispatch({
 					type: 'SET_WEATHER',
@@ -13,6 +22,7 @@ function updateWeather() {
 			.catch((error) => {
 				logger.error(error);
 			});
+		}
 	};
 }
 
