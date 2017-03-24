83fe4a851d1889ab4d9bdd3548cdb4273c4a2f59
Ivan Wu <i8wu@ucsd.edu>
2017-03-24 13:29:57 -0700

    Update event url to prod

--- app/AppSettings.js
+++ app/AppSettings.js
@@ -14,7 +14,7 @@ module.exports = {
     SHUTTLE_STOPS_API_URL: 'https://ies4wyrlx9.execute-api.us-west-2.amazonaws.com/prod/v2/stops/',
     SHUTTLE_VEHICLES_API_URL: 'https://hjr84cay81.execute-api.us-west-2.amazonaws.com/prod?route=',
     DINING_API_URL: 'https://pg83tslbyi.execute-api.us-west-2.amazonaws.com/prod/v2/dining/locations',
-    EVENTS_API_URL: 'https://evv6vpvob6.execute-api.us-west-2.amazonaws.com/prod/',
+    EVENTS_API_URL: 'https://2jjml3hf27.execute-api.us-west-2.amazonaws.com/prod/events/student',
     QUICKLINKS_API_URL: 'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/quick_links/ucsd-quicklinks-v3.json',
     NEWS_API_URL: 'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/allstories.json',
     MAP_SEARCH_API_URL: 'https://xgu9qa7gx4.execute-api.us-west-2.amazonaws.com/prod/v2/map/search?region=0&query=',
