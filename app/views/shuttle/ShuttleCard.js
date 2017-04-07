be396a2811bc6a47cff782e27a1ab2d421d756c4
Charles <c3bryant@ucsd.edu>
2017-04-07 09:09:57 -0700

    Shuttle card capitalize menu selection

--- app/views/shuttle/ShuttleCard.js
+++ app/views/shuttle/ShuttleCard.js
@@ -44,7 +44,7 @@ const ShuttleCard = ({ stopsData, savedStops, permission, gotoRoutesList, gotoSa
 
 	const extraActions = [
 		{
-			name: 'Manage stops',
+			name: 'Manage Stops',
 			action: gotoSavedList
 		}
 	];
