6f8f255ec6620f467f6bd323046e012e7b12e0f5
Charles <c3bryant@ucsd.edu>
2017-04-27 10:57:57 -0700

    Resolve shuttleSaga

--- app/sagas/shuttleSaga.js
+++ app/sagas/shuttleSaga.js
@@ -1,11 +1,7 @@
 import { delay } from 'redux-saga';
 import { call, fork, put, select, takeLatest } from 'redux-saga/effects';
 
-<<<<<<< HEAD
-import ShuttleService from '../services/shuttleService';
-=======
 import { fetchShuttleArrivalsByStop } from '../services/shuttleService';
->>>>>>> v5.1-hotfix
 
 const getShuttle = (state) => (state.shuttle);
 
@@ -13,11 +9,7 @@ function* addStop(action) {
 	const shuttle = yield select(getShuttle);
 	const savedStops = shuttle.savedStops.slice(); // copy array
 	const stops = Object.assign({}, shuttle.stops);
-<<<<<<< HEAD
-	const closestStop = Object.assign({}, shuttle.closestStop);
-=======
 	const closestStop = (shuttle.closestStop) ? Object.assign({}, shuttle.closestStop) : null;
->>>>>>> v5.1-hotfix
 	let contains = false;
 
 	// Make sure stop hasn't already been saved
@@ -32,19 +24,12 @@ function* addStop(action) {
 	}
 
 	// Updated closestStop index
-<<<<<<< HEAD
-	++closestStop.savedIndex;
-
-	yield put({ type: 'CHANGED_STOPS', savedStops });
-	yield put({ type: 'SET_CLOSEST_STOP', closestStop });
-=======
 	if (closestStop) {
 		++closestStop.savedIndex;
 		yield put({ type: 'SET_CLOSEST_STOP', closestStop });
 	}
 
 	yield put({ type: 'CHANGED_STOPS', savedStops });
->>>>>>> v5.1-hotfix
 	yield call(resetScroll);
 	yield fork(fetchArrival, action.stopID);
 }
@@ -52,11 +37,7 @@ function* addStop(action) {
 function* removeStop(action) {
 	const shuttle = yield select(getShuttle);
 	const savedStops = shuttle.savedStops.slice();
-<<<<<<< HEAD
-	const closestStop = Object.assign({}, shuttle.closestStop);
-=======
 	const closestStop = (shuttle.closestStop) ? Object.assign({}, shuttle.closestStop) : null;
->>>>>>> v5.1-hotfix
 
 	let i;
 	// Remove stop from saved array
@@ -67,15 +48,6 @@ function* removeStop(action) {
 		}
 	}
 
-<<<<<<< HEAD
-	// Update closestStop index
-	if (i < closestStop.savedIndex) {
-		--closestStop.savedIndex;
-	}
-
-	yield put({ type: 'CHANGED_STOPS', savedStops });
-	yield put({ type: 'SET_CLOSEST_STOP', closestStop });
-=======
 	if (closestStop) {
 		// Update closestStop index
 		if (i < closestStop.savedIndex) {
@@ -85,7 +57,6 @@ function* removeStop(action) {
 	}
 
 	yield put({ type: 'CHANGED_STOPS', savedStops });
->>>>>>> v5.1-hotfix
 	yield call(resetScroll);
 }
 
@@ -96,14 +67,9 @@ function* orderStops(action) {
 			const { newStops, newClosest } = yield call(doOrder, newOrder);
 
 			yield put({ type: 'CHANGED_STOPS', savedStops: newStops });
-<<<<<<< HEAD
-			yield put({ type: 'SET_CLOSEST_STOP', closestStop: newClosest });
-=======
-
 			if (newClosest) {
 				yield put({ type: 'SET_CLOSEST_STOP', closestStop: newClosest });
 			}
->>>>>>> v5.1-hotfix
 			yield call(resetScroll);
 		}
 	} catch (error) {
@@ -113,11 +79,7 @@ function* orderStops(action) {
 
 function doOrder(newOrder) {
 	const newStops = [];
-<<<<<<< HEAD
-	let closestStop;
-=======
 	let closestStop = null;
->>>>>>> v5.1-hotfix
 
 	for (let i = 0; i < newOrder.length; ++i) {
 		if (newOrder[i].closest) {
@@ -140,18 +102,11 @@ function* setScroll(action) {
 }
 
 function* fetchArrival(stopID) {
-<<<<<<< HEAD
-	try {
-		const shuttle = yield select(getShuttle);
-		const stops = Object.assign({}, shuttle.stops);
-		const arrivals = yield call(ShuttleService.FetchShuttleArrivalsByStop, stopID);
-=======
 	const shuttle = yield select(getShuttle);
 	const stops = Object.assign({}, shuttle.stops);
 
 	try {
 		const arrivals = yield call(fetchShuttleArrivalsByStop, stopID);
->>>>>>> v5.1-hotfix
 
 		// Sort arrivals, should be on lambda?
 		arrivals.sort((a, b) => {
@@ -178,21 +133,12 @@ function* fetchArrivalMan(action) {
 
 function* watchArrivals() {
 	while (true) {
-<<<<<<< HEAD
-		const { closestStop } = yield select(getShuttle);
-		// Fetch arrivals for all saved stops
-		/*for (let i = 0; i < savedStops.length; ++i) {
-			const stopID = savedStops[i].id;
-			yield call(fetchArrival, stopID);
-		}*/
-=======
 		const { savedStops, closestStop } = yield select(getShuttle);
 		// Fetch arrivals for all saved stops
 		for (let i = 0; i < savedStops.length; ++i) {
 			const stopID = savedStops[i].id;
 			yield call(fetchArrival, stopID);
 		}
->>>>>>> v5.1-hotfix
 		if (closestStop) {
 			yield call(fetchArrival, closestStop.id); // Fetch arrival for closest stop
 		}
@@ -201,19 +147,11 @@ function* watchArrivals() {
 }
 
 function* shuttleSaga() {
-<<<<<<< HEAD
-	/*yield takeLatest('ADD_STOP', addStop);
-	yield takeLatest('REMOVE_STOP', removeStop);
-	yield takeLatest('ORDER_STOPS', orderStops);
-	yield takeLatest('FETCH_ARRIVAL', fetchArrivalMan);
-	yield takeLatest('UPDATE_SHUTTLE_SCROLL', setScroll);*/
-=======
 	yield takeLatest('ADD_STOP', addStop);
 	yield takeLatest('REMOVE_STOP', removeStop);
 	yield takeLatest('ORDER_STOPS', orderStops);
 	yield takeLatest('FETCH_ARRIVAL', fetchArrivalMan);
 	yield takeLatest('UPDATE_SHUTTLE_SCROLL', setScroll);
->>>>>>> v5.1-hotfix
 	yield call(watchArrivals);
 }
 
