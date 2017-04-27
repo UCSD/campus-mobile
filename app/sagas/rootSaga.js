a7711a24eeda819cf3fc9fd20f31b16f05cd8754
Charles <c3bryant@ucsd.edu>
2017-04-27 10:59:12 -0700

    Resolve rootSaga

--- app/sagas/rootSaga.js
+++ app/sagas/rootSaga.js
@@ -1,29 +1,21 @@
 import { fork } from 'redux-saga/effects';
 import surveySaga from './surveySaga';
-<<<<<<< HEAD
 import cardSaga from './cardSaga';
 import locationSaga from './locationSaga';
 import shuttleSaga from './shuttleSaga';
 import diningSaga from './diningSaga';
 import dataSaga from './dataSaga';
-=======
-import shuttleSaga from './shuttleSaga';
 import homeSaga from './homeSaga';
->>>>>>> v5.1-hotfix
 
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
 	yield [
 		fork(surveySaga),
-<<<<<<< HEAD
 		fork(cardSaga),
 		fork(locationSaga),
 		fork(shuttleSaga),
 		fork(diningSaga),
-		fork(dataSaga)
-=======
-		fork(shuttleSaga),
+		fork(dataSaga),
 		fork(homeSaga),
->>>>>>> v5.1-hotfix
 	];
 }
