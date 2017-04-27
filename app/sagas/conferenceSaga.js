f1bb5e69c71573cbf1acbbc22d513a051a4a3026
Ivan Wu <i8wu@ucsd.edu>
2017-04-27 14:56:35 -0700

    Fix saving conference to maintain time ordering

--- app/sagas/conferenceSaga.js
+++ app/sagas/conferenceSaga.js
@@ -4,19 +4,25 @@ const getConference = (state) => (state.conference);
 
 function* addConference(action) {
 	const conference = yield select(getConference);
-	const saved = [...conference.saved.slice()]; // copy array
+	const saved = conference.saved.slice(); // copy array
+	const schedule = conference.data;
 	let contains = false;
+	let addIndex = 0;
 	// Make sure stop hasn't already been saved
 	for (let i = 0;  i < saved.length; ++i) {
 		if (saved[i] === action.id) {
 			contains = true;
 			break;
 		}
+		// figure out where to insert with respect to start time
+		if (schedule[action.id]['time-start'] > schedule[saved[i]]['time-start']) {
+			addIndex = i + 1;
+		}
 	}
 	if (!contains) {
 		yield put({ type: 'CHANGED_CONFERENCE_SAVED',
-			saved: [...saved, action.id] });
-	} // TODO: sort by time
+			saved: [...saved.slice(0, addIndex), action.id, ...saved.slice(addIndex)] });
+	}
 }
 
 function* removeConference(action) {
