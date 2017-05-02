07fdc2f61acdc87bdb51db504394940943edd9ad
Charles <c3bryant@ucsd.edu>
2017-05-02 11:44:46 -0700

    Use star border for unfavorited sessions

--- app/views/conference/ConferenceListView.js
+++ app/views/conference/ConferenceListView.js
@@ -148,9 +148,9 @@ const ConferenceItem = ({ conferenceData, saved, add, remove, disabled }) => (
 				}
 			>
 				<Icon
-					name="star"
+					name={ saved ? 'star' : 'star-border' }
 					size={28}
-					color={(saved) ? 'yellow' : 'gray'}
+					color={ saved ? 'yellow' : 'grey' }
 				/>
 			</TouchableOpacity>
 		) }
