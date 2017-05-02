868b3592f5676c2a5e2d51e388f9d285b4f0135c
Ivan Wu <i8wu@ucsd.edu>
2017-05-02 10:03:18 -0700

    Added flex to card header text

--- app/views/card/CardHeader.js
+++ app/views/card/CardHeader.js
@@ -21,7 +21,7 @@ const CardHeader = ({ title, menu, image }) => (
 
 const styles = StyleSheet.create({
 	header_container: { flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: '#DDD', alignSelf: 'stretch' },
-	header_text: { fontSize: 26, color: '#747678', paddingLeft: 10, paddingVertical: 6 },
+	header_text: { flex: 1, fontSize: 26, color: '#747678', paddingLeft: 10, paddingVertical: 6 },
 	header_image: { flex: 1, height: 50 }
 });
 
