06dba92f39f8edadbddaa092c6708086d579a529
Ivan Wu <i8wu@ucsd.edu>
2017-05-19 14:53:50 -0700

    Change class to const

--- app/views/preferences/PreferencesView.js
+++ app/views/preferences/PreferencesView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
 	View,
 } from 'react-native';
@@ -8,13 +8,10 @@ import CardPreferences from './CardPreferences';
 import css from '../../styles/css';
 
 // View for user to manage preferences, including which cards are visible
-export default class PreferencesView extends Component {
-	render() {
-		return (
-			<View style={css.main_container}>
-				<UserAccount />
-				<CardPreferences />
-			</View>
-		);
-	}
-}
+const PreferencesView = () => (
+	<View style={css.main_container}>
+		<UserAccount />
+		<CardPreferences />
+	</View>
+);
+export default PreferencesView;
