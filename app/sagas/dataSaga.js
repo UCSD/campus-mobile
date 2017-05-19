842fe377f8128f3bb5d3e79083917a0b01da1c3d
Ivan Wu <i8wu@ucsd.edu>
2017-05-19 12:39:10 -0700

    Use saga for events

--- app/sagas/dataSaga.js
+++ app/sagas/dataSaga.js
@@ -6,11 +6,13 @@ import WeatherService from '../services/weatherService';
 import { fetchConference } from '../services/conferenceService';
 import { fetchSurveyIds, fetchSurveyById } from '../services/surveyService';
 import LinksService from '../services/quicklinksService';
+import EventService from '../services/eventService';
 import {
 	WEATHER_API_TTL,
 	SURF_API_TTL,
 	CONFERENCE_TTL,
-	QUICKLINKS_API_TTL
+	QUICKLINKS_API_TTL,
+	EVENTS_API_TTL
 } from '../AppSettings';
 
 const getWeather = (state) => (state.weather);
@@ -18,6 +20,7 @@ const getSurf = (state) => (state.surf);
 const getConference = (state) => (state.conference);
 const getLinks = (state) => (state.links);
 const getSurvey = (state) => (state.survey);
+const getEvents = (state) => (state.events);
 
 function* watchData() {
 	while (true) {
@@ -26,6 +29,7 @@ function* watchData() {
 			yield call(updateSurf);
 			yield call(updateConference);
 			yield call(updateLinks);
+			yield call(updateEvents);
 			yield call(updateSurveys);
 			yield put({ type: 'UPDATE_DINING' });
 		} catch (err) {
@@ -98,6 +102,21 @@ function* updateLinks() {
 		if (links) {
 			prefetchLinkImages(links);
 		}
+	}
+}
+
+function* updateEvents() {
+	const { lastUpdated, data } = yield select(getEvents);
+	const nowTime = new Date().getTime();
+	const timeDiff = nowTime - lastUpdated;
+	const ttl = EVENTS_API_TTL * 1000;
+
+	if (timeDiff < ttl && data) {
+		// Do nothing, no need to fetch new data
+	} else {
+		// Fetch for new data
+		const events = yield call(EventService.FetchEvents);
+		yield put({ type: 'SET_EVENTS', events });
 	}
 }
 
--- app/views/events/EventCardContainer.js
+++ app/views/events/EventCardContainer.js
@@ -1,53 +1,31 @@
 import React from 'react';
-import {
-	AppState,
-} from 'react-native';
 
 import { connect } from 'react-redux';
 import moment from 'moment';
 
-import CardComponent from '../card/CardComponent';
-import { updateEvents } from '../../actions/events';
 import DataListCard from '../common/DataListCard';
 import logger from '../../util/logger';
 import { militaryToAMPM } from '../../util/general';
 
-class EventCardContainer extends CardComponent {
-	componentDidMount() {
-		logger.ga('Card Mounted: Events');
+const EventCardContainer = ({ eventsData }) => {
+	logger.ga('Card Mounted: Events');
 
-		this.props.updateEvents();
-		AppState.addEventListener('change', this._handleAppStateChange);
+	if (eventsData) {
+		eventsData.forEach((element) => {
+			element.subtext = moment(element.eventdate).format('MMM Do') + ', ' + militaryToAMPM(element.starttime) + ' - ' + militaryToAMPM(element.endtime);
+			element.image = element.imagethumb;
+		});
 	}
 
-	componentWillUnmount() {
-		AppState.removeEventListener('change', this._handleAppStateChange);
-	}
-
-	_handleAppStateChange = (currentAppState) => {
-		this.setState({ currentAppState });
-		this.props.updateEvents();
-	}
-
-	render() {
-		const { eventsData } = this.props;
-		if (eventsData) {
-			eventsData.forEach((element) => {
-				element.subtext = moment(element.eventdate).format('MMM Do') + ', ' + militaryToAMPM(element.starttime) + ' - ' + militaryToAMPM(element.endtime);
-				element.image = element.imagethumb;
-			});
-		}
-
-		return (
-			<DataListCard
-				id="events"
-				title="Events"
-				data={eventsData}
-				item={'EventItem'}
-			/>
-		);
-	}
-}
+	return (
+		<DataListCard
+			id="events"
+			title="Events"
+			data={eventsData}
+			item={'EventItem'}
+		/>
+	);
+};
 
 const mapStateToProps = (state) => (
 	{
@@ -55,17 +33,8 @@ const mapStateToProps = (state) => (
 	}
 );
 
-const mapDispatchToProps = (dispatch) => (
-	{
-		updateEvents: () => {
-			dispatch(updateEvents());
-		}
-	}
-);
-
 const ActualEventCard = connect(
-	mapStateToProps,
-	mapDispatchToProps
+	mapStateToProps
 )(EventCardContainer);
 
 export default ActualEventCard;
