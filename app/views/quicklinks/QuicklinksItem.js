b71a001654683558e4bcd079e4aed24d270b3d38
Ivan Wu <i8wu@ucsd.edu>
2017-05-22 10:25:14 -0700

    User ColorConstants for quicklinks

--- app/views/quicklinks/QuicklinksItem.js
+++ app/views/quicklinks/QuicklinksItem.js
@@ -2,39 +2,55 @@ import React from 'react';
 import {
 	View,
 	Text,
-	TouchableHighlight,
 	Image,
+	StyleSheet
 } from 'react-native';
-
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const css = require('../../styles/css');
-const general = require('../../util/general');
+import {
+	COLOR_LGREY,
+	COLOR_MGREY,
+	COLOR_DGREY,
+	COLOR_PRIMARY,
+	COLOR_BLACK
+} from '../../styles/ColorConstants';
+import Touchable from '../common/Touchable';
+import { openURL } from '../../util/general';
 
-const ListStyle = function (listType) {
-	if (listType === 'full') {
-		return css.links_row_full;
-	} else {
-		return null;
-	}
-};
-
-const QuicklinksItem = ({ data, listType }) => (
-	<View style={css.quicklinks_row_container}>
+const QuicklinksItem = ({ data }) => (
+	<View style={styles.quicklinks_row_container}>
 		{(data.name && data.url && data.icon) ? (
-			<TouchableHighlight underlayColor="rgba(100,100,100,.1)" onPress={() => general.openURL(data.url)}>
-				<View style={[css.quicklinks_row, ListStyle(listType)]}>
+			<Touchable onPress={() => openURL(data.url)}>
+				<View style={styles.quicklinks_row}>
 					{data.icon.indexOf('fontawesome:') === 0 ? (
-						<Icon name={data.icon.replace('fontawesome:','')} size={26} color={'#014365'} style={css.quicklinks_icon_fa} />
+						<Icon
+							name={data.icon.replace('fontawesome:','')}
+							size={26}
+							style={styles.quicklinks_icon_fa}
+						/>
 					) : (
-						<Image style={css.quicklinks_icon} source={{ uri: data.icon }} />
+						<Image style={styles.quicklinks_icon} source={{ uri: data.icon }} />
 					)}
-					<Text style={css.quicklinks_name}>{data.name}</Text>
-					<Icon name={'chevron-right'} size={20} color={'rgba(116,118,120,.6)'} />
+					<Text style={styles.quicklinks_name}>{data.name}</Text>
+					<Icon
+						name={'chevron-right'}
+						size={20}
+						style={styles.arrowIcon}
+					/>
 				</View>
-			</TouchableHighlight>
+			</Touchable>
 		) : (null)}
 	</View>
 );
+
+const styles = StyleSheet.create({
+	links_row_full: { paddingHorizontal: 12 },
+	quicklinks_row_container: { borderBottomWidth: 1, backgroundColor: COLOR_LGREY, borderBottomColor: COLOR_MGREY, paddingVertical: 10 },
+	quicklinks_row: { flexDirection: 'row', alignItems: 'center', justifyContent: 'center' },
+	quicklinks_icon: { height: 42, width: 38 },
+	quicklinks_icon_fa: { padding: 8, color: COLOR_PRIMARY },
+	quicklinks_name: { flexGrow: 5, color: COLOR_BLACK, fontSize: 16, paddingHorizontal: 8 },
+	arrowIcon: { color: COLOR_DGREY },
+});
 
 export default QuicklinksItem;
