a2167eadd4f53b8bc349127e1d3bfa3c01052dfd
Ivan Wu <i8wu@ucsd.edu>
2017-06-26 12:25:02 -0700

    Style LoginView

--- app/views/preferences/LoginView.js
+++ app/views/preferences/LoginView.js
@@ -4,7 +4,9 @@ import {
 	Text,
 	StyleSheet,
 	TextInput,
+	View,
 } from 'react-native';
+import { connect } from 'react-redux';
 
 import {
 	COLOR_MGREY,
@@ -12,51 +14,97 @@ import {
 	COLOR_PRIMARY,
 } from '../../styles/ColorConstants';
 import {
-	MARGIN_TOP
+	MARGIN_TOP,
+	MAX_CARD_WIDTH,
 } from '../../styles/LayoutConstants';
 import Card from '../card/Card';
 import Touchable from '../common/Touchable';
 
-const LoginView = () => (
+const LoginView = ({ isLoggedIn }) => (
 	<ScrollView
 		style={styles.mainContainer}
 	>
 		<Card
-			title="Login"
+			title="Login with SSO"
 			hideMenu
 		>
-			<TextInput
-				style={styles.textInput}
-				autofocus
-				autoCorrect={false}
-				placeholder="Username"
-				onSubmitEditing={() => this._passwordInput.focus()}
-			/>
-			<TextInput
-				ref={(c) => { this._passwordInput = c; }}
-				style={styles.textInput}
-				placeholder="Password"
-				secureTextEntry
-			/>
-			<Touchable
-				onPress={() => console.log('whatever')}
-				style={styles.button}
-			>
-				<Text
-					style={styles.buttonText}
-				>
-					Login
-				</Text>
-			</Touchable>
+			{
+				(isLoggedIn) ? (
+					<View
+						style={styles.contentContainer}
+					>
+						<Text>
+							MyUsername
+						</Text>
+						<Touchable
+							onPress={() => console.log('whatever')}
+							style={styles.button}
+						>
+							<Text
+								style={styles.buttonText}
+							>
+								Logout
+							</Text>
+						</Touchable>
+					</View>
+					) : (
+						<View
+							style={styles.contentContainer}
+						>
+							<TextInput
+								style={styles.textInput}
+								autofocus
+								autoCorrect={false}
+								placeholder="Username"
+								onSubmitEditing={() => this._passwordInput.focus()}
+							/>
+							<TextInput
+								ref={(c) => { this._passwordInput = c; }}
+								style={styles.textInput}
+								placeholder="Password"
+								secureTextEntry
+							/>
+							<Touchable
+								onPress={() => console.log('whatever')}
+								style={styles.forgotButton}
+							>
+								<Text
+									style={styles.forgotText}
+								>
+									Forgot password?
+								</Text>
+							</Touchable>
+							<Touchable
+								onPress={() => console.log('whatever')}
+								style={styles.loginButton}
+							>
+								<Text
+									style={styles.loginText}
+								>
+									Login
+								</Text>
+							</Touchable>
+						</View>
+				)
+			}
 		</Card>
 	</ScrollView>
 );
 
+function mapStateToProps(state) {
+	return {
+		isLoggedIn: state.user.isLoggedIn
+	};
+}
+
 const styles = StyleSheet.create({
 	mainContainer: { flexGrow: 1, backgroundColor: COLOR_MGREY, marginTop: MARGIN_TOP },
-	textInput: { height: 50 },
-	button: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: COLOR_PRIMARY, borderRadius: 3, padding: 10, margin: 8 },
-	buttonText: { fontSize: 16, color: COLOR_WHITE },
+	contentContainer: { flex: 1, margin: 8 },
+	textInput: { height: 50, width: MAX_CARD_WIDTH - 16 },
+	forgotButton: { flex: 1, justifyContent: 'center', alignItems: 'center', borderRadius: 3, padding: 10, },
+	loginButton: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: COLOR_PRIMARY, borderRadius: 3, padding: 10, },
+	forgotText: { fontSize: 16, color: COLOR_PRIMARY },
+	loginText: { fontSize: 16, color: COLOR_WHITE },
 });
 
-export default LoginView;
+export default connect(mapStateToProps)(LoginView);
