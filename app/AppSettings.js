f1c870e6d77d6299691791592ee69460e14c00d9
Bryant, Charles <c3bryant@ucsd.edu>
2017-07-14 09:36:44 -0700

    Switch conference api to special events api

--- app/AppSettings.js
+++ app/AppSettings.js
@@ -14,7 +14,6 @@ module.exports = {
 	QUICKLINKS_API_URL: 'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/quick_links/ucsd-quicklinks-v3.json',
 	NEWS_API_URL: 'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/allstories.json',
 	MAP_SEARCH_API_URL: 'https://xgu9qa7gx4.execute-api.us-west-2.amazonaws.com/prod/v2/map/search?region=0&query=',
-	CAMPUS_LISA_URL: 'https://95dscnn6od.execute-api.us-west-2.amazonaws.com/prod/v1/schedule',
 	SPECIAL_EVENT_API_URL: 'https://2wxnokqsz2.execute-api.us-west-2.amazonaws.com/dev/GetActiveSpecialEvent',
 
 	/* RESOURCES */
