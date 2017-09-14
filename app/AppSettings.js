5e9eee7bf73e0e3173955d0f5070ae05d87825ae
Bryant, Charles <c3bryant@ucsd.edu>
2017-09-14 14:10:46 -0700

    Update final special events url

--- app/AppSettings.js
+++ app/AppSettings.js
@@ -14,7 +14,7 @@ module.exports = {
 	QUICKLINKS_API_URL: 'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/quick_links/ucsd-quicklinks-v3.json',
 	NEWS_API_URL: 'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/allstories.json',
 	MAP_SEARCH_API_URL: 'https://xgu9qa7gx4.execute-api.us-west-2.amazonaws.com/prod/v2/map/search?region=0&query=',
-	SPECIAL_EVENT_API_URL: 'https://s3-us-west-2.amazonaws.com/ucsd-mobile/dev/feeds/multiefiltersampleProcessed.json',
+	SPECIAL_EVENT_API_URL: 'https://2jjml3hf27.execute-api.us-west-2.amazonaws.com/prod/events/special',
 
 	/* RESOURCES */
 	SHUTTLE_SCHEDULE_URL: 'https://transportation.ucsd.edu/shuttles/',
