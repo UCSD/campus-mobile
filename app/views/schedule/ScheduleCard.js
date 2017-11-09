d0a41c42a1424c29a89d5630f9710fefecd194ce
Bryant, Charles <c3bryant@ucsd.edu>
2017-11-09 11:11:17 -0800

    Update to class schedule card

--- app/views/schedule/ScheduleCard.js
+++ app/views/schedule/ScheduleCard.js
@@ -2,31 +2,23 @@ import React from 'react';
 import {
 	View,
 	Text,
-	StyleSheet,
 	ListView
 } from 'react-native';
-
 import { getData } from './scheduleData';
 import ScrollCard from '../card/ScrollCard';
 import logger from '../../util/logger';
-import {
-	MAX_CARD_WIDTH
-} from '../../styles/LayoutConstants';
-import {
-	COLOR_LGREY,
-	COLOR_DGREY
-} from '../../styles/ColorConstants';
+import css from '../../styles/css';
 
-const scheduleData = getData();
-const daytaSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+var scheduleData = getData();
+var dataSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
-const ScheduleCard = () => {
-	logger.ga('Card Mounted: Schedule');
+var ScheduleCard = () => {
+	logger.ga('Card Mounted: Class Schedule');
 
 	return (
 		<ScrollCard
-			id="Schedule"
-			title="Schedule"
+			id='schedule'
+			title='Class Schedule'
 			scrollData={scheduleData}
 			renderRow={
 				(rowData, sectionID, rowID, highlightRow) => (
@@ -45,13 +37,9 @@ const ScheduleCard = () => {
 	);
 };
 
-const ScheduleDay = ({ id, data }) => (
-	<View
-		style={styles.dayContainer}
-	>
-		<Text
-			style={styles.dayText}
-		>
+var ScheduleDay = ({ id, data }) => (
+	<View style={css.sc_dayContainer}>
+		<Text style={css.sc_dayText}>
 			{id}
 		</Text>
 		<DayList
@@ -60,52 +48,27 @@ const ScheduleDay = ({ id, data }) => (
 	</View>
 );
 
-const DayList = ({ courseItems }) => (
+var DayList = ({ courseItems }) => (
 	<ListView
-		dataSource={daytaSource.cloneWithRows(courseItems)}
+		dataSource={dataSource.cloneWithRows(courseItems)}
 		renderRow={(rowData, sectionID, rowID, highlightRow) => (
-			<DayItem
-				key={rowID}
-				data={rowData}
-			/>
+			<DayItem key={rowID} data={rowData} />
 		)}
 		renderSeparator={(sectionID, rowID, adjacentRowHighlighted) => (
-			<DaySpacer
-				key={'spacer' + rowID}
-			/>
+			<DaySpacer key={'spacer' + rowID} />
 		)}
 	/>
 );
 
-/*
-	{
-		building: currData.building,
-		room: currData.room,
-		instructor_name: currData.instructor_name,
-		section: currData.section,
-		subject_code: currCourse.subject_code,
-		course_code: currCourse.course_code,
-		course_title: currCourse.course_title,
-		time_string,
-		start_time: startSeconds,
-		end_time: endSeconds,
-		meeting_type: currData.meeting_type,
-		special_mtg_code
-	}
- */
-const DayItem = ({ data }) => (
-	<View
-		style={styles.dayRow}
-	>
+var DayItem = ({ data }) => (
+	<View style={css.sc_dayRow}>
 		<Text
-			style={styles.courseText}
+			style={css.sc_courseText}
 			numberOfLines={1}
 		>
 			{data.course_title}
 		</Text>
-		<Text
-			style={styles.subText}
-		>
+		<Text style={css.sc_subText}>
 			{data.meeting_type + ' ' + data.time_string + '\n'}
 			{data.instructor_name + '\n'}
 			{data.building + data.room}
@@ -113,19 +76,8 @@ const DayItem = ({ data }) => (
 	</View>
 );
 
-const DaySpacer = () => (
-	<View
-		style={styles.spacer}
-	/>
+var DaySpacer = () => (
+	<View style={css.sc_spacer} />
 );
-
-const styles = StyleSheet.create({
-	spacer: { height: 1, width: MAX_CARD_WIDTH, backgroundColor: COLOR_LGREY },
-	dayText: { fontSize: 18, color: COLOR_DGREY },
-	courseText: { fontSize: 18, color: COLOR_DGREY },
-	subText: { fontSize: 13, color: COLOR_DGREY, opacity: 0.69 },
-	dayContainer: { width: MAX_CARD_WIDTH + 2, padding: 7 },
-	dayRow: { height: 70, justifyContent: 'center' },
-});
 
 export default ScheduleCard;
