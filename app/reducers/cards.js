c62f7b953842cc1899e8e3c5ae3e66884d4b3559
Bryant, Charles <c3bryant@ucsd.edu>
2017-11-09 10:07:32 -0800

    Add class schedule card reducer

--- app/reducers/cards.js
+++ app/reducers/cards.js
@@ -1,6 +1,7 @@
 const initialState = {
 	cards: {
 		specialEvents: 	{ id: 'specialEvents', active: false, autoActivated: false, name: 'Special Events', component: 'SpecialEventsCard' },
+		schedule: 		{ id: 'schedule', active: true, name: 'Schedule', component: 'ScheduleCard' },
 		weather: 		{ id: 'weather', active: true, name: 'Weather', component: 'WeatherCard' },
 		shuttle: 		{ id: 'shuttle', active: true, name: 'Shuttle', component: 'ShuttleCard' },
 		dining: 		{ id: 'dining', active: true, name: 'Dining', component: 'DiningCard' },
@@ -8,53 +9,54 @@ const initialState = {
 		quicklinks: 	{ id: 'quicklinks', active: true, name: 'Links', component: 'QuicklinksCard' },
 		news: 			{ id: 'news', active: true, name: 'News', component: 'NewsCard' },
 	},
-	cardOrder: ['specialEvents', 'weather', 'shuttle', 'dining', 'events', 'quicklinks', 'news'],
+	cardOrder: ['specialEvents', 'schedule', 'weather', 'shuttle', 'dining', 'events', 'quicklinks', 'news'],
 };
 
 function cards(state = initialState, action) {
-	const newState = { ...state };
+	var newState = { ...state };
 
 	switch (action.type) {
-	case 'SET_CARD_STATE':
-		return {
-			...state,
-			cards: {
-				...state.cards,
-				[action.id]: { ...state.cards[action.id], active: action.active }
+		case 'SET_CARD_STATE':
+			return {
+				...state,
+				cards: {
+					...state.cards,
+					[action.id]: { ...state.cards[action.id], active: action.active }
+				}
+			};
+		case 'SET_AUTOACTIVATED_STATE':
+			newState.cards[action.id] = Object.assign({}, newState.cards[action.id], { autoActivated: action.autoActivated });
+
+			return newState;
+		case 'SHOW_CARD': {
+			if (newState.cards[action.id] && !newState.cards[action.id].active) {
+				return {
+					...state,
+					cards: {
+						...state.cards,
+						[action.id]: { ...state.cards[action.id], active: true }
+					}
+				};
 			}
-		};
-	case 'SET_AUTOACTIVATED_STATE':
-		newState.cards[action.id] = Object.assign({}, newState.cards[action.id], { autoActivated: action.autoActivated });
-
-		return newState;
-	case 'SHOW_CARD': {
-		if (newState.cards[action.id] && !newState.cards[action.id].active) {
-			return {
-				...state,
-				cards: {
-					...state.cards,
-					[action.id]: { ...state.cards[action.id], active: true }
-				}
-			};
+			return newState;
 		}
-		return newState;
-	}
-	case 'HIDE_CARD':
-		if (newState.cards[action.id] && newState.cards[action.id].active) {
-			return {
-				...state,
-				cards: {
-					...state.cards,
-					[action.id]: { ...state.cards[action.id], active: false }
-				}
-			};
-		}
-		return newState;
-	case 'SET_CARD_ORDER':
-		newState.cardOrder = action.cardOrder.slice();
+		case 'HIDE_CARD':
+			if (newState.cards[action.id] && newState.cards[action.id].active) {
+				return {
+					...state,
+					cards: {
+						...state.cards,
+						[action.id]: { ...state.cards[action.id], active: false }
+					}
+				};
+			}
+			return newState;
+		case 'SET_CARD_ORDER':
+			newState.cardOrder = action.cardOrder.slice();
 
-		return newState;
+			return newState;
 	}
+
 	return state;
 }
 
