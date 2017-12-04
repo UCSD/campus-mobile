3d26b1fed0bf51177989279785153bc499d7b7f1
Vivian Pham <vvp002@ucsd.edu>
2017-12-04 10:48:46 -0800

    Reformat Surf Report w/1 col list and date moved to top

--- app/views/weather/SurfReport.js
+++ app/views/weather/SurfReport.js
@@ -14,6 +14,29 @@ import logger from '../../util/logger';
 var surfHeader = require('../../assets/img/surf_report_header.jpg');
 var surfDataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 var forecastParsed = [];
+var mapWeekdays = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday'
+]
+var mapMonths = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December'
+]
 
 class SurfReport extends React.Component {
 	componentDidMount() {
@@ -21,6 +44,7 @@ class SurfReport extends React.Component {
 	}
 
 	render() {
+		var dateString = new Date(this.props.surfData.spots[0].date)
 		
 		if ("forecast" in this.props.surfData && this.props.surfData.forecast.length !== 0) {
 			this.formatForecast(this.props.surfData.forecast);
@@ -30,7 +54,14 @@ class SurfReport extends React.Component {
 				<Image style={css.sr_headerImage} source={surfHeader} />
 
 				<View style={css.dateDayContainer}>
-					<View style={css.dateDayHeader}> 
+					<View style={css.dateDayHeader}>
+						<Text style={css.sr_reportTitle}>
+							{mapWeekdays[dateString.getDay()]}
+							{', '}
+							{mapMonths[dateString.getMonth()]} 
+							{' '}
+							{dateString.getDate()}
+						</Text> 
 						{forecastParsed}
 					</View>
 				</View>
@@ -38,18 +69,6 @@ class SurfReport extends React.Component {
 				<ListView
 					dataSource={surfDataSource.cloneWithRows(this.props.surfData.spots)}
 					renderRow={rowData => this.surfItem(rowData)}
-					renderSectionHeader={(sectionData) => {
-						var dateString = new Date(sectionData[0]['date']).toDateString();
-						return (
-							<View style={css.sr_dateDayContainer}>
-								<View style={css.sr_dateDayHeader}>
-									<Text style={css.sr_dateDayOfWeek}>{dateString.substring(4,10)}</Text>
-									<Text style={css.sr_dateDayAndMonth}>{dateString.substring(0,3)}</Text>
-								</View>
-								<View style={css.sr_dateDayEmpty} />
-							</View>
-						);
-					}}
 				/>
 			</ScrollView>
 		);
@@ -59,7 +78,6 @@ class SurfReport extends React.Component {
 	surfItem(rowData) {
 		return (
 			<View style={css.sr_surfDetailsContainer}>
-				<View style={css.sr_surfDetailsEmpty} />
 				<View style={css.sr_surfDetails}>
 					<Text style={css.sr_titleText}>{rowData.title}</Text>
 					<Text style={css.sr_heightText}>{rowData.surf_min}{'-'}{rowData.surf_max}{'ft'}</Text>
@@ -73,11 +91,11 @@ class SurfReport extends React.Component {
 		var reports = forecast.map(function(rep){
 			forecast.indexOf(rep)%2==0 ? forecastParsed.push(
 				<View style={css.dateDayHeader}> 
-					<Text style={css.sr_reportTitle}>{rep.replace(/&nbsp;/g,'')}</Text>
+					<Text style={css.sr_reportTitle}>{rep}</Text>
 				</View>
 			) : forecastParsed.push(
 				<View style={css.dateDayHeader}> 
-					<Text style={css.sr_reportText}>{rep.replace(/&nbsp;/g,'')}</Text>
+					<Text style={css.sr_reportText}>{rep}</Text>
 				</View>);
 		})}
 }
