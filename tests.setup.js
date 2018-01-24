3d7d23cc51c298c87bf2079ab664b8a1aea3be47
Mario Muniz <mcmuniz@ucsd.edu>
2018-01-24 09:39:29 -0800

    Add tests for Home, NearbyMapView, and Preferences views

--- tests.setup.js
+++ tests.setup.js
@@ -2,5 +2,21 @@
 import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
 
 // Mock native modules here
-jest.mock('react-native-simple-toast', () => 'Toast');
+
+// Mock Google analytics
+// used by various components
 jest.mock('react-native-google-analytics-bridge');
+
+// Mock react-native-simple-toast
+// used by Feedback and NearbyMapView
+jest.mock('react-native-simple-toast', () => 'Toast');
+
+// Mock react-native-experimental navigation Linking
+// used by Home
+jest.mock('Linking', () => ({
+	addEventListener: jest.fn(),
+	removeEventListener: jest.fn(),
+	openURL: jest.fn(),
+	canOpenURL: jest.fn(),
+	getInitialURL: jest.fn(),
+}));
