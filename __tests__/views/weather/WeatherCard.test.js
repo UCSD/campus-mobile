c77e55f87451970e924aaf58d9187fa1cbb310c7
Mario Muniz <mcmuniz@ucsd.edu>
2018-01-25 09:45:12 -0800

    Add tests for weather card; Add test for actual Special Events homescreen card

--- __tests__/views/weather/WeatherCard.test.js
+++ __tests__/views/weather/WeatherCard.test.js
@@ -4,21 +4,21 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15.4';
 
 // Component to be tested
-import { SpecialEventsCardContainer } from '../../../app/views/specialEvents/SpecialEventsCardContainer';
-import { SPECIAL_EVENTS_RESPONSE } from '../../../mockApis/specialEventsApi';
+import WeatherCard from '../../../app/views/weather/WeatherCard';
+import SurfButton from '../../../app/views/weather/SurfButton';
+import { WEATHER_RESPONSE } from '../../../mockApis/weatherApi';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 // Mock props passed down from state
 const initialState = {
-	specialEventsData: SPECIAL_EVENTS_RESPONSE,
-	saved: [],
-	hideCard: jest.fn()
+	weatherData: WEATHER_RESPONSE,
+	actionButton: (<SurfButton />)
 };
 
 // Set up component to be rendered
 function setup(props) {
-	return shallow(<SpecialEventsCardContainer {...props} />);
+	return shallow(<WeatherCard {...props} />);
 }
 
 test('renders without crashing', () => {
--- __tests__/views/weather/WeatherCardContainer.test.js
+++ __tests__/views/weather/WeatherCardContainer.test.js
@@ -4,21 +4,22 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15.4';
 
 // Component to be tested
-import { SpecialEventsCardContainer } from '../../../app/views/specialEvents/SpecialEventsCardContainer';
-import { SPECIAL_EVENTS_RESPONSE } from '../../../mockApis/specialEventsApi';
+import { WeatherCardContainer } from '../../../app/views/weather/WeatherCardContainer';
+import WeatherCard from '../../../app/views/weather/WeatherCard';
+import { SurfButton } from '../../../app/views/weather/SurfButton';
+import { WEATHER_RESPONSE } from '../../../mockApis/weatherApi';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 // Mock props passed down from state
 const initialState = {
-	specialEventsData: SPECIAL_EVENTS_RESPONSE,
-	saved: [],
-	hideCard: jest.fn()
+	weatherData: WEATHER_RESPONSE,
+	actionButton: { SurfButton }
 };
 
 // Set up component to be rendered
 function setup(props) {
-	return shallow(<SpecialEventsCardContainer {...props} />);
+	return shallow(<WeatherCardContainer {...props} />);
 }
 
 test('renders without crashing', () => {
