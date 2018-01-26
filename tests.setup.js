b1df48a7006663ea9a584db95f20cf5a17ac465f
Mario Muniz <mcmuniz@ucsd.edu>
2018-01-26 15:21:08 -0800

    Mock AppSettings manually as a result of differing dev and prod values

--- tests.setup.js
+++ tests.setup.js
@@ -1,6 +1,14 @@
 // Needed to properly mock react-native-google-analytics-bridge
 import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
 
+// Mock AppSettings here explicitly
+// Needed because dev and prod values can differ, which may
+// break certain snapshots under different testing environments
+
+const AppSettingsMock = jest.genMockFromModule('./app/AppSettings');
+AppSettingsMock.APP_NAME = 'Campus Mobile';
+jest.setMock('./app/AppSettings', AppSettingsMock);
+
 // Mock native modules here
 
 // Mock Google analytics
