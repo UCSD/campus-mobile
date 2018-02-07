2ceea302557c5db7097b26b08dd7550a6636425c
Bryant, Charles <c3bryant@ucsd.edu>
2018-02-07 13:10:18 -0800

    Dining API V2 updates

--- app/AppSettings.js
+++ app/AppSettings.js
@@ -9,7 +9,7 @@ module.exports = {
 	SURF_API_URL: 'https://0yi5xwy0v7.execute-api.us-west-2.amazonaws.com/dev',
 	SHUTTLE_STOPS_API_URL: 'https://ies4wyrlx9.execute-api.us-west-2.amazonaws.com/prod/v2/stops/',
 	SHUTTLE_VEHICLES_API_URL: 'https://hjr84cay81.execute-api.us-west-2.amazonaws.com/prod?route=',
-	DINING_API_URL: 'https://pg83tslbyi.execute-api.us-west-2.amazonaws.com/prod/v2/dining/locations',
+	DINING_API_URL: 'http://hdh2.ucsd.edu/diningmenu/api/DiningDataService/GetDiningInfo_V2',
 	EVENTS_API_URL: 'https://2jjml3hf27.execute-api.us-west-2.amazonaws.com/prod/events/student',
 	QUICKLINKS_API_URL: 'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/quick_links/ucsd-quicklinks-v3.json',
 	NEWS_API_URL: 'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/now_ucsandiego/v1/allstories.json',
--- app/views/dining/DiningItem.js
+++ app/views/dining/DiningItem.js
@@ -2,38 +2,61 @@ import React from 'react';
 import {
 	View,
 	Text,
-	TouchableHighlight,
+	StyleSheet,
 } from 'react-native';
 
+import DiningHours from './DiningHours';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Actions } from 'react-native-router-flux';
-import { COLOR_SECONDARY } from '../../styles/ColorConstants';
-const css = require('../../styles/css');
+
+import Touchable from '../common/Touchable';
+import {
+	COLOR_PRIMARY,
+	COLOR_BLACK,
+	COLOR_LGREY,
+	COLOR_MGREY,
+	COLOR_DGREY
+} from '../../styles/ColorConstants';
+
 const general = require('../../util/general');
 
 const DiningItem = ({ data }) => (
-	<View style={css.dc_locations_row}>
-		<TouchableHighlight style={css.dc_locations_row_left} underlayColor={'rgba(200,200,200,.1)'} onPress={() => Actions.DiningDetail({ data })}>
+	<View style={styles.rowContainer}>
+		<Touchable
+			style={styles.rowContainerLeft}
+			onPress={() => Actions.DiningDetail({ data })}
+		>
 			<View>
-				<Text style={css.dc_locations_title}>{data.name}</Text>
-				<Text style={css.dc_locations_hours}>{data.regularHours}</Text>
-				{data.specialHours ? (
-					<Text style={css.dc_locations_spec_hours}>Special Hours:{'\n'}{data.specialHours}</Text>
-				) : null }
+				<Text style={styles.titleText}>{data.name}</Text>
+				<DiningHours
+					regularHours={data.regularHours}
+					specialHours={data.specialHours}
+				/>
 			</View>
-		</TouchableHighlight>
+		</Touchable>
 
 		{data.coords.lat !== 0 ? (
-			<TouchableHighlight style={css.dc_locations_row_right} underlayColor={'rgba(200,200,200,.1)'} onPress={() => general.gotoNavigationApp(data.coords.lat, data.coords.lon)}>
-				<View style={css.dl_dir_traveltype_container}>
-					<Icon name="md-walk" size={32} color={COLOR_SECONDARY} />
+			<Touchable
+				style={styles.rowContainerRight}
+				onPress={() => general.gotoNavigationApp(data.coords.lat, data.coords.lon)}
+			>
+				<View style={styles.dl_dir_traveltype_container}>
+					<Icon name="md-walk" size={32} color={COLOR_PRIMARY} />
 					{data.distanceMilesStr ? (
-						<Text style={css.dl_dir_eta}>{data.distanceMilesStr}</Text>
+						<Text style={styles.dl_dir_eta}>{data.distanceMilesStr}</Text>
 					) : null }
 				</View>
-			</TouchableHighlight>
+			</Touchable>
 		) : null }
 	</View>
 );
+
+const styles = StyleSheet.create({
+	rowContainer: { backgroundColor: COLOR_LGREY, flexDirection: 'row', padding: 10, borderBottomWidth: 1, borderBottomColor: COLOR_MGREY },
+	rowContainerLeft: { flex: 6, justifyContent: 'center' },
+	titleText: { fontSize: 20, color: COLOR_PRIMARY },
+	descriptionText: { fontSize: 12, color: COLOR_DGREY, paddingTop: 1 },
+	rowContainerRight: { flex: 1, alignItems: 'flex-end', justifyContent: 'center' },
+});
 
 export default DiningItem;
