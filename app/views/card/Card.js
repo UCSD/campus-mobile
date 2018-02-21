9054235839d9d76d4ed916eeb3459568b54991be
Solomon Liu <slmnliu@gmail.com>
2018-02-21 13:37:42 -0800

    adjusting Card.js to have list structure rendering similar to scrollcard, but broke weather - need to separate into separate .js file

--- app/views/card/Card.js
+++ app/views/card/Card.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {
+	View,
+	Text,
+	ListView,
+	StyleSheet,
+	TouchableHighlight,
+	List,
+	FlatList
+} from 'react-native';
 import { connect } from 'react-redux';
 import ElevatedView from 'react-native-elevated-view';
 
@@ -9,30 +17,64 @@ import {
 import CardHeader from './CardHeader';
 import CardMenu from './CardMenu';
 
-const Card = ({ hideMenu, cardRefresh, id, title, header, hide, children }) => (
-	<ElevatedView
-		style={styles.mainContainer}
-		ref={(i) => { this._card = i; }}
-		elevation={3}
-	>
-		{(title || header) ? (
-			<CardHeader
-				id={id}
-				title={title}
-				menu={
-					<CardMenu
-						hideMenu={hideMenu}
-						cardRefresh={cardRefresh}
-						hideCard={hide}
-						id={id}
+
+class Card extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: {}
+		};
+	}
+
+	render() {
+		let list;
+		if (this.props.data !== {}) {
+			list = (
+				// <ListView
+				// 	ref={c => { this._listview = c; }}
+				// 	style={styles.listStyle}
+				// 	onContentSizeChange={this.countDots}
+				// 	pagingEnabled
+				// 	horizontal
+				// 	showsHorizontalScrollIndicator={false}
+				// 	onScroll={this.handleScroll}
+				// 	scrollEventThrottle={69}
+				// 	dataSource={scrollDataSource.cloneWithRows(this.props.scrollData)}
+				// 	enableEmptySections={true}
+				// 	renderRow={this.props.renderRow}
+				// />
+			<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+				<Text>Coming soon...</Text>
+			</View>
+			);
+		}
+
+		return (
+			<View>
+				<ElevatedView
+					style={styles.mainContainer}
+					ref={(i) => { this._card = i; }}
+					elevation={3}
+				>
+					<CardHeader
+						id={this.props.id}
+						title={this.props.title}
+						menu={
+							<CardMenu
+								hideMenu={this.props.hideMenu}
+								hideCard={this.props.hide}
+								cardRefresh={this.props.cardRefresh}
+								id={this.props.id}
+							/>
+						}
+						image={this.props.header}
 					/>
-				}
-				image={header}
-			/>
-		) : (null)}
-		{children}
-	</ElevatedView>
-);
+					{list}
+				</ElevatedView>
+			</View>
+		);
+	}
+}
 
 const mapDispatchToProps = (dispatch) => (
 	{
