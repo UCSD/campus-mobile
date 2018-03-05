fec46184d013435f81f2debba472cc2876f40db0
Mario Muniz <mcmuniz@ucsd.edu>
2018-03-05 12:28:57 -0800

    Update to react-native 0.50.4; Update tests to latest version of React

--- __tests__/views/shuttle/ShuttleCard.test.js
+++ __tests__/views/shuttle/ShuttleCard.test.js
@@ -1,7 +1,7 @@
 import 'react-native';
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15.4';
+import Adapter from 'enzyme-adapter-react-16';
 
 // Component to be tested
 import { ShuttleCardContainer } from '../../../app/views/shuttle/ShuttleCardContainer';
