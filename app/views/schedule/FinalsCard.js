a564c942850ec72c8e590adabd7b4fec93433127
Jerry Z <namespacestd@Jerrys-MacBook-Pro-3.local>
2018-03-06 15:34:38 -0800

    finals card complete

--- app/views/schedule/FinalsCard.js
+++ app/views/schedule/FinalsCard.js
@@ -10,11 +10,16 @@ import Card from '../card/Card';
 import logger from '../../util/logger';
 import css from '../../styles/css';
 import {
+	COLOR_PRIMARY,
+	COLOR_SECONDARY,
+	COLOR_VDGREY,
 	COLOR_DGREY,
 	COLOR_MGREY,
-	COLOR_PRIMARY,
 	COLOR_LGREY,
-	COLOR_SECONDARY,
+	COLOR_WHITE,
+	COLOR_BLACK,
+	COLOR_DMGREY,
+	COLOR_TRANSPARENT,
 } from '../../styles/ColorConstants';
 import {
 	MAX_CARD_WIDTH,
@@ -22,6 +27,8 @@ import {
 
 var scheduleData = getFinals();
 var dataSource = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+
+console.log(scheduleData);
 
 var FinalsCard = () => {
 	logger.ga('Card Mounted: Finals Schedule');
@@ -46,10 +53,40 @@ var FinalsCard = () => {
 	);
 };
 
+const dayOfWeekIntepreter = (abbr) => {
+	fullString = '';
+	switch(abbr) {
+		case 'MO':
+			fullString = 'Monday';
+			break;
+		case 'TU':
+			fullString = 'Tuesday';
+			break;
+		case 'WE':
+			fullString = 'Wednesday';
+			break;
+		case 'TH':
+			fullString = 'Thursday';
+			break;
+		case 'FR':
+			fullString = 'Friday';
+			break;
+		case 'SA':
+			fullString = 'Saturday';
+			break;
+		case 'SU':
+			fullString = 'Sunday';
+			break;				
+		default:
+			fullString = abbr;
+	}
+	return fullString;
+}
+
 var ScheduleDay = ({ id, data }) => (
-	<View style={css.sc_fullScheduleContainer}>
-		<Text style={css.sc_dayText}>
-			{id}
+	<View style={styles.card_content}>
+		<Text style={styles.day_of_week}>
+			{dayOfWeekIntepreter(id)}
 		</Text>
 		<DayList courseItems={data} />
 	</View> 
@@ -63,29 +100,54 @@ var DayList = ({ courseItems }) => (
 		)}
 	/>
 );
-
+ 
 var DayItem = ({ data }) => (
-	<View style={css.sc_dayRow}>
+	<View style={styles.day_container}>
+		<Text style={styles.course_title}>
+			{data.subject_code} {data.course_code} 
+		</Text>
 		<Text
-			style={css.sc_courseText}
+			style={styles.course_text}
 			numberOfLines={1}
 		>
 			{data.course_title}
 		</Text>
-		<Text style={css.sc_subText}>
-			{data.meeting_type + ' ' + data.time_string + '\n'}
-			{data.instructor_name + '\n'}
-			{data.building + data.room}
+		<Text style={styles.course_text}>
+			{data.time_string + '\n'}
+			{data.building + ' ' + data.room}
 		</Text>
 	</View>
 );
 
 const styles = StyleSheet.create({
-	more: { alignItems: 'center', justifyContent: 'center', padding: 6 },
-	more_label: { fontSize: 20, color: COLOR_PRIMARY, fontWeight: '300' },
-	specialEventsListView: { borderBottomWidth: 1, borderBottomColor: COLOR_MGREY },
-	nextClassContainer: { flexGrow: 1, width: MAX_CARD_WIDTH },
-	contentContainer: { flexShrink: 1, width: MAX_CARD_WIDTH }
+	card_content: {
+		width: MAX_CARD_WIDTH + 2,
+		marginBottom: 10,
+	},
+	day_of_week: {
+		paddingTop: 10,
+		paddingBottom: 5,
+		paddingLeft: 15,
+		fontWeight: 'bold',
+		fontSize: 18,
+		color: COLOR_VDGREY,
+		borderColor: '#EAEAEA', 
+		borderTopWidth:1,
+	},
+	day_container: { 
+		paddingLeft: 15,
+		justifyContent: 'center', 
+		paddingBottom: 5, 
+	},
+	course_title: {
+		fontSize: 16, 
+		fontWeight: 'bold',
+		color: COLOR_VDGREY,
+	},
+	course_text: { 
+		fontSize: 14, 
+		color: COLOR_DGREY, 
+	}
 });
 
 export default FinalsCard;
