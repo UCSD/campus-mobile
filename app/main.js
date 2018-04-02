0a19c26219dcc94835b5dc299b5ac9a1ccb21e8f
Bryant, Charles <c3bryant@ucsd.edu>
2018-04-02 16:14:59 -0700

    Cleanup main

--- app/main.js
+++ app/main.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react';
-import {
-	View,
-	StatusBar
-} from 'react-native';
-
+import { View } from 'react-native';
 import css from './styles/css';
-import general from './util/general';
-
-// PUSH
 import PushNotificationContainer from './containers/pushNotificationContainer';
-
-// ROUTER
 import Router from './navigation/Router';
-
-if (general.platformIOS()) {
-	StatusBar.setBarStyle('light-content');
-} else if (general.platformAndroid()) {
-	StatusBar.setBackgroundColor('#101d32', false);
-}
 
 const Main = () => (
 	<View style={css.flex}>
