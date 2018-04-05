2a02fccb362be0938115698491a06a8eeffa0aeb
Mario Muniz <mcmuniz@ucsd.edu>
2018-04-05 12:29:45 -0700

    Adjust styling of dots to prevent artifacting created by React Native bug

--- app/views/common/ColoredDot.js
+++ app/views/common/ColoredDot.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
 
 const ColoredDot = ({ size, style, color }) => (
 	<View style={style}>
 		<View
 			style={{
-				width: size, height: size, borderRadius: size / 2, borderColor: color, borderWidth: size / 2, zIndex: 2
+				width: size, height: size, borderRadius: size / 2, backgroundColor: color, zIndex: 2
 			}}
 		/>
 	</View>
-);
+)
 
-export default ColoredDot;
+export default ColoredDot
