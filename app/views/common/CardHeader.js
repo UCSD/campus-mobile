0defceb8b7f05c74a79996f223995f71d0d7022d
Mario Muniz <mcmuniz@ucsd.edu>
2018-06-08 10:24:09 -0700

    Resolve Special Events Card not appearing due to not having a title

--- app/views/common/CardHeader.js
+++ app/views/common/CardHeader.js
@@ -3,14 +3,19 @@ import { View, Text } from 'react-native'
 
 import css from '../../styles/css'
 
-const CardHeader = ({ id, title, menu }) => (
-	<View style={css.ch_headerContainer}>
-		<Text style={css.ch_titleText}>
-			{title}
-		</Text>
-		<View style={css.ch_filler} />
-		{menu}
-	</View>
-)
+const CardHeader = ({ id, title, menu }) => {
+	if (!title || !id || !menu) return null
+	else {
+		return (
+			<View style={css.ch_headerContainer}>
+				<Text style={css.ch_titleText}>
+					{title}
+				</Text>
+				<View style={css.ch_filler} />
+				{menu}
+			</View>
+		)
+	}
+}
 
 export default CardHeader
