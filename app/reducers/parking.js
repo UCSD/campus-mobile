c0e0cf590ccfe9c8ebd2161a98a84c56666d0813
Gagandeep Singh <g9singh@ucsd.edu>
2018-09-10 13:53:49 -0700

    Connected reducers/sagas for parking lot data retrieval

--- app/reducers/parking.js
+++ app/reducers/parking.js
@@ -1,4 +1,8 @@
-const initialState = { isChecked: [false, false, false, false, false], count: 0 }
+const initialState = {
+	isChecked: [false, false, false, false, false],
+	count: 0,
+	parkingData: []
+}
 
 function parking(state = initialState, action) {
 	const newState = { ...state }
@@ -6,6 +10,10 @@ function parking(state = initialState, action) {
 		case 'SET_PARKING_TYPE_SELECTION': {
 			newState.isChecked = action.isChecked
 			newState.count = action.count
+			return newState
+		}
+		case 'SET_PARKING_DATA': {
+			newState.parkingData[0] = action.ParkingData
 			return newState
 		}
 		default:
