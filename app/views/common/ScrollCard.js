a72362899121227600c07243b46af5a55a6e89dd
Mario Muniz <mcmuniz@ucsd.edu>
2018-09-17 11:14:03 -0700

    Fix parking card not rerendering when data changes; Modify ScrollCard to take extraData prop

--- app/views/common/ScrollCard.js
+++ app/views/common/ScrollCard.js
@@ -46,6 +46,7 @@ class ScrollCard extends React.Component {
 			list = (
 				<FlatList
 					ref={(c) => { this._flatlist = c }}
+					extraData={this.props.extraData}
 					style={css.scrollcard_listStyle}
 					onContentSizeChange={this.countDots}
 					pagingEnabled
@@ -57,6 +58,7 @@ class ScrollCard extends React.Component {
 					enableEmptySections={true}
 					keyExtractor={(listItem, index) => {
 						if (listItem.id) return listItem.id.toString() + index
+						if (listItem.LocationId) return listItem.LocationId.toString() + index
 						else {
 							const error = new Error('Invalid ScrollCard list item')
 							trackException(error, listItem)
--- app/views/parking/ParkingCard.js
+++ app/views/parking/ParkingCard.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from 'react'
 import { withNavigation } from 'react-navigation'
-import { AnimatedCircularProgress } from 'react-native-circular-progress';
-import ScrollCard from '../common/ScrollCard';
-import ParkingOverview from './ParkingOverview';
+import ScrollCard from '../common/ScrollCard'
+import ParkingOverview from './ParkingOverview'
 
-const ParkingCard = ({ savedStructures, navigation, gotoParkingSpotType, mySpots, lotCount }) => {
-  const extraActions = [
+const ParkingCard = ({
+	savedStructures,
+	navigation,
+	gotoParkingSpotType,
+	mySpots,
+	lotCount
+}) => {
+	const extraActions = [
 		{
 			name: 'Edit Spot Type',
 			action: gotoParkingSpotType
 		}
-	];
+	]
 
-  return (
-	   <ScrollCard
-        id="parking"
-        title="Parking Availability"
-        renderItem={({ item }) => (
-          <ParkingOverview
-            structureData={item}
-            spotsSelected={mySpots}
-            lotCount={lotCount}
-          />
-        )}
-        scrollData={savedStructures}
-        extraActions={extraActions}
-    />
-  );
-};
+	const parkingData = {
+		mySpots,
+		lotCount,
+	}
 
-export default withNavigation(ParkingCard);
+	return (
+		<ScrollCard
+			id="parking"
+			title="Parking Availability"
+			renderItem={({ item }) => (
+				<ParkingOverview
+					structureData={item}
+					spotsSelected={mySpots}
+					lotCount={lotCount}
+				/>
+			)}
+			scrollData={savedStructures}
+			extraData={parkingData}
+			extraActions={extraActions}
+		/>
+	)
+}
+
+export default withNavigation(ParkingCard)
