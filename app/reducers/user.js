14ef852dcdba9746a3bd29cf6e8ff5828a1d26a3
Mario Muniz <mcmuniz@ucsd.edu>
2018-09-21 13:44:28 -0700

    Add more default topics; Remove unusued reference to removed reducer in notification preferences

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -4,7 +4,7 @@ const initialState = {
 		username: null,
 		classifications: null,
 		pid: null,
-		subscribedTopics: ['emergency', 'all'],
+		subscribedTopics: ['emergency', 'all', 'shuttle', 'freefood'],
 	},
 	syncedProfile: {},
 	invalidSavedCredentials: false,
