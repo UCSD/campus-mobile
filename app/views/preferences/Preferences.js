2e4ff66c00629946f1fb198ea81905facc69e870
Bryant, Charles <c3bryant@ucsd.edu>
2018-09-25 10:54:05 -0700

    Add privacy policy to preferences

--- app/views/preferences/Preferences.js
+++ app/views/preferences/Preferences.js
@@ -3,6 +3,7 @@ import { View, ScrollView } from 'react-native'
 import UserAccount from './user/UserAccount'
 import CardPreferences from './card/CardPreferences'
 import PreferencesItem from './PreferencesItem'
+import AppSettings from '../../AppSettings'
 import css from '../../styles/css'
 
 // View for user to manage preferences, including which cards are visible
@@ -24,8 +25,9 @@ class PreferencesView extends Component {
 				<View style={css.profile_inner}>
 					<UserAccount />
 					<CardPreferences toggleScroll={this.toggleScroll} />
-					<PreferencesItem title="Notifications" iconPack="FontAwesome" icon="bell-o" link="Notifications" />
-					<PreferencesItem title="Feedback" iconPack="Entypo" icon="new-message" link="Feedback" />
+					<PreferencesItem title="Notifications" iconPack="FontAwesome" icon="bell-o" linkType="internal" link="Notifications" />
+					<PreferencesItem title="Feedback" iconPack="Entypo" icon="new-message" linkType="internal" link="Feedback" />
+					<PreferencesItem title="Privacy Policy" iconPack="Feather" icon="lock" linkType="external" link={AppSettings.PRIVACY_POLICY_URL} />
 				</View>
 			</ScrollView>
 		)
--- app/views/preferences/PreferencesItem.js
+++ app/views/preferences/PreferencesItem.js
@@ -3,8 +3,10 @@ import { Text } from 'react-native'
 import { withNavigation } from 'react-navigation'
 import Entypo from 'react-native-vector-icons/Entypo'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import Feather from 'react-native-vector-icons/Feather'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import Touchable from '../common/Touchable'
+import { openURL } from '../../util/general'
 import css from '../../styles/css'
 
 /**
@@ -15,19 +17,26 @@ import css from '../../styles/css'
  * @param {Object} style Optional style
  * @return {JSX} Return PreferencesItem JSX
  */
-const PreferencesItem = ({ title, iconPack, icon, link, style, navigation }) => (
+const PreferencesItem = ({ title, iconPack, icon, linkType, link, style, navigation }) => (
 	<Touchable
-		onPress={() => { navigation.navigate(link) }}
+		onPress={() => {
+			if (linkType === 'internal') {
+				navigation.navigate(link)
+			} else if (linkType === 'external') {
+				openURL(link)
+			}
+		}}
 		style={css.pi_container}
 	>
 		{iconPack === 'Entypo' ? (
 			<Entypo name={icon} size={24} style={css.pi_icon} />
 		) : null }
-
+		{iconPack === 'Feather' ? (
+			<Feather name={icon} size={24} style={css.pi_icon} />
+		) : null }
 		{iconPack === 'FontAwesome' ? (
 			<FontAwesome name={icon} size={24} style={css.pi_icon} />
 		) : null }
-
 		<Text style={css.pi_title}>{title}</Text>
 		<Ionicons name="ios-arrow-forward" size={24} style={css.pi_arrow} />
 	</Touchable>
