fcf0d3a6790eb3e2c48becbaa43bfe1f78240c98
Bryant, Charles <c3bryant@ucsd.edu>
2018-10-19 16:04:12 -0700

    Replacements/CI update p6

--- scripts/replace_all.js
+++ scripts/replace_all.js
@@ -50,51 +50,64 @@ const PH = {
 	GS_ANDROID_API_KEY_PH: 'GS_ANDROID_API_KEY_PH',
 	GS_ANDROID_APP_ID_PH: 'GS_ANDROID_APP_ID_PH',
 	GS_ANDROID_BUNDLE_ID_PH: 'GS_ANDROID_BUNDLE_ID_PH',
-
-
 }
 
 if (REPLACEMENT_ENV === 'prod' || REPLACEMENT_ENV === 'qa' || REPLACEMENT_ENV === 'ph') {
 	/*
 	// AppSettings.js
 	makeReplacements(APP_SETTINGS_PATH, REPLACEMENT_ENV, [
-		{ prodVal: myEnv.APP_NAME, stgVal: myEnv.APP_NAME, phVal: PH.APP_NAME_PH },
-		{ prodVal: myEnv.GOOGLE_ANALYTICS_ID_PROD, stgVal: myEnv.GOOGLE_ANALYTICS_ID_PH, phVal: PH.GOOGLE_ANALYTICS_ID_PH },
+		{ prodVal: myEnv.APP_NAME, qaVal: myEnv.APP_NAME, phVal: PH.APP_NAME_PH },
+		{ prodVal: myEnv.GOOGLE_ANALYTICS_ID_PROD, qaVal: myEnv.GOOGLE_ANALYTICS_ID_PH, phVal: PH.GOOGLE_ANALYTICS_ID_PH },
 	])
 
 	// ssoService.js
 	makeReplacements(SSO_SERVICE_PATH, REPLACEMENT_ENV, [
-		{ prodVal: myEnv.AUTH_SERVICE_API_KEY_PROD, stgVal: myEnv.AUTH_SERVICE_API_KEY_QA, phVal: PH.AUTH_SERVICE_API_KEY_PH },
+		{ prodVal: myEnv.AUTH_SERVICE_API_KEY_PROD, qaVal: myEnv.AUTH_SERVICE_API_KEY_QA, phVal: PH.AUTH_SERVICE_API_KEY_PH },
 	])
 
 	// Info.plist
 	makeReplacements(IOS_INFO_PLIST_PATH, REPLACEMENT_ENV, [
-		{ prodVal: myEnv.APP_NAME, stgVal: myEnv.APP_NAME, phVal: PH.APP_NAME_PH },
-		{ prodVal: myEnv.BUGSNAG_KEY, stgVal: myEnv.BUGSNAG_KEY, phVal: PH.BUGSNAG_KEY_PH }
+		{ prodVal: myEnv.APP_NAME, qaVal: myEnv.APP_NAME, phVal: PH.APP_NAME_PH },
+		{ prodVal: myEnv.BUGSNAG_KEY, qaVal: myEnv.BUGSNAG_KEY, phVal: PH.BUGSNAG_KEY_PH }
 	])
 
 	// strings.xml
 	makeReplacements(ANDROID_STRINGS_PATH, REPLACEMENT_ENV, [
-		{ prodVal: myEnv.APP_NAME, stgVal: myEnv.APP_NAME, phVal: PH.APP_NAME_PH },
+		{ prodVal: myEnv.APP_NAME, qaVal: myEnv.APP_NAME, phVal: PH.APP_NAME_PH },
 	])
 
 	// AndroidManifest.xml
 	makeReplacements(ANDROID_MANIFEST_PATH, REPLACEMENT_ENV, [
-		{ prodVal: myEnv.GOOGLE_MAPS_API_KEY, stgVal: myEnv.GOOGLE_MAPS_API_KEY, phVal: PH.GOOGLE_MAPS_API_KEY_PH },
-		{ prodVal: myEnv.BUGSNAG_KEY, stgVal: myEnv.BUGSNAG_KEY, phVal: PH.BUGSNAG_KEY_PH }
+		{ prodVal: myEnv.GOOGLE_MAPS_API_KEY, qaVal: myEnv.GOOGLE_MAPS_API_KEY, phVal: PH.GOOGLE_MAPS_API_KEY_PH },
+		{ prodVal: myEnv.BUGSNAG_KEY, qaVal: myEnv.BUGSNAG_KEY, phVal: PH.BUGSNAG_KEY_PH }
 	])
 	*/
 
 	// GoogleService-Info.plist
 	makeReplacements(IOS_GOOGLE_SERVICES_PATH, REPLACEMENT_ENV, [
-		{ prodVal: myEnv.FIREBASE_IOS_KEY, phVal: PH.FIREBASE_KEY_PH },
+		{ prodVal: myEnv.GS_IOS_PROD_CLIENT_ID, 				qaVal: myEnv.GS_IOS_QA_CLIENT_ID, 				phVal: PH.GS_IOS_CLIENT_ID_PH },
+		{ prodVal: myEnv.GS_IOS_PROD_REVERSED_CLIENT_ID,		qaVal: myEnv.GS_IOS_QA_REVERSED_CLIENT_ID, 		phVal: PH.GS_IOS_REVERSED_CLIENT_ID_PH },
+		{ prodVal: myEnv.GS_IOS_PROD_API_KEY, 					qaVal: myEnv.GS_IOS_QA_API_KEY, 				phVal: PH.GS_IOS_API_KEY_PH },
+		{ prodVal: myEnv.GS_IOS_PROD_BUNDLE_ID, 				qaVal: myEnv.GS_IOS_QA_BUNDLE_ID, 				phVal: PH.GS_IOS_BUNDLE_ID_PH },
+		{ prodVal: myEnv.GS_IOS_PROD_APP_ID, 					qaVal: myEnv.GS_IOS_QA_APP_ID, 					phVal: PH.GS_IOS_APP_ID_PH },
+
+		{ prodVal: myEnv.GS_PROD_FIREBASE_URL, 					qaVal: myEnv.GS_QA_FIREBASE_URL, 				phVal: PH.GS_FIREBASE_URL_PH },
+		{ prodVal: myEnv.GS_PROD_STORAGE_BUCKET, 				qaVal: myEnv.GS_QA_STORAGE_BUCKET, 				phVal: PH.GS_STORAGE_BUCKET_PH },
+		{ prodVal: myEnv.GS_PROD_PROJECT_ID, 					qaVal: myEnv.GS_QA_PROJECT_ID, 					phVal: PH.GS_PROJECT_ID_PH },
+		{ prodVal: myEnv.GS_PROD_PROJECT_NUMBER, 				qaVal: myEnv.GS_QA_PROJECT_NUMBER, 				phVal: PH.GS_PROJECT_NUMBER_PH },
 	])
-	/*
 	// google-services.json
 	makeReplacements(ANDROID_GOOGLE_SERVICES_PATH, REPLACEMENT_ENV, [
-		{ prodVal: myEnv.FIREBASE_ANDROID_KEY, phVal: PH.FIREBASE_KEY_PH },
+		{ prodVal: myEnv.GS_ANDROID_PROD_CLIENT_ID, 			qaVal: myEnv.GS_ANDROID_QA_CLIENT_ID, 			phVal: PH.GS_ANDROID_CLIENT_ID_PH },
+		{ prodVal: myEnv.GS_ANDROID_PROD_API_KEY, 				qaVal: myEnv.GS_ANDROID_QA_API_KEY, 			phVal: PH.GS_ANDROID_API_KEY_PH },
+		{ prodVal: myEnv.GS_ANDROID_PROD_APP_ID, 				qaVal: myEnv.GS_ANDROID_QA_APP_ID, 				phVal: PH.GS_ANDROID_APP_ID_PH },
+		{ prodVal: myEnv.GS_ANDROID_PROD_BUNDLE_ID, 			qaVal: myEnv.GS_ANDROID_QA_BUNDLE_ID, 			phVal: PH.GS_ANDROID_BUNDLE_ID_PH },
+
+		{ prodVal: myEnv.GS_PROD_FIREBASE_URL, 					qaVal: myEnv.GS_QA_FIREBASE_URL, 				phVal: PH.GS_FIREBASE_URL_PH },
+		{ prodVal: myEnv.GS_PROD_STORAGE_BUCKET, 				qaVal: myEnv.GS_QA_STORAGE_BUCKET, 				phVal: PH.GS_STORAGE_BUCKET_PH },
+		{ prodVal: myEnv.GS_PROD_PROJECT_ID, 					qaVal: myEnv.GS_QA_PROJECT_ID, 					phVal: PH.GS_PROJECT_ID_PH },
+		{ prodVal: myEnv.GS_PROD_PROJECT_NUMBER, 				qaVal: myEnv.GS_QA_PROJECT_NUMBER, 				phVal: PH.GS_PROJECT_NUMBER_PH },
 	])
-*/
 } else {
 	console.log('Error: Replacement type not specififed')
 	console.log('Sample Usage: npm run-script [campus-ph|campus-qa|campus-prod]')
@@ -105,12 +118,17 @@ function makeReplacements(FILE_PATH, ENV, REPLACEMENTS) {
 	fs.readFile(FILE_PATH, 'utf8', (err, data) => {
 		if (!err) {
 			for (let i = 0; REPLACEMENTS.length > i; i++) {
-				if (ENV === 'prod') {
-					data = data.replace(REPLACEMENTS[i].phVal, REPLACEMENTS[i].prodVal).replace(REPLACEMENTS[i].stgVal, REPLACEMENTS[i].prodVal)
+				/*if (ENV === 'prod') {
+					data = data.replace(REPLACEMENTS[i].phVal, REPLACEMENTS[i].prodVal).replace(REPLACEMENTS[i].qaVal, REPLACEMENTS[i].prodVal)
 				} else if (ENV === 'qa') {
-					data = data.replace(REPLACEMENTS[i].prodVal, REPLACEMENTS[i].stgVal).replace(REPLACEMENTS[i].phVal, REPLACEMENTS[i].stgVal)
+					data = data.replace(REPLACEMENTS[i].prodVal, REPLACEMENTS[i].qaVal).replace(REPLACEMENTS[i].phVal, REPLACEMENTS[i].qaVal)
 				} else if (ENV === 'ph') {
-					data = data.replace(REPLACEMENTS[i].prodVal, REPLACEMENTS[i].phVal).replace(REPLACEMENTS[i].stgVal, REPLACEMENTS[i].phVal)
+					data = data.replace(REPLACEMENTS[i].prodVal, REPLACEMENTS[i].phVal).replace(REPLACEMENTS[i].qaVal, REPLACEMENTS[i].phVal)
+				}*/
+				if (ENV === 'prod') {
+					data = data.replace(REPLACEMENTS[i].phVal, REPLACEMENTS[i].prodVal)
+				} else if (ENV === 'ph') {
+					data = data.replace(REPLACEMENTS[i].prodVal, REPLACEMENTS[i].phVal)
 				}
 			}
 			fs.writeFile(FILE_PATH, data, 'utf8', (writeErr) => {
