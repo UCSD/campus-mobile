a84d6b6ed83b119c3bff8f1a57d5d8e36154bd79
Bryant, Charles <c3bryant@ucsd.edu>
2018-11-13 13:19:57 -0800

    Fix tests

--- tests.setup.js
+++ tests.setup.js
@@ -1,3 +1,6 @@
+// Needed to properly mock react-native-google-analytics-bridge
+import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge' // eslint-disable-line no-unused-vars
+
 // Mock AppSettings here explicitly
 // Needed because dev and prod values can differ, which may
 // break certain snapshots under different testing environments
@@ -7,5 +10,6 @@ AppSettingsMock.APP_NAME = 'Campus Mobile'
 jest.setMock('./app/AppSettings', AppSettingsMock)
 
 // Mock native modules here
+jest.mock('react-native-google-analytics-bridge')
 jest.mock('bugsnag-react-native')
 jest.mock('react-native-simple-toast', () => ({ showWithGravity: () => jest.fn() }))
