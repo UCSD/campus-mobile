a3c148db04532ff9d3a71391503754d1e50452ee
Bryant, Charles <c3bryant@ucsd.edu>
2019-01-23 14:18:33 -0800

    Use appcenter qa

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -8,4 +8,4 @@ curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HO
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
 # Insert environment variables
-npm run campus-prod-ci
+npm run campus-qa-ci
