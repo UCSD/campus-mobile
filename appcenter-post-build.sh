2384112014478facd2c3c44a15c9cab807dcaa56
Bryant, Charles <c3bryant@ucsd.edu>
2019-03-14 09:23:36 -0700

    appcenter post build debug

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,12 +4,17 @@ echo "#### appcenter-post-build START ####"
 # Install bugsnag-sourcemaps
 npm install --global bugsnag-sourcemaps
 
+# Debug
+pwd
+ls -la
+echo "Bundle File: $BUNDLE_FILE"
+
 # Upload source maps to bugsnag 
 bugsnag-sourcemaps upload \
     --api-key $BUGSNAG_KEY \
     --code-bundle-id $APPCENTER_BUILD_ID \
     --source-map index.ios.map \
-    --minified-url dummy-sourcemap-main.jsbundle \
+    --minified-url main.jsbundle \
     --minified-file ios-release.bundle \
     --upload-sources
 
