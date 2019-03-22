d0c506cdb196ad7254c48a3efed9f3e75842fb8e
Bryant, Charles <c3bryant@ucsd.edu>
2019-03-21 21:42:55 -0700

    add SPECIAL_EVENT_API_URL to env setup

--- scripts/replace-all.js
+++ scripts/replace-all.js
@@ -61,6 +61,7 @@ if (REPLACEMENT_ENV === 'prod' || REPLACEMENT_ENV === 'qa') {
 		{ prodVal: myEnv.MP_REGISTRATION_API_URL_PROD, qaVal: myEnv.MP_REGISTRATION_API_URL_QA },
 		{ prodVal: myEnv.MESSAGES_TOPICS_URL_PROD, qaVal: myEnv.MESSAGES_TOPICS_URL_QA },
 		{ prodVal: myEnv.PARKING_API_URL_PROD, qaVal: myEnv.PARKING_API_URL_QA },
+		{ prodVal: myEnv.SPECIAL_EVENT_API_URL_PROD, qaVal: myEnv.SPECIAL_EVENT_API_URL_QA },
 	])
 
 	// ssoService.js
