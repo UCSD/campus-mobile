35a57deb8a8c704f08fdf2ad4d96f63ecb9ae4d2
Bryant, Charles <c3bryant@ucsd.edu>
2019-04-03 23:02:20 -0700

    set max brightness on modal barcode

--- app/views/studentId/StudentIDCardContainer.js
+++ app/views/studentId/StudentIDCardContainer.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Alert } from 'react-native'
 import { connect } from 'react-redux'
+import SystemSetting from 'react-native-system-setting'
 import StudentIDCard from './StudentIDCard'
-
 
 class StudentIDCardContainer extends React.Component {
 	constructor(props) {
@@ -12,7 +13,30 @@ class StudentIDCardContainer extends React.Component {
 	}
 
 	_toggleModal = () => {
+		this.state.barcodeModalVisible ? this._restoreBrightness() : this._setMaxBrightness()
 		this.setState({ barcodeModalVisible: !this.state.barcodeModalVisible })
+	}
+
+	_setMaxBrightness = () => {
+		// Save original brightness to restore later
+		SystemSetting.saveBrightness()
+
+		// Set max brightness
+		SystemSetting.setBrightnessForce(1.0).then((success) => {
+			!success && Alert.alert(
+				'Unable to Increase Brightness',
+				'For easier scanning, it is recommended to temporarily set your device to it\'s maximum brightness setting.',
+				[
+					{ 'text': 'Allow', style: 'Cancel' },
+					{ 'text': 'Open Settings', onPress: () => SystemSetting.grantWriteSettingPremission() }
+				]
+			)
+		})
+	}
+
+	_restoreBrightness = () => {
+		// Restore original brightness
+		SystemSetting.restoreBrightness()
 	}
 
 	render() {
@@ -34,4 +58,3 @@ const mapStateToProps = state => (
 )
 
 export default connect(mapStateToProps)(StudentIDCardContainer)
-
