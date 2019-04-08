396d72536ad8c4cb7ede1a4a3b174b459b42834b
Bryant, Charles <c3bryant@ucsd.edu>
2019-04-08 10:02:49 -0700

    update large barcode close icon style

--- app/styles/css.js
+++ app/styles/css.js
@@ -397,7 +397,7 @@ const css = StyleSheet.create({
 	sid_barcode_id: { textAlign: 'center', fontSize: 14, zIndex: 2, marginTop: -8, letterSpacing: 4 },
 	// Barcode Modal
 	sid_modal: { margin: 40, backgroundColor: COLOR.WHITE },
-	sid_modal_close: { position: 'absolute', bottom: 16, right: 16, transform: [{ rotate: '90deg' }] },
+	sid_modal_close: { position: 'absolute', bottom: 16, right: 16 },
 	sid_modal_close_icon: { color: COLOR.PRIMARY },
 	sid_modal_barcode: { justifyContent: 'center', transform: [{ rotate: '90deg' }] },
 	sid_modal_barcode_id: { textAlign: 'center', fontSize: 20, zIndex: 2, marginTop: -8, letterSpacing: 4 },
