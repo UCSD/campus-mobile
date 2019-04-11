5350bfa99569deee7f7e652b955dc20b62dc9d64
atmartorana <atmartorana@gmail.com>
2019-04-11 14:09:28 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+//This will be the style guide used for this project
+//https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final ucsdBackgrounndColor = const Color(0xFF182B49);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    );
+  }
+}
