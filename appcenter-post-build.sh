d70f70d29833fdb03ce7d497d2d05e12b2ffca74
Bryant, Charles <c3bryant@ucsd.edu>
2019-04-16 14:46:06 -0700

    update post build script for ios source map

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -2,26 +2,33 @@
 echo "#### appcenter-post-build START ####"
 
 # Install bugsnag-sourcemaps
-#npm install --global bugsnag-sourcemaps
+npm install --global bugsnag-sourcemaps
 
 # Debug
-#pwd
-#ls -la
-#echo "Bundle File: $BUNDLE_FILE"
+pwd
+ls -la
+echo "Bundle File: $BUNDLE_FILE"
+
+react-native bundle \
+    --platform ios \
+    --dev false \
+    --entry-file index.js \
+    --bundle-output ios-release.bundle \
+    --sourcemap-output ios-release.bundle.map
 
 # Upload source maps to bugsnag 
-#bugsnag-sourcemaps upload \
-#    --api-key $BUGSNAG_KEY \
-#    --code-bundle-id $APPCENTER_BUILD_ID \
-#    --source-map index.ios.map \
-#    --minified-url main.jsbundle \
-#    --minified-file ios-release.bundle \
-#    --upload-sources
+bugsnag-sourcemaps upload \
+   --api-key $BUGSNAG_KEY \
+   --code-bundle-id $APPCENTER_BUILD_ID \
+   --source-map ios-release.bundle.map \
+   --minified-url main.jsbundle \
+   --minified-file ios-release.bundle \
+   --upload-sources
 
-#echo '## APPCENTER SOURCE:'
-#find $APPCENTER_SOURCE_DIRECTORY
+echo '## APPCENTER SOURCE:'
+find $APPCENTER_SOURCE_DIRECTORY
 
-#echo '## APPCENTER OUTPUT:'
-#find $APPCENTER_OUTPUT_DIRECTORY
+echo '## APPCENTER OUTPUT:'
+find $APPCENTER_OUTPUT_DIRECTORY
 
 echo "#### appcenter-post-build END ####"
