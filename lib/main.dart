7d0fe75337855ac881cd21e07ec1cd41b939252b
Alex Wu <a6wu@ucsd.edu>
2019-04-16 18:18:49 -0700

    Add appBar and blue theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,34 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final ucsdBackgrounndColor = const Color(0xFF182B49);
+  final ucsdBackgroundColor = const Color(0xFF182B49);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'UCSD',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+
     );
   }
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("UCSD"),
+      ),
+      body: Container(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => new HomeState();
 }
