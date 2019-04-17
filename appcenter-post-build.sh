31813a8d99dc2be21f27bccc54716c595eabc0d1
Bryant, Charles <c3bryant@ucsd.edu>
2019-04-16 17:10:03 -0700

    update post build script p4

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,6 +4,7 @@ echo "#### appcenter-post-build START ####"
 npm install --global react-native-cli
 npm install --global bugsnag-sourcemaps
 
+# Source Maps - iOS
 react-native bundle \
     --platform ios \
     --dev false \
@@ -11,14 +12,28 @@ react-native bundle \
     --bundle-output ios-release.bundle \
     --sourcemap-output ios-release.bundle.map
 
-# Upload source maps to bugsnag 
 bugsnag-sourcemaps upload \
-   --api-key $BUGSNAG_KEY \
-   --app-version $APP_VERSION \
-   --code-bundle-id $APPCENTER_BUILD_ID \
-   --source-map ios-release.bundle.map \
-   --minified-url main.jsbundle \
-   --minified-file ios-release.bundle \
-   --upload-sources
+    --api-key $BUGSNAG_KEY \
+    --code-bundle-id $APPCENTER_BUILD_ID \
+    --source-map ios-release.bundle.map \
+    --minified-url main.jsbundle \
+    --minified-file ios-release.bundle \
+    --upload-sources
 
+# Source Maps - Android
+react-native bundle \
+    --platform android \
+    --dev false \
+    --entry-file index.js \
+    --bundle-output android-release.bundle \
+    --sourcemap-output android-release.bundle.map
+
+bugsnag-sourcemaps upload \
+    --api-key $BUGSNAG_KEY \
+    --code-bundle-id $APPCENTER_BUILD_ID \
+    --source-map android-release.bundle.map \
+    --minified-url main.jsbundle \
+    --minified-file android-release.bundle \
+    --upload-sources
+    
 echo "#### appcenter-post-build END ####"
