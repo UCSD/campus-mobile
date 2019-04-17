22b913012594ca25d7628f822b1323f030b65047
Bryant, Charles <c3bryant@ucsd.edu>
2019-04-16 20:49:10 -0700

    update post build script p6

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,41 +4,41 @@ echo "#### appcenter-post-build START ####"
 npm install --global react-native-cli
 npm install --global bugsnag-sourcemaps
 
-echo "APPCENTER_XCODE_PROJECT: $APPCENTER_XCODE_PROJECT"
-echo "APPCENTER_XCODE_SCHEME: $APPCENTER_XCODE_SCHEME"
-echo "APPCENTER_ANDROID_VARIANT: $APPCENTER_ANDROID_VARIANT"
-echo "APPCENTER_ANDROID_MODULE: $APPCENTER_ANDROID_MODULE"
+if [ -n "$APPCENTER_XCODE_PROJECT" ]
+then
+    echo "Generating React Native bundle for iOS..."
+    react-native bundle \
+        --platform ios \
+        --dev false \
+        --entry-file index.js \
+        --bundle-output ios-release.bundle \
+        --sourcemap-output ios-release.bundle.map
+    
+    echo "Uploading source map for iOS..."
+    bugsnag-sourcemaps upload \
+        --api-key $BUGSNAG_KEY \
+        --code-bundle-id $APPCENTER_BUILD_ID \
+        --source-map ios-release.bundle.map \
+        --minified-url main.jsbundle \
+        --minified-file ios-release.bundle \
+        --upload-sources
+else
+    echo "Generating React Native bundle for Android..."
+    react-native bundle \
+        --platform android \
+        --dev false \
+        --entry-file index.js \
+        --bundle-output android-release.bundle \
+        --sourcemap-output android-release.bundle.map
 
-# Source Maps - iOS
-react-native bundle \
-    --platform ios \
-    --dev false \
-    --entry-file index.js \
-    --bundle-output ios-release.bundle \
-    --sourcemap-output ios-release.bundle.map
-
-bugsnag-sourcemaps upload \
-    --api-key $BUGSNAG_KEY \
-    --code-bundle-id $APPCENTER_BUILD_ID \
-    --source-map ios-release.bundle.map \
-    --minified-url main.jsbundle \
-    --minified-file ios-release.bundle \
-    --upload-sources
-
-# Source Maps - Android
-react-native bundle \
-    --platform android \
-    --dev false \
-    --entry-file index.js \
-    --bundle-output android-release.bundle \
-    --sourcemap-output android-release.bundle.map
-
-bugsnag-sourcemaps upload \
-    --api-key $BUGSNAG_KEY \
-    --code-bundle-id $APPCENTER_BUILD_ID \
-    --source-map android-release.bundle.map \
-    --minified-url main.jsbundle \
-    --minified-file android-release.bundle \
-    --upload-sources
+    echo "Uploading source map for Android..."
+    bugsnag-sourcemaps upload \
+        --api-key $BUGSNAG_KEY \
+        --code-bundle-id $APPCENTER_BUILD_ID \
+        --source-map android-release.bundle.map \
+        --minified-url main.jsbundle \
+        --minified-file android-release.bundle \
+        --upload-sources
+fi
 
 echo "#### appcenter-post-build END ####"
