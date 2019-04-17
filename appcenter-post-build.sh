159ce6b029b7b4d21ac2c9b39a59d8d12366d6fe
Bryant, Charles <c3bryant@ucsd.edu>
2019-04-16 19:52:09 -0700

    update post build script p5

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -4,6 +4,11 @@ echo "#### appcenter-post-build START ####"
 npm install --global react-native-cli
 npm install --global bugsnag-sourcemaps
 
+echo "APPCENTER_XCODE_PROJECT: $APPCENTER_XCODE_PROJECT"
+echo "APPCENTER_XCODE_SCHEME: $APPCENTER_XCODE_SCHEME"
+echo "APPCENTER_ANDROID_VARIANT: $APPCENTER_ANDROID_VARIANT"
+echo "APPCENTER_ANDROID_MODULE: $APPCENTER_ANDROID_MODULE"
+
 # Source Maps - iOS
 react-native bundle \
     --platform ios \
@@ -35,5 +40,5 @@ bugsnag-sourcemaps upload \
     --minified-url main.jsbundle \
     --minified-file android-release.bundle \
     --upload-sources
-    
+
 echo "#### appcenter-post-build END ####"
