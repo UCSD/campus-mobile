09d7c4b2cb7f7d12793e11ba433ce7e2b69bd7c0
vishant101 <vishant.singh@outlook.com>
2019-04-18 15:37:46 -0700

    Add support for promo banners featuring in-app redirects and external URLs

--- app/views/specialEvents/SpecialEventsCard.js
+++ app/views/specialEvents/SpecialEventsCard.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { View } from 'react-native'
 import { withNavigation } from 'react-navigation'
-
+import { openURL } from '../../util/general'
 import BannerCard from '../common/BannerCard'
 
 export const SpecialEventsCard = ({ navigation, specialEvents, saved, hideCard }) => (
 	<View>
 		<BannerCard
-			title={specialEvents.name}
-			image={specialEvents.logo}
-			onPress={() => navigation.navigate('SpecialEventsView', { title: specialEvents.name, personal: false })}
+			showButton={!(specialEvents['event-type'] === 'Promo Banner')}
+			title={(specialEvents.name) ? specialEvents.name : specialEvents.eventname}
+			image={(specialEvents.logo) ? specialEvents.logo : specialEvents['banner-image']}
+			onPress={() => {
+				if (specialEvents['event-type'] === 'Promo Banner') {
+					const array = specialEvents['banner-url'].split('://')
+					if (array[0] === 'app') {
+						navigation.navigate(array[1])
+					} else {
+						openURL(specialEvents['banner-url'])
+					}
+				} else {
+					navigation.navigate('SpecialEventsView', { title: specialEvents.name, personal: false })
+				}
+			}}
 			onClose={() => hideCard('specialEvents')}
 		/>
 	</View>
