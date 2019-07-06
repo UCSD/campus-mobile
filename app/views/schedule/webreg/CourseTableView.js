c57b54506a25bb78dc0179fe5b394de8217ddb7a
Dian Qi <qidian990107@163.com>
2019-07-06 13:20:21 -0700

    add animation to course table and begin refactoring

--- app/views/schedule/webreg/CourseTableView.js
+++ app/views/schedule/webreg/CourseTableView.js
@@ -1,6 +1,15 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react'
-import { View, Text, Image, Dimensions, TouchableWithoutFeedback } from 'react-native'
+import {
+	View,
+	Text,
+	Image,
+	Dimensions,
+	TouchableWithoutFeedback,
+	Animated,
+	Easing,
+	FlatList
+} from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import HeaderRow from './HeaderRow'
 import SectionRow from './SectionRow'
@@ -11,12 +20,104 @@ import ExpandArrow from './icons8-expand-arrow-filled-500.png'
 const { width } = Dimensions.get('screen')
 
 export default class CourseTableView extends Component {
+
 	constructor() {
 		super()
 		this.state = {
-			expanded: false
+			expanded: false,
+			/* Add custom animation and color changes */
+			animatedHeight: new Animated.Value(0),
+			animatedColor: new Animated.Value(0),
+			/* Min and Max height of course table */
+			minHeight: 0,
+			maxHeight: 0,
+			/* Keep track of the progress of animated value by adding a listener */
+			progress: 0,
+			/* Lecture style that will be added the animiated value once
+			   our component has mounted */
+			lectureStyle: {
+				overflow: 'hidden'
+			}
 		}
 		this._onPress = this._onPress.bind(this)
+		this._setMaxHeight = this._setMaxHeight.bind(this)
+		this._setMinHeight = this._setMinHeight.bind(this)
+		this.toggle = this.toggle.bind(this)
+		this.state.animatedHeight.addListener((progress) => {
+			console.log(progress.value)
+			this.setState({ progress })
+		})
+	}
+
+	_setLectureStyle() {
+		this.setState({ lectureStyle: [{...this.state.lectureStyle}, { height: this.state.animatedHeight }] })
+	}
+
+	_setMaxHeight(event) {
+		console.log('in set max height', event.nativeEvent.layout.height)
+
+		this.setState({
+			maxHeight: event.nativeEvent.layout.height + 7
+		})
+
+		this._setLectureStyle()
+	}
+
+	_setMinHeight(event) {
+		console.log('in set min height', event.nativeEvent.layout.height)
+		const { height } = event.nativeEvent.layout
+		this.setState({
+			minHeight: height + 7,
+		})
+		this.state.animatedHeight.setValue(height + 7)
+	}
+
+	/**
+	 * Toggle the discussion sections in a course table.
+	 *
+	 */
+	toggle() {
+		const initialValue = this.state.expanded ? this.state.maxHeight : this.state.minHeight
+		const finalValue = this.state.expanded ? this.state.minHeight : this.state.maxHeight
+
+		this.setState({
+			expanded: !this.state.expanded
+		})
+
+		this.state.animatedHeight.setValue(initialValue)
+		if (this.state.expanded) {
+			Animated.timing(
+				this.state.animatedHeight,
+				{
+					toValue: finalValue,
+					duration: 500,
+					easing: Easing.in(Easing.ease)
+				}
+			).start()
+			Animated.timing(
+				this.state.animatedColor,
+				{
+					toValue: 0,
+					duration: 500,
+				}
+			).start()
+		} else {
+			Animated.timing(
+				this.state.animatedHeight,
+				{
+					toValue: finalValue,
+					duration: 500,
+					easing: Easing.out(Easing.ease)
+				}
+			).start()
+			Animated.timing(
+				this.state.animatedColor,
+				{
+					toValue: 150,
+					duration: 500,
+				}
+			).start()
+		}
 	}
 
 	_onPress = () => {
@@ -29,7 +130,13 @@ export default class CourseTableView extends Component {
 		const { cellWrapperStyle, cellContainerStyle, dotStyle, expandIconStyle } = styles
 		const { expanded } = this.state
 		return (
-			<TouchableWithoutFeedback onPress={this._onPress}>
+			<TouchableWithoutFeedback
+				onPress={() => {
+					console.log('in rendering lectures')
+					this.toggle()
+				}}
+				onLayout={this._setMinHeight}
+			>
 				<View style={[cellWrapperStyle, { paddingTop: 4, paddingBottom: 7 }]}>
 					<View style={dotStyle} />
 					<View style={cellContainerStyle}>
@@ -79,7 +186,13 @@ export default class CourseTableView extends Component {
 	renderPrereq() {
 		const { cellWrapperStyle, cellContainerStyle, expandIconStyle, prereqTextStyle } = styles
 		return (
-			<TouchableWithoutFeedback>
+			<TouchableWithoutFeedback
+				// onPress={() => {
+				// 	console.log('in rendering course Prerequisites')
+				// 	this.toggle()
+				// }}
+				// onLayout={this._setMinHeight}
+			>
 				<View style={[cellWrapperStyle, { marginBottom: 12 }]}>
 					<View style={cellContainerStyle}>
 						<Text style={prereqTextStyle}>Course Prerequisites & Level Restrictions</Text>
@@ -93,19 +206,81 @@ export default class CourseTableView extends Component {
 	render() {
 		const { containerStyle } = styles
 		return (
-			<View style={[containerStyle, this.props.style]}>
+			<Animated.View
+				style={
+					[
+						containerStyle,
+						this.props.style,
+					]
+				}
+			>
 				{this.renderPrereq()}
-				{Course.sections.map((sect) => {
-					switch (sect.type) {
-						case 'LE':
-							return this.renderLecture(Course, sect)
-						case 'DI':
-							return this.state.expanded && this.renderEnrollment(sect)
-						default:
-							return this.renderAdditionalMeeting(sect)
+				<Animated.View
+					style={this.state.lectureStyle}
+				>
+					<View style={{ overflow: 'hidden' }} onLayout={this._setMaxHeight}>
+					{
+						Course.sections.map((sect) => {
+							switch (sect.type) {
+								case 'LE':
+									return this.renderLecture(Course, sect)
+								case 'DI':
+									// return this.state.expanded && this.renderEnrollment(sect)
+									return this.renderEnrollment(sect)
+								default:
+									// return this.renderAdditionalMeeting(sect)
+									return
+							}
+						})
 					}
-				})}
-			</View>
+					{/*<FlatList
+						data={Course.sections}
+						style={{ overflow: 'hidden' }}
+						keyExtractor={({item, index}) => index}
+						renderItem={({item, index}) => {
+							console.log('section', item, index)
+						switch (item.type) {
+							case 'LE':
+								return this.renderLecture(Course, item)
+							case 'DI':
+								// return this.state.expanded && this.renderEnrollment(sect)
+								return this.renderEnrollment(item)
+							default:
+								// return this.renderAdditionalMeeting(sect)
+								return
+						}}}
+					/>*/}
+					</View>
+						{/*
+							style={{ flex: 1, flexDirection: 'column' }}
+
+							Course.sections.map((sect) => {
+							switch (sect.type) {
+								case 'LE':
+									return this.renderLecture(Course, sect)
+								case 'DI':
+									// return this.state.expanded && this.renderEnrollment(sect)
+									return this.renderEnrollment(sect)
+								default:
+									// return this.renderAdditionalMeeting(sect)
+									return
+							}
+						})*/}
+				</Animated.View>
+				<View>
+					{Course.sections.map((sect) => {
+						switch (sect.type) {
+							case 'LE':
+								return
+							case 'DI':
+								return
+							default:
+								return this.renderAdditionalMeeting(sect)
+						}
+					})}
+				</View>
+
+			</Animated.View>
 		)
 	}
 }
@@ -115,6 +290,7 @@ const styles = {
 		flexDirection: 'column',
 		justifyContent: 'flex-start',
 		alignItems: 'center',
+		overflow: 'hidden'
 	},
 	cellWrapperStyle: {
 		width: width - 32,
@@ -131,12 +307,12 @@ const styles = {
 			height: 2
 		},
 		shadowRadius: 5,
-		shadowOpacity: 0.1
+		shadowOpacity: 0.1,
 	},
 	cellContainerStyle: {
 		width: '73%',
 		flexDirection: 'column',
-		right: 3
+		right: 3,
 	},
 	expandIconStyle: {
 		position: 'absolute',
