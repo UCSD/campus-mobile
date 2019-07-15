280bc499d6a0120cbc7dcb8b8c201574d9418df1
Alex Wu <alex.h.wu@gmail.com>
2019-07-15 12:03:13 -0700

    Add weather card with static data

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final ucsdBackgroundColor = const Color(0xFF182B49);
+  final ucsdBackgroundColor = const Color(0x034263);
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Home(),
-
     );
   }
 }
@@ -28,7 +27,99 @@ class HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("UCSD"),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    leading: Icon(Icons.wb_sunny),
+                    title: Text('72 in San Diego'),
+                    subtitle: Text('Clear'),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        width: 70,
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text('MON'),
+                            Icon(Icons.wb_cloudy),
+                            Text('74'),
+                            Text('61'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 70,
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text('TUE'),
+                            Icon(Icons.wb_sunny),
+                            Text('74'),
+                            Text('61'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 70,
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text('WED'),
+                            Icon(Icons.wb_sunny),
+                            Text('74'),
+                            Text('61'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 70,
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text('THU'),
+                            Icon(Icons.wb_sunny),
+                            Text('74'),
+                            Text('61'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 70,
+                        padding: EdgeInsets.all(10.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text('FRI'),
+                            Icon(Icons.wb_sunny),
+                            Text('74'),
+                            Text('61'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  ButtonTheme.bar(
+                    // make buttons use the appropriate styles for cards
+                    child: ButtonBar(
+                      children: <Widget>[
+                        FlatButton(
+                          child: const Text('Surf Report'),
+                          onPressed: () {/* ... */},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
