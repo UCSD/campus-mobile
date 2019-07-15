9d6bd8ceeb0f7da21195b2d6fd0872ddad758e28
Alex Wu <alex.h.wu@gmail.com>
2019-07-15 12:52:12 -0700

    Refactor weather card static data

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,96 @@ class MyApp extends StatelessWidget {
   }
 }
 
+Widget buildWeatherCard() {
+  return Card(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        const ListTile(
+          leading: Icon(Icons.wb_sunny),
+          title: Text('72 in San Diego'),
+          subtitle: Text('Clear'),
+        ),
+        Row(
+          children: <Widget>[
+            Container(
+              width: 70,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Text('MON'),
+                  Icon(Icons.wb_cloudy),
+                  Text('74'),
+                  Text('61'),
+                ],
+              ),
+            ),
+            Container(
+              width: 70,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Text('TUE'),
+                  Icon(Icons.wb_sunny),
+                  Text('74'),
+                  Text('61'),
+                ],
+              ),
+            ),
+            Container(
+              width: 70,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Text('WED'),
+                  Icon(Icons.wb_sunny),
+                  Text('74'),
+                  Text('61'),
+                ],
+              ),
+            ),
+            Container(
+              width: 70,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Text('THU'),
+                  Icon(Icons.wb_sunny),
+                  Text('74'),
+                  Text('61'),
+                ],
+              ),
+            ),
+            Container(
+              width: 70,
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  Text('FRI'),
+                  Icon(Icons.wb_sunny),
+                  Text('74'),
+                  Text('61'),
+                ],
+              ),
+            ),
+          ],
+        ),
+        ButtonTheme.bar(
+          // make buttons use the appropriate styles for cards
+          child: ButtonBar(
+            children: <Widget>[
+              FlatButton(
+                child: const Text('Surf Report'),
+                onPressed: () {/* ... */},
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
 class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
@@ -30,93 +120,7 @@ class HomeState extends State<Home> {
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const ListTile(
-                    leading: Icon(Icons.wb_sunny),
-                    title: Text('72 in San Diego'),
-                    subtitle: Text('Clear'),
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Container(
-                        width: 70,
-                        padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text('MON'),
-                            Icon(Icons.wb_cloudy),
-                            Text('74'),
-                            Text('61'),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 70,
-                        padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text('TUE'),
-                            Icon(Icons.wb_sunny),
-                            Text('74'),
-                            Text('61'),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 70,
-                        padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text('WED'),
-                            Icon(Icons.wb_sunny),
-                            Text('74'),
-                            Text('61'),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 70,
-                        padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text('THU'),
-                            Icon(Icons.wb_sunny),
-                            Text('74'),
-                            Text('61'),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 70,
-                        padding: EdgeInsets.all(10.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text('FRI'),
-                            Icon(Icons.wb_sunny),
-                            Text('74'),
-                            Text('61'),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  ButtonTheme.bar(
-                    // make buttons use the appropriate styles for cards
-                    child: ButtonBar(
-                      children: <Widget>[
-                        FlatButton(
-                          child: const Text('Surf Report'),
-                          onPressed: () {/* ... */},
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            buildWeatherCard(),
           ],
         ),
       ),
