bd2bde9711ab6ac25cb08ccf2a8f1f7f1200eab4
hanyt08 <hanyt08@gmail.com>
2019-07-17 01:26:36 +0800

    add notification on action buttons

--- app/views/schedule/webreg/CourseActionView.js
+++ app/views/schedule/webreg/CourseActionView.js
@@ -1,63 +1,112 @@
 import React from 'react'
-import { View, Text, Dimensions, TouchableOpacity } from 'react-native'
+import { View, Text, Dimensions, TouchableOpacity, Alert } from 'react-native'
+import { withNavigation } from 'react-navigation'
 // import { getScreenWidth } from '../../../util/general'
 
 const { width } = Dimensions.get('screen')
-
-const CourseActionView = ({ style }) => {
-	const {
-		cellWrapperStyle,
-		buttonStyle,
-		borderStyle,
-		borderReverseStyle,
-		buttonTextStyle,
-		selectorStyle,
-		selectorButtonStyle
-	} = styles
-	const buttons = ['Plan', 'Remove', ['P/NP', 'Letter'], 'Enroll']
-	const leftStyle = {
-		borderTopLeftRadius: 4,
-		borderBottomLeftRadius: 4,
-	}
-	const rightStyle = {
-		borderTopRightRadius: 4,
-		borderBottomRightRadius: 4,
-	}
-	return (
-		<View style={[cellWrapperStyle, style, { paddingTop: 7, paddingBottom: 14 }]}>
-			{buttons.map((label, index) => {
-				switch (index) {
-					case 0: case 1:
-						return (
-							<TouchableOpacity style={[buttonStyle, borderStyle]}>
-								<Text style={buttonTextStyle}>{label}</Text>
-							</TouchableOpacity>
-						)
-					case 2:
-						return (
-							<View style={selectorStyle}>
-								<TouchableOpacity style={[selectorButtonStyle, borderStyle, leftStyle]}>
-									<Text style={buttonTextStyle}>{label[0]}</Text>
-								</TouchableOpacity>
-								<TouchableOpacity style={[selectorButtonStyle, borderReverseStyle, rightStyle]}>
-									<Text style={[buttonTextStyle, { color: '#fff' }]}>{label[1]}</Text>
-								</TouchableOpacity>
-							</View>
-						)
-					case 3:
-						return (
-							<TouchableOpacity style={[buttonStyle, borderReverseStyle]}>
-								<Text style={[buttonTextStyle, { color: '#fff' }]}>{label}</Text>
-							</TouchableOpacity>
-						)
-					default: return null
-				}
-			})}
-		</View>
-	)
+const buttons = ['Plan', 'Remove', ['P/NP', 'Letter'], 'Enroll']
+const leftStyle = {
+	borderTopLeftRadius: 4,
+	borderBottomLeftRadius: 4,
 }
+const rightStyle = {
+	borderTopRightRadius: 4,
+	borderBottomRightRadius: 4,
+}
+class CourseActionView extends React.Component {
+	onBtnClicked = (title, msg) => {
+		Alert.alert(
+			title,
+			msg,
+			[
+				{ text: 'Send Email Verification', onPress: () => console.log('email') },
+				{ text: 'Back to WebReg',
+					onPress: () => {
+						this.props.navigation.navigate('WebReg')
+						console.log('back')
+					},
+					style: 'cancel'
+				},
+			],
+		)
+	}
 
-export default CourseActionView
+	onPlanClicked = () => {
+		this.onBtnClicked('Course added to plan', 'CSE 12: added to Fall 2019 plan')
+	}
+
+	onRemoveClicked = () => {
+		Alert.alert(
+			'Remove course from plan?',
+			'the action cannot be undone',
+			[
+				{ text: 'Cancel', onPress: () => console.log('cancel'), style: 'cancel' },
+				{ text: 'Remove',
+					onPress: () => {
+						this.onBtnClicked('Course removed from plan', 'CSE 12: removed from Fall 2019 plan')
+					},
+					style: 'destructive'
+				},
+			],
+		)
+	}
+
+	onEnrollClicked = () => {
+		this.onBtnClicked('Course enrolled', 'CSE 12: added to Fall 2019 plan')
+	}
+
+	render() {
+		const { style } = this.props
+		const {
+			cellWrapperStyle,
+			buttonStyle,
+			borderStyle,
+			borderReverseStyle,
+			buttonTextStyle,
+			selectorStyle,
+			selectorButtonStyle
+		} = styles
+
+		return (
+			<View style={[cellWrapperStyle, style, { paddingTop: 7, paddingBottom: 14 }]}>
+				{buttons.map((label, index) => {
+					switch (index) {
+						case 0: case 1:
+							return (
+								<TouchableOpacity
+									style={[buttonStyle, borderStyle]}
+									onPress={index === 0 ? this.onPlanClicked : this.onRemoveClicked}
+								>
+									<Text style={buttonTextStyle}>{label}</Text>
+								</TouchableOpacity>
+							)
+						case 2:
+							return (
+								<View style={selectorStyle}>
+									<TouchableOpacity style={[selectorButtonStyle, borderStyle, leftStyle]}>
+										<Text style={buttonTextStyle}>{label[0]}</Text>
+									</TouchableOpacity>
+									<TouchableOpacity style={[selectorButtonStyle, borderReverseStyle, rightStyle]}>
+										<Text style={[buttonTextStyle, { color: '#fff' }]}>{label[1]}</Text>
+									</TouchableOpacity>
+								</View>
+							)
+						case 3:
+							return (
+								<TouchableOpacity
+									style={[buttonStyle, borderReverseStyle]}
+									onPress={this.onEnrollClicked}
+								>
+									<Text style={[buttonTextStyle, { color: '#fff' }]}>{label}</Text>
+								</TouchableOpacity>
+							)
+						default: return null
+					}
+				})}
+			</View>
+		)
+	}
+}
 
 const styles = {
 	cellWrapperStyle: {
@@ -109,3 +158,5 @@ const styles = {
 		alignItems: 'center'
 	}
 }
+
+export default withNavigation(CourseActionView)
