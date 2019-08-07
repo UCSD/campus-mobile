ace149134a9ab61a462f316a74f2a7b59f35b2d7
g9singh <g1singh@ucsd.edu>
2019-08-07 14:53:30 -0700

    update App.js to configure store

--- App.js
+++ App.js
@@ -3,11 +3,19 @@
  */
 /* eslint react/jsx-pascal-case: 0 */
 import React from 'react'
-import Router from './test_components/react-navigation/Router'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import storeAndPersistor from './test_components/redux-persist/configureStore'
+import Router from './app/navigation/Router'
 
 
 const App = () => (
-	<Router />
+	<Provider store={storeAndPersistor.store}>
+		<PersistGate loading={null} persistor={storeAndPersistor.persistor}>
+			<Router />
+		</PersistGate>
+	</Provider>
 )
 
 export default App
