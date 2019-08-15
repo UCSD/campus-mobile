a4dc038b9e855745625a9a043582874a4ce3b419
Bryant, Charles <c3bryant@ucsd.edu>
2019-08-15 11:04:32 -0700

    add launch screen, app icon for iOS

--- ios/CampusMobile/AppDelegate.m
+++ ios/CampusMobile/AppDelegate.m
@@ -25,7 +25,7 @@
                                                    moduleName:@"CampusMobile"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.094f green:0.169f blue:0.287f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
