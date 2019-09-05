d76b53fb5e6db73588285854b72201de3d4ce619
g9singh <g1singh@ucsd.edu>
2019-09-05 10:39:04 -0700

    add push notification container back into root of app

--- app/App.js
+++ app/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-
+import PushNotificationContainer from './containers/pushNotificationContainer'
 import storeAndPersistor from './configureStore'
 import Router from './navigation/Router'
 
 const App = () => (
 	<Provider store={storeAndPersistor.store}>
 		<PersistGate loading={null} persistor={storeAndPersistor.persistor}>
+			<PushNotificationContainer />
 			<Router />
 		</PersistGate>
 	</Provider>
