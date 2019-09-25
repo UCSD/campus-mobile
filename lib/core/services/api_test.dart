d0145230049161ac158bee4534157eebd723322c
Charles Bryant <c3bryant@ucsd.edu>
2019-09-24 21:54:15 -0700

    Refactor main Setup MultiProvider and test api

---
+++ lib/core/services/api_test.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:campus_mobile/core/models/data.dart';
+
+/// The service responsible for networking requests
+class Api {
+    static const endpoint = 'https://jsonplaceholder.typicode.com/users/1';
+
+    var client = new http.Client();
+
+    Future<Data> getTest() async {
+        // Get user profile for id
+        var response = await client.get('$endpoint');
+
+        // Convert and return
+        return json.decode(response.body);
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 //This will be the style guide used for this project
 //https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
+import 'package:campus_mobile/core/constants/app_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:campus_mobile/ui/views/home.dart';
-import 'package:campus_mobile/ui/widgets/cards/weather.dart';
-import 'package:campus_mobile/ui/views/map.dart';
-import 'package:campus_mobile/ui/views/notifications.dart';
-import 'package:campus_mobile/ui/views/profile.dart';
+import 'package:campus_mobile/provider.dart';
+import 'package:campus_mobile/ui/router.dart';
 import 'package:campus_mobile/ui/theme/app_theme.dart';
 
 void main() => runApp(MyApp());
@@ -15,77 +13,16 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'UC San Diego',
-      theme: ThemeData(
-        primarySwatch: ColorPrimary,
-      ),
-      home: Home(),
-    );
-  }
-}
-
-class HomeState extends State<Home> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black);
-
-  final List<Widget> _children = <Widget>[
-    Weather(),
-    Map(Colors.deepOrange), // map
-    Notifications(Colors.blue), // notifications
-    Profile(), // profile
-  ];
-
-  Weather weatherCard;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Image.asset(
-            'assets/images/UCSanDiegoLogo-nav.png',
-            width: 200,
-          ),
+    return MultiProvider(
+      providers: providers,
+      child: MaterialApp(
+        title: 'UC San Diego',
+        theme: ThemeData(
+          primarySwatch: ColorPrimary,
         ),
-      ),
-      body: _children[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map),
-            title: Text('Map'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            title: Text('Messages'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('Profile'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue[900],
-        unselectedItemColor: Colors.grey[500],
-        onTap: _onItemTapped,
+        initialRoute: RoutePaths.Home,
+        onGenerateRoute: Router.generateRoute,
       ),
     );
   }
-}
-
-class Home extends StatefulWidget {
-  @override
-  HomeState createState() => new HomeState();
 }
