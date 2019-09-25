afb2a7e4ca7ac86dfec89666decaad22abd5a938
Charles Bryant <c3bryant@ucsd.edu>
2019-09-25 15:43:19 -0700

    Install Firebase p1 contd

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 //This will be the style guide used for this project
 //https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo
-import 'package:campus_mobile/core/constants/app_constants.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'package:campus_mobile/provider.dart';
+import 'package:campus_mobile/core/constants/app_constants.dart';
 import 'package:campus_mobile/ui/router.dart';
 import 'package:campus_mobile/ui/theme/app_theme.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer =
+    FirebaseAnalyticsObserver(analytics: analytics);
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -22,6 +28,9 @@ class MyApp extends StatelessWidget {
         ),
         initialRoute: RoutePaths.Home,
         onGenerateRoute: Router.generateRoute,
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: analytics),
+        ],
       ),
     );
   }
