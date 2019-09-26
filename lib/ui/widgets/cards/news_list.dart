d6e80c31029d1dfb2584f79a80f03b87fd9e348a
Alex Wu <alex.h.wu@gmail.com>
2019-09-25 22:12:42 -0700

    update card padding and style

--- lib/ui/widgets/cards/news_list.dart
+++ lib/ui/widgets/cards/news_list.dart
@@ -1,3 +1,4 @@
+import 'package:campus_mobile/ui/theme/text_styles.dart';
 import 'package:flutter/material.dart';
 
 class NewsList extends StatelessWidget {
@@ -6,6 +7,16 @@ class NewsList extends StatelessWidget {
     return Card(
       child: Column(
         children: <Widget>[
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                'News',
+                style: headerStyle,
+              ),
+            ),
+            alignment: Alignment.centerLeft,
+          ),
           buildNewsCard(),
           buildNewsCard(),
           buildNewsCard(),
@@ -19,15 +30,26 @@ class NewsList extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Column(children: <Widget>[
-          Text('Ah-Mason Jars - Revelle Residents  Ah-Mason Jars'),
+          Container(
+            child: Text(
+              'Ah-Mason Jars - Revelle Residents  Ah-Mason Jars',
+              style: subHeaderStyle,
+            ),
+            alignment: Alignment.centerLeft,
+          ),
           Row(
             children: <Widget>[
               Expanded(
-                child: Text(
-                    'Revelle Hall Association invites Revelle Residents to come out and decorate a mason jar!! A nice way to add a personal touch to your bedroom decor.'),
+                  child: Text(
+                      'Revelle Hall Association invites Revelle Residents to come out and decorate a mason jar!! A nice way to add a personal touch to your bedroom decor.'),
+                  flex: 3),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.network(
+                      'https://studentevents.ucsd.edu/article_images/190925409555925.png'),
+                ),
               ),
-              Image.network(
-                  'https://studentevents.ucsd.edu/article_images/190925409555925.png'),
             ],
           ),
         ]),
