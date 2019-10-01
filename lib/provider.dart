c61cf97eee2706ac4b40b91832252fa29d9cde88
Charles Bryant <c3bryant@ucsd.edu>
2019-10-01 09:37:05 -0700

    Remove duplicate class

--- lib/provider.dart
+++ lib/provider.dart
@@ -9,15 +9,3 @@ List<SingleChildCloneableWidget> providers = [
 List<SingleChildCloneableWidget> independentServices = [
     Provider.value(value: Api())
 ];
-
-
-class BottomNavigationBarProvider with ChangeNotifier {
-    int _currentIndex = 0;
-
-    get currentIndex => _currentIndex;
-
-    set currentIndex(int index) {
-        _currentIndex = index;
-        notifyListeners();
-    }
-}
