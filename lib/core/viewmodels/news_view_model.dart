739453f2c321f2501477e67bd4dff0cc2f201198
g9singh <g1singh@ucsd.edu>
2019-10-04 14:28:31 -0700

    update news view model to read correct data

--- lib/core/viewmodels/news_view_model.dart
+++ lib/core/viewmodels/news_view_model.dart
@@ -2,6 +2,7 @@ import 'package:campus_mobile/core/models/news_model.dart';
 import 'package:flutter/material.dart';
 import 'package:campus_mobile/core/services/news_service.dart';
 import 'package:campus_mobile/ui/widgets/cards/card_container.dart';
+import 'package:campus_mobile/ui/widgets/image_loader.dart';
 
 class NewsCard extends StatefulWidget {
   @override
@@ -10,7 +11,7 @@ class NewsCard extends StatefulWidget {
 
 class _NewsState extends State<NewsCard> {
   final NewsService _newsService = NewsService();
-  Future<List<NewsModel>> _data;
+  Future<NewsModel> _data;
 
   initState() {
     super.initState();
@@ -26,14 +27,17 @@ class _NewsState extends State<NewsCard> {
   }
 
   Widget buildNewsCard(AsyncSnapshot snapshot) {
+    return buildNewsList(snapshot, 3);
+  }
+
+  Widget buildNewsList(AsyncSnapshot snapshot, int listSize) {
     if (snapshot.hasData) {
-      final List<NewsModel> data = snapshot.data;
+      final NewsModel data = snapshot.data;
+      final List<Item> listOfNews = data.items;
       final List<Widget> newsTiles = List<Widget>();
-      //data.length instead of 3
-      for (int i = 0; i < 3; i++) {
-        final NewsModel item = data[i];
-        final tile = buildNewsTile(
-            item.shortDescription, item.description, item.imageThumb);
+      for (int i = 0; i < listSize; i++) {
+        final Item item = listOfNews[i];
+        final tile = buildNewsTile(item.title, item.description, item.image);
         newsTiles.add(tile);
       }
       return Flexible(
@@ -55,6 +59,7 @@ class _NewsState extends State<NewsCard> {
   }
 
   Widget buildNewsTile(String title, String subtitle, String imageURL) {
+    if (imageURL.isEmpty) print('was empty');
     return ListTile(
       title: Text(
         title,
@@ -66,16 +71,22 @@ class _NewsState extends State<NewsCard> {
         textAlign: TextAlign.start,
         overflow: TextOverflow.ellipsis,
       ),
-      trailing: Image.network(
-        imageURL,
-        width: 100,
+      trailing: ImageLoader(url: imageURL),
+    );
+  }
+
+  Widget buildActionButton() {
+    return FlatButton(
+      child: Text(
+        'View All',
       ),
+      onPressed: () {/*TODO navigate to view with full news list*/},
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<NewsModel>>(
+    return FutureBuilder<NewsModel>(
       future: _data,
       builder: (context, snapshot) {
         return CardContainer(
