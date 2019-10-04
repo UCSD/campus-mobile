cb771858719c1a1f83e7c848f1002844dc79a69d
g9singh <g1singh@ucsd.edu>
2019-10-04 14:30:02 -0700

    create image loader widget

---
+++ lib/ui/widgets/image_loader.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ImageLoader extends StatelessWidget {
+  final String url;
+  final double width;
+  final double height;
+  ImageLoader({@required this.url, this.width = 100.0, this.height = 100.0});
+  @override
+  Widget build(BuildContext context) {
+    return url.isEmpty
+        ? Container(
+            width: 0,
+            height: 0,
+          )
+        : Image.network(
+            url,
+            width: width,
+            height: height,
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes
+                      : null,
+                ),
+              );
+            },
+          );
+  }
+}
