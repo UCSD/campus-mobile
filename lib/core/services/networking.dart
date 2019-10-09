5c4c72c526f8cd2aad19761f64d1ade430d89ae8
g9singh <g9singh@ucsd.edu>
2019-10-09 11:34:09 -0700

    add authorized public fetch in network

--- lib/core/services/networking.dart
+++ lib/core/services/networking.dart
@@ -27,6 +27,26 @@ class NetworkHelper {
     }
   }
 
+  Future<dynamic> authorizedFetch(Map<String, String> headers) async {
+    _error = null;
+    _isLoading = true;
+    _response = await http.get(url, headers: headers);
+    if (_response.statusCode == 200) {
+      // If server returns an OK response, return the body
+      _isLoading = false;
+      _error = null;
+      print(_response.body);
+      return _response.body;
+    } else {
+      _error = _response.body;
+      _isLoading = false;
+
+      ///TODO: log this as a bug because the response was bad
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+
   String get error => _error;
   http.Response get response => _response;
   bool get isLoading => _isLoading;
