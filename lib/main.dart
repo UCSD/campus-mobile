0c658d80721bbed802605a0358a9a54491c84adb
Bryant, Charles <c3bryant@ucsd.edu>
2019-10-14 23:06:35 -0700

    iOS updates 1

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'UC San Diego'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
