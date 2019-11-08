0aece8919186e432215c6b907a32cf54013591fc
sssaiya <sssaiya@ucsd.edu>
2019-11-07 18:48:15 -0800

    Add UI for events

--- lib/ui/views/special_events/special_events_detail_view.dart
+++ lib/ui/views/special_events/special_events_detail_view.dart
@@ -2,6 +2,7 @@ import 'package:campus_mobile_beta/core/models/special_events_model.dart';
 import 'package:campus_mobile_beta/core/services/special_events_service.dart';
 import 'package:flutter/material.dart';
 import 'package:campus_mobile_beta/ui/widgets/container_view.dart';
+import 'package:intl/intl.dart';
 
 class SpecialEventsViewModel extends StatefulWidget {
   @override
@@ -52,7 +53,7 @@ class _SpecialEventsViewModelState extends State<SpecialEventsViewModel> {
           child: ListView.builder(
               itemCount: uids.length,
               itemBuilder: (BuildContext ctxt, int index) =>
-                  buildEventWidget(ctxt, index, data, uids)));
+                  buildEventWidget(ctxt, data, uids[index])));
     } else {
       return Container();
     }
@@ -63,8 +64,61 @@ class _SpecialEventsViewModelState extends State<SpecialEventsViewModel> {
     return data.dateItems[dateSelected];
   }
 
-  Widget buildEventWidget(BuildContext ctxt, int index, SpecialEventsModel data,
-      List<String> uids) {
-    return Text("Hello from " + uids[index]);
+  Widget buildEventWidget(
+      BuildContext ctxt, SpecialEventsModel data, String uid) {
+    Schedule event = data.schedule[uid];
+    return ListTile(
+      contentPadding: EdgeInsets.all(0),
+      //dense: true,
+      leading: timeWidget(event),
+      title: titleWidget(event),
+      subtitle: buildSubtitle(event),
+      trailing: buildTrailing(event),
+    );
   }
+
+  // Container(
+  //             width: 1,
+  //             height: 50,
+  //             color: Colors.grey,
+  //           ),
+
+  Widget timeWidget(Schedule event) {
+    var dateTime = new DateTime.fromMillisecondsSinceEpoch(event.startTime);
+    return Text(new DateFormat.jm().format(dateTime));
+  }
+
+  Widget titleWidget(Schedule event){
+    return Text(event.talkTitle);
+  }
+
+  Widget buildSubtitle(Schedule event) {
+    Color labelTheme = HexColor(event.labelTheme);
+    return Text(
+      event.label,
+      style: TextStyle(color: labelTheme),
+    );
+  }
+
+  Widget buildTrailing(Schedule event){
+    bool isGoing = true; //TODO add personal event list after state management is done
+    if(isGoing){
+      return Icon(Icons.star, color: Colors.yellow , size: 54, semanticLabel: 'Starred' );
+    }
+    else
+    return Icon(Icons.star_border, color: Colors.yellow , size: 54, semanticLabel: 'Not Starred' );
+  }
+}
+
+//Helper class to convert RGB to ARGB for flutter
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
