4e217c9d7adf93bb5e6da5c250bde2a03b0d9942
g9singh <g9singh@ucsd.edu>
2019-11-13 11:08:00 -0800

    provider p4

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:campus_mobile_beta/provider_setup.dart' as prefix0;
+import 'package:campus_mobile_beta/core/viewmodels/bottom_navigation_bar_model.dart';
+import 'package:campus_mobile_beta/provider_setup.dart';
 import 'package:campus_mobile_beta/ui/router.dart';
 import 'package:campus_mobile_beta/ui/theme/app_theme.dart';
-import 'package:campus_mobile_beta/ui/views/home.dart';
-import 'package:campus_mobile_beta/ui/views/map.dart';
-import 'package:campus_mobile_beta/ui/views/notifications.dart';
-import 'package:campus_mobile_beta/ui/views/profile.dart';
-import 'package:campus_mobile_beta/core/services/availability_service.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,15 +12,7 @@ class CampusMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<BottomNavigationBarProvider>(
-          child: BottomNavigationBarExample(),
-          builder: (BuildContext context) => BottomNavigationBarProvider(),
-        ),
-        ChangeNotifierProvider<AvailabilityService>(
-          builder: (context) => AvailabilityService(),
-        ),
-      ],
+      providers: changeNotifierProviders,
       child: MaterialApp(
         title: 'UC San Diego',
         theme: ThemeData(
@@ -45,83 +33,9 @@ class CampusMobile extends StatelessWidget {
           iconTheme: darkIconTheme,
           appBarTheme: darkAppBarTheme,
         ),
-        home: BottomNavigationBarExample(),
+        home: BottomTabBar(),
         onGenerateRoute: Router.generateRoute,
       ),
     );
-  }
-}
-
-class BottomNavigationBarExample extends StatefulWidget {
-  @override
-  _BottomNavigationBarExampleState createState() =>
-      _BottomNavigationBarExampleState();
-}
-
-class _BottomNavigationBarExampleState
-    extends State<BottomNavigationBarExample> {
-  var currentTab = [
-    Home(),
-    Map(),
-    Notifications(),
-    Profile(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    var provider = Provider.of<BottomNavigationBarProvider>(context);
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(42),
-        child: AppBar(
-          primary: true,
-          centerTitle: true,
-          title: Image.asset(
-            'assets/images/UCSanDiegoLogo-nav.png',
-            fit: BoxFit.contain,
-            height: 28,
-          ),
-        ),
-      ),
-      body: currentTab[provider.currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: provider.currentIndex,
-        onTap: (index) {
-          provider.currentIndex = index;
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            title: new Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.map),
-            title: new Text('Map'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.notifications),
-            title: new Text('Notifications'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.person),
-            title: new Text('User Profile'),
-          ),
-        ],
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        selectedItemColor: IconTheme.of(context).color,
-        unselectedItemColor: Colors.grey.shade500,
-      ),
-    );
-  }
-}
-
-class BottomNavigationBarProvider with ChangeNotifier {
-  int _currentIndex = 0;
-  get currentIndex => _currentIndex;
-  set currentIndex(int index) {
-    _currentIndex = index;
-    notifyListeners();
   }
 }
--- lib/provider_setup.dart
+++ lib/provider_setup.dart
@@ -1,10 +1,14 @@
 // provider_setup.dart
 import 'package:campus_mobile_beta/core/services/availability_service.dart';
+import 'package:campus_mobile_beta/core/services/bottom_navigation_bar_service.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
 
-List<SingleChildCloneableWidget> providers = [
-  ...independentServices,
-];
-List<SingleChildCloneableWidget> independentServices = [
-  Provider.value(value: AvailabilityService())
+List<SingleChildCloneableWidget> changeNotifierProviders = [
+  ChangeNotifierProvider<BottomNavigationBarProvider>(
+    builder: (BuildContext context) => BottomNavigationBarProvider(),
+  ),
+  ChangeNotifierProvider<AvailabilityService>(
+    builder: (context) => AvailabilityService(),
+  ),
 ];
