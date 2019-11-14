1e21cacdeba78f8b412826560038195b0e5f96ab
Atreto76 <AdvancedTrigonometry@protonmail.com>
2019-11-14 10:54:24 -0800

    Integrate Google Maps for Android p1

--- lib/ui/views/map.dart
+++ lib/ui/views/map.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Map extends StatelessWidget {
+  GoogleMapController mapController;
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(
-          'Map'
+      child: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
       ),
     );
   }
