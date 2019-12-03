42d6ed7675c87b40dc1abdc532e89ea426296b41
atmartorana <atmartorana@gmail.com>
2019-12-03 07:57:11 -0800

    Initial setup, added dependencies

---
+++ lib/core/services/message_service.dart
@@ -0,0 +1,7 @@
+import 'dart:async';
+import 'package:campus_mobile_experimental/core/models/message_model.dart';
+import 'package:http/http.dart' as http;
+
+class MessageService {
+
+}
---
+++ lib/core/viewmodels/messages_view_model.dart
@@ -0,0 +1,18 @@
+import 'package:campus_mobile_experimental/core/models/message_model.dart';
+import 'package:flutter/material.dart';
+import 'package:campus_mobile_experimental/core/services/message_service.dart';
+import 'package:campus_mobile_experimental/ui/views/messages/messages_list_view.dart';
+
+class Messages extends StatefulWidget{
+  @override
+  _MessagesState createState() => _MessagesState();
+
+}
+
+class _MessagesState extends State<Messages>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  }
+
+}
