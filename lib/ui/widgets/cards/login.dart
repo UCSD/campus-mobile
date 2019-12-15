05784a39630496b676d68de0b2c9c317c9268856
g9singh <g9singh@ucsd.edu>
2019-12-14 17:42:30 -0800

    update login ui to use new data provider setup

--- lib/ui/widgets/cards/login.dart
+++ lib/ui/widgets/cards/login.dart
@@ -1,62 +1,6 @@
-import 'package:campus_mobile_experimental/core/services/authentication_service.dart';
+import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-//// TODO: Move to remote config
-//const String AUTH_SERVICE_API_URL =
-//    'https://3hepzvdimd.execute-api.us-west-2.amazonaws.com/dev/v1/access-profile';
-//// TODO: Move to Codemagic and generate new key
-//const String AUTH_SERVICE_API_KEY = 'eKFql1kJAj53iyU2fNKyH4jI2b7t70MZ5YbAuPBZ';
-//
-//// TODO: Setup as a standalone service
-//Future<String> mobileAuthService(String url, Map<String, String> options) async {
-//  final response = await http.post(url, headers: options);
-//
-//  if (response.statusCode == 200) {
-//    // If server returns an OK response, return the response body
-//    return response.body;
-//  } else {
-//    // If that response was not OK, throw an error.
-//    if (response.body == null) {
-//      throw Exception('An unknown error occurred.');
-//    } else {
-//      throw Exception(response.body);
-//    }
-//  }
-//}
-
-//void doLogin(String email, String password) async {
-//  print("SSO login initiated for user: $email");
-//
-//  // TODO: import assets/public_key.txt
-//  final String pkString = '-----BEGIN PUBLIC KEY-----\n' +
-//      'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDJD70ejMwsmes6ckmxkNFgKley\n' +
-//      'gfN/OmwwPSZcpB/f5IdTUy2gzPxZ/iugsToE+yQ+ob4evmFWhtRjNUXY+lkKUXdi\n' +
-//      'hqGFS5sSnu19JYhIxeYj3tGyf0Ms+I0lu/MdRLuTMdBRbCkD3kTJmTqACq+MzQ9G\n' +
-//      'CaCUGqS6FN1nNKARGwIDAQAB\n' +
-//      '-----END PUBLIC KEY-----';
-//
-//  final rsaParser = RSAKeyParser();
-//  final pc.RSAPublicKey publicKey = rsaParser.parse(pkString);
-//  var cipher = OAEPEncoding(pc.AsymmetricBlockCipher('RSA'));
-//  pc.AsymmetricKeyParameter<pc.RSAPublicKey> keyParametersPublic =
-//      new pc.PublicKeyParameter(publicKey);
-//  cipher.init(true, keyParametersPublic);
-//  Uint8List output = cipher.process(utf8.encode(password));
-//  var base64EncodedText = base64.encode(output);
-//  final String base64EncodedWithEncryptedPassword =
-//      base64.encode(utf8.encode(email + ':' + base64EncodedText));
-//  final Map<String, String> authServiceHeaders = {
-//    'x-api-key': AUTH_SERVICE_API_KEY,
-//    'Authorization': base64EncodedWithEncryptedPassword,
-//  };
-//  final response =
-//      await mobileAuthService(AUTH_SERVICE_API_URL, authServiceHeaders);
-//  debugPrint(response);
-//
-//  // TODO: Setup authentication model
-//  // TODO: Make available to provider
-//}
 
 class Login extends StatefulWidget {
   @override
@@ -66,13 +10,13 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final _emailTextFieldController = TextEditingController();
   final _passwordTextFieldController = TextEditingController();
-  AuthenticationService _authenticationService;
+  UserDataProvider _userDataProvider;
 
   @override
   void didChangeDependencies() {
     // TODO: implement didChangeDependencies
     super.didChangeDependencies();
-    _authenticationService = Provider.of<AuthenticationService>(context);
+    _userDataProvider = Provider.of<UserDataProvider>(context);
   }
 
   @override
@@ -110,10 +54,10 @@ class _LoginState extends State<Login> {
                 Expanded(
                   child: FlatButton(
                     child: Text('Sign In'),
-                    onPressed: _authenticationService.isLoading
+                    onPressed: _userDataProvider.isLoading
                         ? null
                         : () {
-                            _authenticationService.login(
+                            _userDataProvider.login(
                                 _emailTextFieldController.text,
                                 _passwordTextFieldController.text);
                           },
