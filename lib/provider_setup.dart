de0037ce3611d3d290cdca3ddeaeeaa9435f8edc
g9singh <g9singh@ucsd.edu>
2019-12-14 17:42:07 -0800

    update provider to use proxy provider setup

--- lib/provider_setup.dart
+++ lib/provider_setup.dart
@@ -1,4 +1,5 @@
-import 'package:campus_mobile_experimental/core/services/authentication_service.dart';
+import 'package:campus_mobile_experimental/core/data_providers/parking_data_provider.dart';
+import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
 import 'package:campus_mobile_experimental/core/services/availability_service.dart';
 import 'package:campus_mobile_experimental/core/services/bottom_navigation_bar_service.dart';
 import 'package:campus_mobile_experimental/core/services/dining_service.dart';
@@ -6,15 +7,19 @@ import 'package:campus_mobile_experimental/core/services/event_service.dart';
 import 'package:campus_mobile_experimental/core/services/location_service.dart';
 import 'package:campus_mobile_experimental/core/models/coordinates_model.dart';
 import 'package:campus_mobile_experimental/ui/widgets/navigation/app_bar.dart';
-import 'package:campus_mobile_experimental/core/services/parking_service.dart';
 import 'package:provider/provider.dart';
 
 List<SingleChildCloneableWidget> providers = [
+  ...independentServices,
+  ...dependentServices,
+  ...uiConsumableProviders,
+];
+List<SingleChildCloneableWidget> independentServices = [
+  ChangeNotifierProvider<UserDataProvider>(
+    builder: (_) => UserDataProvider(),
+  ),
   ChangeNotifierProvider<BottomNavigationBarProvider>(
     builder: (_) => BottomNavigationBarProvider(),
-  ),
-  ChangeNotifierProvider<AvailabilityService>(
-    builder: (_) => AvailabilityService(),
   ),
   ChangeNotifierProvider<EventsService>(
     builder: (_) => EventsService(),
@@ -25,13 +30,22 @@ List<SingleChildCloneableWidget> providers = [
   ChangeNotifierProvider<CustomAppBar>(
     builder: (_) => CustomAppBar(),
   ),
+];
+List<SingleChildCloneableWidget> dependentServices = [
   ChangeNotifierProvider<DiningService>(
     builder: (_) => DiningService(),
   ),
-  ChangeNotifierProvider<ParkingService>(
-    builder: (_) => ParkingService(),
+  ChangeNotifierProxyProvider<UserDataProvider, ParkingDataProvider>(
+    initialBuilder: (_) {
+      var parkingDataProvider = ParkingDataProvider();
+      parkingDataProvider.fetchParkingLots();
+      return parkingDataProvider;
+    },
+    builder: (_, userDataProvider, parkingDataProvider) =>
+        parkingDataProvider..userDataProvider = userDataProvider,
   ),
-  ChangeNotifierProvider<AuthenticationService>(
-    builder: (_) => AuthenticationService(),
+  ChangeNotifierProvider<AvailabilityService>(
+    builder: (_) => AvailabilityService(),
   ),
 ];
+List<SingleChildCloneableWidget> uiConsumableProviders = [];
