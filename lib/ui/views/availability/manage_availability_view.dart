8bf1d8beed0528db1cee8da3b0fd8f1cefe81d5d
g9singh <g9singh@ucsd.edu>
2019-12-19 13:11:08 -0800

    update manage availability view to use availability data provider

--- lib/ui/views/availability/manage_availability_view.dart
+++ lib/ui/views/availability/manage_availability_view.dart
@@ -1,14 +1,14 @@
-import 'package:campus_mobile_experimental/core/services/availability_service.dart';
+import 'package:campus_mobile_experimental/core/data_providers/availability_data_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:campus_mobile_experimental/ui/widgets/container_view.dart';
 import 'package:campus_mobile_experimental/core/models/availability_model.dart';
 import 'package:provider/provider.dart';
 
 class ManageAvailabilityView extends StatelessWidget {
-  AvailabilityService _availabilityService;
+  AvailabilityDataProvider _availabilityDataProvider;
   @override
   Widget build(BuildContext context) {
-    _availabilityService = Provider.of<AvailabilityService>(context);
+    _availabilityDataProvider = Provider.of<AvailabilityDataProvider>(context);
     return ContainerView(
       child: buildLocationsList(context),
     );
@@ -25,15 +25,21 @@ class ManageAvailabilityView extends StatelessWidget {
     if (newIndex > oldIndex) {
       newIndex -= 1;
     }
-    List<AvailabilityModel> newOrder = _availabilityService.data;
+    List<AvailabilityModel> newOrder =
+        _availabilityDataProvider.availabilityModels;
     AvailabilityModel item = newOrder.removeAt(oldIndex);
     newOrder.insert(newIndex, item);
-    _availabilityService.data = newOrder;
+    List<String> orderedLocationNames = List<String>();
+    for (AvailabilityModel item in newOrder) {
+      orderedLocationNames.add(item.locationName);
+    }
+    _availabilityDataProvider.reorderLocations(orderedLocationNames);
   }
 
   List<Widget> createList(BuildContext context) {
     List<Widget> list = List<Widget>();
-    for (AvailabilityModel model in _availabilityService.data) {
+    for (AvailabilityModel model
+        in _availabilityDataProvider.availabilityModels) {
       list.add(ListTile(
         key: Key(model.locationId.toString()),
         title: Text(model.locationName),
