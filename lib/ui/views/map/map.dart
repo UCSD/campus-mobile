96086a7ad8a0d181561d14ad8127260d26cf293d
Atreto76 <AdvancedTrigonometry@protonmail.com>
2020-01-28 09:58:34 -0800

    Partial work

--- lib/ui/views/map/map.dart
+++ lib/ui/views/map/map.dart
@@ -3,6 +3,7 @@ import 'package:campus_mobile_experimental/core/models/map_search_model.dart';
 import 'package:campus_mobile_experimental/core/services/map_search_service.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class Maps extends StatefulWidget {
   @override
@@ -16,6 +17,7 @@ class _MapsState extends State<Maps> {
   List<MapSearchModel> _data;
   TextEditingController _searchBarController;
   bool _isSearching = false;
+  var location = new Location();
 
   final LatLng _center = const LatLng(32.8911637, -117.2428029);
 
@@ -45,9 +47,32 @@ class _MapsState extends State<Maps> {
       position: LatLng(_data[listIndex].mkrLat, _data[listIndex].mkrLong),
       infoWindow: InfoWindow(title: _data[listIndex].title),
     );
-    _markers.clear();
-    _markers[marker.markerId] = marker;
-    _mapController.animateCamera(CameraUpdate.newLatLng(marker.position));
+    setState(() {
+      _markers.clear();
+      _markers[marker.markerId] = marker;
+      _mapController.animateCamera(CameraUpdate.newLatLng(marker.position));
+    });
+  }
+
+  List<Widget> createTiles(BuildContext context) {
+    List<Widget> list = List<Widget>();
+    int listIndex = 0;
+    for (var i in _data) {
+      list.add(aLocation(i, context, listIndex++));
+    }
+    return ListTile.divideTiles(tiles: list, context: context).toList();
+  }
+
+  Widget aLocation(MapSearchModel data, BuildContext context, int index) {
+    return ListTile(
+      leading: Icon(Icons.location_on),
+      title: Text(data.title),
+      trailing: Text(' mi'),
+      onTap: () {
+        _addMarker(index);
+        Navigator.pop(context);
+      },
+    );
   }
 
   @override
@@ -128,8 +153,25 @@ class _MapsState extends State<Maps> {
                 alignment: Alignment.bottomCenter,
                 child: RaisedButton(
                   onPressed: () {
-                    Navigator.pushNamed(context, RoutePaths.MapLocationList,
-                        arguments: {'data': _data, 'addMarker': _addMarker});
+                    showModalBottomSheet(
+                      //isScrollControlled: true,
+                      context: context,
+                      builder: (context) => Column(
+                        children: <Widget>[
+                          Container(
+                            height: 50,
+                            child: Text('More Results'),
+                          ),
+                          Expanded(
+                            child: ListView(
+                              children: createTiles(context),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+//                    Navigator.pushNamed(context, RoutePaths.MapLocationList,
+//                        arguments: {'data': _data, 'addMarker': _addMarker});
                   },
                   color: Theme.of(context).buttonColor,
                   child: Text(
