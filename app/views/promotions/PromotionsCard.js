85f1769460461ee0da53ebc360fdcfd25141e47e
g9singh <g9singh@ucsd.edu>
2020-03-12 10:21:02 -0700

    add promotions card

---
+++ app/views/promotions/PromotionsCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View } from 'react-native'
+import { withNavigation } from 'react-navigation'
+import { openURL } from '../../util/general'
+import BannerCard from '../common/BannerCard'
+
+export const PromotionsCard = ({ navigation, promoFeed, saved, hideCard }) => (
+	<View>
+		<BannerCard
+			showButton={!(promoFeed['event-type'] === 'Promo Banner')}
+			title={(promoFeed.name) ? promoFeed.name : promoFeed.eventname}
+			image={(promoFeed.logo) ? promoFeed.logo : promoFeed['banner-image']}
+			onPress={() => {
+				if (promoFeed['event-type'] === 'Promo Banner') {
+					const array = promoFeed['banner-url'].split('://')
+					if (array[0] === 'app') {
+						navigation.navigate(array[1])
+					} else {
+						if ( promoFeed['banner-url'].length > 0 ) openURL(promoFeed['banner-url'])
+					}
+				}
+			}}
+			onClose={() => hideCard('specialEvents')}
+		/>
+	</View>
+)
+
+export default withNavigation(PromotionsCard)
