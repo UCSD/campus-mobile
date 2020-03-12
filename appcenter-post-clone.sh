c81b4d028d572a140917545a9e7eb186850304f5
Bryant, Charles <c3bryant@ucsd.edu>
2020-03-12 14:25:26 -0700

    Appcenter post clone debug p2

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
-
 echo "#### appcenter-post-clone START ####"
-
-# Install our version of Node
 set -ex
-node -v
-brew uninstall node@6
-NODE_VERSION="9.4.0"
-curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
-sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
 # Insert environment variables
 if [ "$BUILD_ENV" == "PROD" ];
