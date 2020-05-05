5320ad839b46f25e1bfe110f32e536c22f615da5
g9singh <g9singh@ucsd.edu>
2020-05-05 13:50:36 -0700

    updated endpoint for barcode scanner service and add refresh token mechanism

--- lib/core/services/barcode_service.dart
+++ lib/core/services/barcode_service.dart
@@ -6,8 +6,11 @@ class BarcodeService {
   String _error;
 
   final NetworkHelper _networkHelper = NetworkHelper();
-  final String _endpoint =
-      'https://api.ucsd.edu:8243/qrscanner/1.0.0/barcode';
+  final String _endpoint = 'https://api.ucsd.edu:8243/scandata/1.0.0/scanData';
+  final Map<String, String> headers = {
+    "accept": "application/json",
+    "Authorization": "",
+  };
 
   Future<bool> uploadResults(
       Map<String, String> headers, Map<String, dynamic> body) async {
@@ -23,10 +26,34 @@ class BarcodeService {
         throw (response.toString());
       }
     } catch (e) {
+      /// if the authorized fetch failed we know we have to refresh the
+      /// token for this service
+      if (e.response != null && e.response.statusCode == 401) {
+        if (await getNewToken()) {
+          return await uploadResults(headers, body);
+        }
+      }
       _error = e.toString();
       _isLoading = false;
+      return false;
     }
-    return false;
+  }
+
+  Future<bool> getNewToken() async {
+    final String tokenEndpoint = "https://api.ucsd.edu:8243/token";
+    final Map<String, String> tokenHeaders = {
+      "content-type": 'application/x-www-form-urlencoded',
+      "Authorization": "PUBLIC_AUTH_SERVICE_TOKEN_API_KEY_PH"
+    };
+    try {
+      var response = await _networkHelper.authorizedPost(
+          tokenEndpoint, tokenHeaders, "grant_type=client_credentials");
+      headers["Authorization"] = "Bearer " + response["access_token"];
+      return true;
+    } catch (e) {
+      _error = e.toString();
+      return false;
+    }
   }
 
   String get error => _error;
