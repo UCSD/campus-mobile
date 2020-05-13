2c84bff1eb5358ca4c25e5d4060188dc59f115b7
g9singh <g9singh@ucsd.edu>
2020-05-13 11:50:50 -0700

    update links card to point to card data provider

--- lib/ui/cards/links/links_card.dart
+++ lib/ui/cards/links/links_card.dart
@@ -1,6 +1,6 @@
 import 'package:campus_mobile_experimental/core/constants/app_constants.dart';
+import 'package:campus_mobile_experimental/core/data_providers/cards_data_provider.dart';
 import 'package:campus_mobile_experimental/core/data_providers/links_data_provider.dart';
-import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
 import 'package:campus_mobile_experimental/ui/views/links/links_list.dart';
 import 'package:campus_mobile_experimental/ui/reusable_widgets/card_container.dart';
 import 'package:flutter/material.dart';
@@ -16,8 +16,9 @@ class LinksCard extends StatelessWidget {
           Provider.of<LinksDataProvider>(context, listen: false).fetchLinks(),
       errorText: Provider.of<LinksDataProvider>(context).error,
       child: () => buildLinksCard(),
-      active: Provider.of<UserDataProvider>(context).cardStates['links'],
-      hide: () => Provider.of<UserDataProvider>(context).toggleCard('links'),
+      active: Provider.of<CardsDataProvider>(context).cardStates['links'],
+      hide: () => Provider.of<CardsDataProvider>(context, listen: false)
+          .toggleCard('links'),
       actionButtons: buildActionButtons(context),
     );
   }
