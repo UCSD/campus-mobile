5ded4ea59aa0a6ff8faf5676b4a6ec9618381eb8
Bryant, Charles <c3bryant@ucsd.edu>
2020-06-12 11:44:06 -0700

    Migrate student ID card core setup

---
+++ lib/core/data_providers/student_id_data_provider.dart
@@ -0,0 +1,56 @@
+import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
+import 'package:campus_mobile_experimental/core/models/student_id_barcode_model.dart';
+import 'package:campus_mobile_experimental/core/models/student_id_name_model.dart';
+import 'package:campus_mobile_experimental/core/models/student_id_photo_model.dart';
+import 'package:campus_mobile_experimental/core/models/student_id_profile_model.dart';
+import 'package:campus_mobile_experimental/core/services/student_id_service.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class StudentIdDataProvider extends ChangeNotifier {
+  StudentIdDataProvider() {
+    ///DEFAULT STATES
+    _isLoading = false;
+
+    ///INITIALIZE SERVICES
+    _studentIdService = StudentIdService();
+  }
+
+  ///STATES
+  bool _isLoading;
+  DateTime _lastUpdated;
+  String _error;
+  int _selectedCourse;
+
+  ///MODELS
+  StudentIdBarcodeModel _studentIdBarcodeModel;
+  StudentIdNameModel _studentIdNameModel;
+  StudentIdPhotoModel _studentIdPhotoModel;
+  StudentIdProfileModel _studentIdProfileModel;
+
+
+  UserDataProvider _userDataProvider;
+
+  ///SERVICES
+  StudentIdService _studentIdService;
+
+  void fetchData() async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    print("fetchData-----------");
+
+  }
+
+
+  ///SIMPLE GETTERS
+  bool get isLoading => _isLoading;
+  String get error => _error;
+  DateTime get lastUpdated => _lastUpdated;
+  StudentIdBarcodeModel get studentIdBarcodeModel => _studentIdBarcodeModel;
+  StudentIdNameModel get studentIdNameModel => _studentIdNameModel;
+  StudentIdPhotoModel get studentIdPhotoModel => _studentIdPhotoModel;
+  StudentIdProfileModel get studentIdProfileModel => _studentIdProfileModel;
+  int get selectedCourse => _selectedCourse;
+}
---
+++ lib/ui/cards/student_id/student_id_card.dart
@@ -0,0 +1,44 @@
+import 'package:campus_mobile_experimental/core/constants/app_constants.dart';
+import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
+import 'package:campus_mobile_experimental/core/models/weather_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:campus_mobile_experimental/ui/reusable_widgets/card_container.dart';
+
+import 'package:provider/provider.dart';
+import 'package:campus_mobile_experimental/core/data_providers/weather_data_provider.dart';
+
+const String WEATHER_ICON_BASE_URL =
+    'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/images/v1/weather-icons/';
+
+class StudentIdCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CardContainer(
+      /// TODO: need to hook up hidden to state using provider
+      active: Provider.of<UserDataProvider>(context).cardStates['student_id'],
+      hide: () => Provider.of<UserDataProvider>(context, listen: false)
+          .toggleCard('student_id'),
+      reload: () => Provider.of<StudentIdDataProvider>(context, listen: false)
+          .fetchWeather(),
+      isLoading: Provider.of<StudentIdDataProvider>(context).isLoading,
+      title: buildTitle(),
+      errorText: Provider.of<StudentIdDataProvider>(context).error,
+      child: () => buildCardContent(
+          Provider.of<StudentIdDataProvider>(context).studentIdModel),
+    );
+  }
+
+  Widget buildTitle() {
+    return Text(
+      "Student ID",
+      textAlign: TextAlign.left,
+    );
+  }
+
+  Widget buildCardContent(WeatherModel data) {
+    return Column(children: <Widget>[
+      Text('Student ID Card content'),
+    ]);
+  }
+}
