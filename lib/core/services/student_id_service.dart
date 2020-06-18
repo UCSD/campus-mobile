1e7220c9e3d8fb9bde087eb36328ac9a5bf03525
Peter Gonzalez <p8gonzal@ucsd.edu>
2020-06-18 16:27:57 -0700

    Updating student service

--- lib/core/services/student_id_service.dart
+++ lib/core/services/student_id_service.dart
@@ -8,7 +8,7 @@ import 'package:campus_mobile_experimental/core/services/networking.dart';
 import 'package:flutter/cupertino.dart';
 
 class StudentIdService {
-  final String MY_STUDENT_PROFILE_API_URL = 'https://api.ucsd.edu:8243/student/my/v1';
+  final String MY_STUDENT_PROFILE_API_URL = 'https://api.ucsd.edu: 243/student/my/v1';
   final String MY_STUDENT_CONTACT_API_URL = 'https://api.ucsd.edu:8243/student/my/student_contact_info/v1';
 
   bool _isLoading = false;
