01f4933a1f658d9f5dfd613e563550ee0d156fb0
Mihir Gupta <m08.in.us@gmail.com>
2020-06-18 10:08:51 -0700

    updated student_id_card.dart

--- lib/ui/cards/student_id/student_id_card.dart
+++ lib/ui/cards/student_id/student_id_card.dart
@@ -1,32 +1,40 @@
 import 'package:campus_mobile_experimental/core/constants/app_constants.dart';
+import 'package:campus_mobile_experimental/core/data_providers/cards_data_provider.dart';
 import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
-import 'package:campus_mobile_experimental/core/models/weather_model.dart';
+import 'package:campus_mobile_experimental/core/models/student_id_barcode_model.dart';
+import 'package:campus_mobile_experimental/core/models/student_id_name_model.dart';
+import 'package:campus_mobile_experimental/core/models/student_id_photo_model.dart';
+import 'package:campus_mobile_experimental/core/models/student_id_profile_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:campus_mobile_experimental/ui/reusable_widgets/card_container.dart';
 
 import 'package:provider/provider.dart';
 import 'package:campus_mobile_experimental/core/data_providers/student_id_data_provider.dart';
-import 'package:campus_mobile_experimental/core/data_providers/weather_data_provider.dart';
 
-const String WEATHER_ICON_BASE_URL =
-    'https://s3-us-west-2.amazonaws.com/ucsd-its-wts/images/v1/weather-icons/';
+
 
 class StudentIdCard extends StatelessWidget {
+
+  String cardId = "student_id";
+
   @override
   Widget build(BuildContext context) {
     return CardContainer(
       /// TODO: need to hook up hidden to state using provider
-      active: Provider.of<UserDataProvider>(context).cardStates['student_id'],
-      hide: () => Provider.of<UserDataProvider>(context, listen: false)
-          .toggleCard('student_id'),
+      active: Provider.of<CardsDataProvider>(context).cardStates[cardId],
+      hide: () => Provider.of<CardsDataProvider>(context, listen: false)
+          .toggleCard(cardId),
       reload: () => Provider.of<StudentIdDataProvider>(context, listen: false)
-          .fetchWeather(),
+          .fetchData(),
       isLoading: Provider.of<StudentIdDataProvider>(context).isLoading,
-      title: buildTitle(),
+      titleText: CardTitleConstants.titleMap[cardId],
       errorText: Provider.of<StudentIdDataProvider>(context).error,
       child: () => buildCardContent(
-          Provider.of<StudentIdDataProvider>(context).studentIdModel),
+          Provider.of<StudentIdDataProvider>(context).studentIdBarcodeModel,
+          Provider.of<StudentIdDataProvider>(context).studentIdNameModel,
+          Provider.of<StudentIdDataProvider>(context).studentIdPhotoModel,
+          Provider.of<StudentIdDataProvider>(context).studentIdProfileModel),
     );
   }
 
@@ -37,7 +45,8 @@ class StudentIdCard extends StatelessWidget {
     );
   }
 
-  Widget buildCardContent(WeatherModel data) {
+  Widget buildCardContent(StudentIdBarcodeModel barcodeModel, StudentIdNameModel nameModel, StudentIdPhotoModel photoModel,
+                            StudentIdProfileModel profileModel) {
     return Column(children: <Widget>[
       Text('Student ID Card content'),
     ]);
