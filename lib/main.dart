1b42e009bb57c1a95b7cf29246dc948a0de0e1a2
Atreto76 <AdvancedTrigonometry@protonmail.com>
2020-06-25 09:19:36 -0700

    Set enableInDevMode for Crashlytics to true

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ void main() async {
 
   await initializeStorage();
 
+  Crashlytics.instance.enableInDevMode = true;
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runApp(CampusMobile());
