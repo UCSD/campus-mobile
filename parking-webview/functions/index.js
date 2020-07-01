6f0aec47f5a4a107fa7a6ebbf4287ac598891a4a
sssaiya <sssaiya@ucsd.edu>
2020-07-01 14:50:41 -0700

    Add get request for data

--- parking-webview/functions/index.js
+++ parking-webview/functions/index.js
@@ -1,38 +1,72 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const app = express();
 
 app.set("view engine", "ejs");
 
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
-// http://localhost:5000/parking?lot=Athena&spots=A,B,C,D
-// http://localhost:5000/parking?lot=Athena
-app.get("/parking", (req, res) => {
-  if (req.query.lot) app.locals.lot = req.query.lot;
-  else app.locals.lot = undefined;
+//localhost:5000/parking?lot=P406&spots=A,B,S
+app.get("/parking", function (req, res) {
+  let loc = app.locals
+  loc.lotName = undefined;
+  loc.lotContext = undefined;
+  loc.totalSpaces = undefined;
+  loc.numSpots = undefined;
+  loc.spot0 = undefined;
+  loc.spot1 = undefined;
+  loc.spot2 = undefined;
+  if (!req.query.lot) {
+    res.render("parking");
+  }
+  let lotID = req.query.lot;
+  const url =
+    "https://4pefyt8qv7.execute-api.us-west-2.amazonaws.com/dev/parking/v1.1/status/" +
+    lotID;
+  var client = new HttpClient();
+  client.get(url, (response) => {
+    var lotInfo = JSON.parse(response);
+    loc.lotName = lotInfo["LocationName"];
+    loc.lotContext = lotInfo["LocationContext"];
+    var availability = lotInfo["Availability"];
+    console.log(availability);
 
-  if (req.query.spots) {
+    var totalSpots = 0;
+
+    loc.totalSpaces = 0;
+
+    if (!req.query.spots) {
+      res.render("parking");
+    }
     str = req.query.spots;
     const spotTypes = str.split(",");
 
-    console.log(spotTypes.length)
+    console.log(spotTypes.length);
 
-    app.locals.numSpots = spotTypes.length;
+    loc.numSpots = spotTypes.length;
 
-    if (spotTypes[0]) {
-      app.locals.spot0 = spotTypes[0];
-    } else app.locals.spot0 = undefined;
-    if (spotTypes[1]) {
-      app.locals.spot1 = spotTypes[1];
-    } else app.locals.spot1 = undefined;
-    if (spotTypes[2]) {
-      app.locals.spot2 = spotTypes[2];
-    } else app.locals.spot2 = undefined;
-  }
+    if (spotTypes[0]) loc.spot0 = spotTypes[0];
+    if (spotTypes[1]) loc.spot1 = spotTypes[1];
+    if (spotTypes[2]) loc.spot2 = spotTypes[2];
 
-  res.render("parking");
+    res.render("parking");
+  });
 });
+
+// Resusable code to make an XML HTTP request
+var HttpClient = function () {
+  this.get = function (aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function () {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.responseText);
+    };
+
+    anHttpRequest.open("GET", aUrl, true);
+    anHttpRequest.send(null);
+  };
+};
 
 exports.app = functions.https.onRequest(app);
