67f1882cff6d37f7d089149304d1eb96e9513dab
Atreto76 <AdvancedTrigonometry@protonmail.com>
2020-07-09 13:54:34 -0700

    Remove set height for base home cards

--- lib/ui/cards/availability/availability_card.dart
+++ lib/ui/cards/availability/availability_card.dart
@@ -55,19 +55,23 @@ class _AvailabilityCardState extends State<AvailabilityCard> {
 
     return Column(
       children: <Widget>[
-        Flexible(
+        Container(
+          height: 250,
           child: PageView(
             controller: _controller,
             children: locationsList,
           ),
         ),
-        DotsIndicator(
-          controller: _controller,
-          itemCount: locationsList.length,
-          onPageSelected: (int index) {
-            _controller.animateToPage(index,
-                duration: Duration(seconds: 1), curve: Curves.ease);
-          },
+        Container(
+          height: 15,
+          child: DotsIndicator(
+            controller: _controller,
+            itemCount: locationsList.length,
+            onPageSelected: (int index) {
+              _controller.animateToPage(index,
+                  duration: Duration(seconds: 1), curve: Curves.ease);
+            },
+          ),
         ),
       ],
     );
