a2fb3da7b352b48505f87b9aaa772bf37d5adb8c
Mihir Gupta <m08.in.us@gmail.com>
2020-07-16 11:22:41 -0700

    updated UI to match current UI

--- lib/ui/views/scanner/material_barcode_scanner.dart
+++ lib/ui/views/scanner/material_barcode_scanner.dart
@@ -11,6 +11,7 @@ import 'package:path_provider/path_provider.dart';
 
 import 'colors.dart';
 import 'scanner_utils.dart';
+import 'package:campus_mobile_experimental/ui/theme/app_theme.dart';
 
 enum AnimationState { search, barcodeNear, barcodeFound, endSearch }
 
@@ -378,73 +379,94 @@ class _MaterialBarcodeScannerState extends State<MaterialBarcodeScanner>
       );
     }
 
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            background,
-            Container(
-              constraints: const BoxConstraints.expand(),
-              child: CustomPaint(
-                painter: WindowPainter(
-                  windowSize: Size(widget.validRectangle.width,
-                      widget.validRectangle.height),
-                  outerFrameColor: widget.frameColor,
-                  closeWindow: _closeWindow,
-                  innerFrameColor: _currentState == AnimationState.endSearch
-                      ? Colors.transparent
-                      : kShrineFrameBrown,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 0,
-              right: 0,
-              top: 0,
-              child: Container(
-                height: 56,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: const <Color>[Colors.black87, Colors.transparent],
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              left: 0.0,
-              bottom: 0.0,
-              right: 0.0,
-              height: 56,
-              child: Container(
-                color: Color(0xFF182B49),
-                child: Center(
-                  child: Text(
-                    !_hasScanned ? 'Point camera at barcode' : _barcode,
-                    style: Theme.of(context).textTheme.button,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              constraints: const BoxConstraints.expand(),
-              child: CustomPaint(
-                painter: _animationPainter,
-              ),
-            ),
-            AppBar(
-              title:Text("Test Kit Scanner"),
-              backgroundColor: Colors.transparent,
-              leading: IconButton(
-                icon: const Icon(Icons.close, color: Colors.white),
-                onPressed: () => Navigator.of(context).pop(),
-              ),
-              elevation: 0.0,
-            ),
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("Test Kit Scanner"),
+        backgroundColor: Color(0xFF182B49),
+        leading: IconButton(
+          icon: const Icon(Icons.close, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
         ),
+        elevation: 0.0,
       ),
+      body: Stack(
+        children: <Widget>[
+          background,
+          Container(
+            constraints: const BoxConstraints.expand(),
+            child: CustomPaint(
+              painter: WindowPainter(
+                windowSize: Size(widget.validRectangle.width,
+                    widget.validRectangle.height),
+                outerFrameColor: widget.frameColor,
+                closeWindow: _closeWindow,
+                innerFrameColor: _currentState == AnimationState.endSearch
+                    ? Colors.transparent
+                    : kShrineFrameBrown,
+              ),
+            ),
+          ),
+            Container(
+            constraints: const BoxConstraints.expand(),
+            child: CustomPaint(
+              painter: _animationPainter,
+            ),
+          ),
+          Align(
+            alignment:FractionalOffset.bottomCenter,
+            child: ConstrainedBox(
+                constraints: BoxConstraints.loose(Size(MediaQuery.of(context).size.width, 0.3 * MediaQuery.of(context).size.height)),
+                child: Container(
+                  color: Colors.white,
+                  child: SingleChildScrollView(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          _hasScanned
+                              ? Padding(
+                            padding: const EdgeInsets.only(top: 20.0, bottom: 4.0),
+                            child: Text(
+                              "Submit",
+                              style: TextStyle( color: Colors.black, fontSize: 18.0 ),
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                              : Padding(
+                            padding: const EdgeInsets.only(top:20.0, bottom: 4.0),
+                            child: Text(
+                              "Scan a test kit.",
+                              style: TextStyle( color: Colors.black, fontSize: 18.0 ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              Container(
+                                margin: EdgeInsets.all(16.0),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(bottom:16.0),
+                                  child: FlatButton(
+                                    disabledTextColor: Colors.black,
+                                    disabledColor: Color.fromRGBO(218, 218, 218, 1.0),
+                                    onPressed: (){},
+                                    child: Text("Submit"),
+                                    color: ColorPrimary,
+                                    textColor: lightTextColor,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ]),
+                  ),
+                ),
+              ),
+          ),
+        ],
+
+    ),
     );
   }
 }
