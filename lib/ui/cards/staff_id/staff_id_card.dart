71e17e3ae557a988bb1adf189d28dd3499bcf432
Kevin De Silva <kevindj01@gmail.com>
2020-07-22 12:16:10 -0700

    added test query string for token, token should display instead of name on ID card

--- lib/ui/cards/staff_id/staff_id_card.dart
+++ lib/ui/cards/staff_id/staff_id_card.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 import 'package:campus_mobile_experimental/core/constants/app_constants.dart';
 import 'package:campus_mobile_experimental/core/data_providers/cards_data_provider.dart';
+import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
 import 'package:campus_mobile_experimental/ui/reusable_widgets/card_container.dart';
 import 'package:campus_mobile_experimental/ui/theme/app_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
 
 class StaffIdCard extends StatefulWidget {
   StaffIdCard();
@@ -32,21 +32,22 @@ class _StaffIdCardState extends State<StaffIdCard> {
     );
   }
 
-  //final _url = "https://stage-cwo-test.ucsd.edu/WebCards/staff_id.html";
-  //final _url = "file:///Users/kevindesilva/Downloads/staff_id%20(1).html";
   final _url = "https://cwo-test.ucsd.edu/WebCards/staff_id.html";
+  UserDataProvider _userDataProvider = UserDataProvider();
 
   Widget buildCardContent(BuildContext context) {
-    var nameQueryString = "name=Joe%20Employee";
-    var affiliationQueryString = "affiliation=Staff";
-    var idNumQueryString = "id=1111111111111";
-    var url = _url +
-        "?" +
-        nameQueryString +
-        "&" +
-        affiliationQueryString +
-        "&" +
-        idNumQueryString;
+    /// Verify that user is logged in
+    if (_userDataProvider.isLoggedIn) {
+      /// Initialize header
+      final Map<String, String> header = {
+        'Authorization':
+            'Bearer ${_userDataProvider?.authenticationModel?.accessToken}'
+      };
+    }
+    var tokenQueryString =
+        "token=" + '${_userDataProvider.authenticationModel.accessToken}';
+    print(tokenQueryString);
+    var url = _url + "?" + tokenQueryString; // +
     return Column(
       children: <Widget>[
         Flexible(
