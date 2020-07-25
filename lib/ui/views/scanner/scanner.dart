ceb4a04ccfd10b2d1aa38aab3ff165192bd93ae8
Mihir Gupta <m08.in.us@gmail.com>
2020-07-24 17:38:40 -0700

    submission json is ready to be sent to scanner api

--- lib/ui/views/scanner/scanner.dart
+++ lib/ui/views/scanner/scanner.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 import 'dart:io' show Platform;
 
 import 'package:barcode_scan/barcode_scan.dart';
+import 'package:campus_mobile_experimental/core/constants/app_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:campus_mobile_experimental/ui/theme/app_theme.dart';
-import 'package:campus_mobile_experimental/ui/theme/app_theme.dart';
-
+import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:campus_mobile_experimental/core/services/barcode_service.dart';
 
 
 class Scanner extends StatefulWidget {
@@ -16,7 +18,8 @@ class Scanner extends StatefulWidget {
 
 class _ScannerState extends State<Scanner> {
   ScanResult scanResult;
-
+  BarcodeService _barcodeService;
+  UserDataProvider _userDataProvider;
   final _flashOnController = TextEditingController(text: "Flash on");
   final _flashOffController = TextEditingController(text: "Flash off");
   final _cancelController = TextEditingController(text: "Cancel");
@@ -28,9 +31,12 @@ class _ScannerState extends State<Scanner> {
   var _autoEnableFlash = false;
   bool _hasScanned = false;
   String _barcode = "";
+  bool _submitted = false;
   BuildContext scaffoldContext;
+  var ucsdAffiliation = "";
+  var accessToken = "";
 
-
+  set userDataProvider(UserDataProvider value) => _userDataProvider = value;
   //all types of barcodes this library supports
   //will only support the required format when released, supports all for testing purposes
   static final _possibleFormats = BarcodeFormat.values.toList()
@@ -71,24 +77,80 @@ class _ScannerState extends State<Scanner> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               !_hasScanned
-              ? Center(
-                child: OutlineButton(
-                  borderSide: BorderSide(color: Colors.white),
-                  onPressed: scan,
-                  child: Text("Start scan.", style: TextStyle(color: Colors.white)),
-                ),
-              )
-              : Center(
-                child: OutlineButton(
-                onPressed: scan,
-                child: Text(_barcode, style: TextStyle(color: Colors.white)),
-                  ),
-                ),
+              ? buildStartScan()
+              : buildSubmitScan()
             ],
           ),
         ),
       )
    );
+  }
+
+  Widget buildStartScan() {
+    return(
+        Center(
+          child: OutlineButton(
+            borderSide: BorderSide(color: Colors.white),
+            onPressed: scan,
+            child: Text("Start scan", style: TextStyle(color: Colors.white)),
+          ),
+        )
+    );
+  }
+
+  Widget buildSubmitScan() {
+    _userDataProvider = Provider.of<UserDataProvider>(context);
+    this.setState(() {
+      ucsdAffiliation = _userDataProvider.authenticationModel.ucsdaffiliation;
+      accessToken = _userDataProvider.authenticationModel.accessToken;
+    });
+    return(
+        Column(
+          children: [
+            Text(_barcode, style: TextStyle(color: Colors.white)),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: OutlineButton(
+                borderSide: BorderSide(color: Colors.white),
+                onPressed: submit,
+                child: Text("Submit", style: TextStyle(color: Colors.white)),
+              ),
+            )
+          ],
+        )
+
+        );
+  }
+
+    Map<String,dynamic> createUserData() {
+    print("affiliation: " + ucsdAffiliation.toString());
+    return{
+      'barcode': _barcode,
+      'ucsdaffiliation': ucsdAffiliation
+    };
+  }
+
+  Future<void> submit() async {
+    print("in submit");
+    var headers = {
+      "Content-Type": "application/json",
+      'Authorization':
+      'Bearer ${accessToken}'
+    };
+    var data = createUserData();
+    print(headers.toString());
+    print(data.toString());
+//    var results = await _barcodeService.uploadResults(headers, data);
+//    if (results) {
+//      _submitted = true;
+//    } else {
+//      if (_barcodeService.error.contains(ErrorConstants.invalidBearerToken)) {
+//        await _userDataProvider.refreshToken();
+//      } else {
+//      }
+//      _submitted = true;
+//    }
+//    setState(() {});
   }
 
   Future scan() async {
