44873fbc4c1cfed189a7e5d154aed521b81b7662
Mihir Gupta <m08.in.us@gmail.com>
2020-07-29 10:06:35 -0700

    now using pub.dev plugin, not fork

--- lib/ui/views/scanner/scanner_view.dart
+++ lib/ui/views/scanner/scanner_view.dart
@@ -6,7 +6,6 @@ import 'package:campus_mobile_experimental/ui/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
-//import 'package:qr_code_scanner/qr_scanner_overlay_shape.dart';
 
 class ScannerView extends StatefulWidget {
   const ScannerView({
