b4d350dad929f211d09b110d1afdd71aeeceb5c0
Mihir Gupta <m08.in.us@gmail.com>
2020-08-05 17:05:27 -0700

    default card list should work

--- lib/core/services/cards_service.dart
+++ lib/core/services/cards_service.dart
@@ -1,9 +1,13 @@
 import 'package:campus_mobile_experimental/core/models/cards_model.dart';
+import 'package:campus_mobile_experimental/core/models/user_profile_model.dart';
 import 'package:campus_mobile_experimental/core/services/networking.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
+
 
 class CardsService {
-  final String cardListEndpoint =
-      'https://mobile.ucsd.edu/replatform/v1/qa/cards-v4.json';
+
 
   bool _isLoading = false;
   DateTime _lastUpdated;
@@ -13,13 +17,16 @@ class CardsService {
 
   final NetworkHelper _networkHelper = NetworkHelper();
 
-  Future<bool> fetchCards() async {
+  Future<bool> fetchCards(String ucsdAffiliation) async {
+    String cardListEndpoint =
+        'https://rj786p8erh.execute-api.us-west-2.amazonaws.com/qa/defaultcards';
     _error = null;
     _isLoading = true;
     try {
+      //form query string with ucsd affiliation
+      cardListEndpoint += "?ucsdaffiliation=${ucsdAffiliation}";
       /// fetch data
       String _response = await _networkHelper.fetchData(cardListEndpoint);
-
       /// parse data
       _cardsModel = cardsModelFromJson(_response);
       _isLoading = false;
