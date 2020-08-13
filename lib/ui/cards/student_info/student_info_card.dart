e446cfccaecad4ac961cc47a5c412efab1983e38
Kevin De Silva <kevindj01@gmail.com>
2020-08-13 12:36:54 -0700

    improved dynamic webcard height resizing, more efficient method that doesnt require any JS

--- lib/ui/cards/student_info/student_info_card.dart
+++ lib/ui/cards/student_info/student_info_card.dart
@@ -4,6 +4,7 @@ import 'package:campus_mobile_experimental/core/data_providers/user_data_provide
 import 'package:campus_mobile_experimental/ui/reusable_widgets/card_container.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:campus_mobile_experimental/ui/theme/app_layout.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -31,10 +32,9 @@ class _StudentInfoCardState extends State<StudentInfoCard> {
     );
   }
 
-  double _contentHeight = 0;
+  double _contentHeight = cardContentMinHeight;
 
-  final _url =
-      "https://mobile.ucsd.edu/replatform/v1/qa/webview/student_info.html";
+  final _url = "https://cwo-test.ucsd.edu/WebCards/student_info_kevin.html";
 
   @override
   void didChangeDependencies() {
@@ -58,19 +58,30 @@ class _StudentInfoCardState extends State<StudentInfoCard> {
     var tokenQueryString =
         "token=" + '${_userDataProvider.authenticationModel.accessToken}';
     var url = _url + "?" + tokenQueryString;
+    print(url);
     return Container(
         height: _contentHeight,
         child: WebView(
-          javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: url,
-          onWebViewCreated: (controller) {
-            _webViewController = controller;
-          },
-          javascriptChannels: <JavascriptChannel>[
-            _heightJavascriptChannel(context),
-            _printJavascriptChannel(context),
-          ].toSet(),
-        ));
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: url,
+            onWebViewCreated: (controller) {
+              _webViewController = controller;
+            },
+            javascriptChannels: <JavascriptChannel>[
+              _printJavascriptChannel(context),
+            ].toSet(),
+            onPageFinished: (some) async {
+              double height = double.parse(await _webViewController
+                  .evaluateJavascript("document.documentElement.offsetHeight"));
+              setState(() {
+                if (height >= cardContentMinHeight &&
+                    height <= cardContentMaxHeight) {
+                  _contentHeight = double.parse(height.toString());
+                }
+                print("\n\nHeight - " + height.toString());
+                print(_contentHeight);
+              });
+            }));
   }
 
   //Channel to obtain links and open them in new browser
@@ -79,20 +90,7 @@ class _StudentInfoCardState extends State<StudentInfoCard> {
       name: 'CampusMobile',
       onMessageReceived: (JavascriptMessage message) {
         openLink(message.message);
-      },
-    );
-  }
-
-  //Channel to obtain body height via Jquery and assign it as the height of the container
-  JavascriptChannel _heightJavascriptChannel(BuildContext context) {
-    return JavascriptChannel(
-      name: 'GetHeight',
-      onMessageReceived: (JavascriptMessage message) {
-        print(message.message); //print messages to check, remove if not needed
-        setState(() {
-          _contentHeight = double.parse(message.message);
-          print(_contentHeight); //print messages to check, remove if not needed
-        });
+        print(message.message);
       },
     );
   }
--- lib/ui/theme/app_layout.dart
+++ lib/ui/theme/app_layout.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
 
 // Card Layout
 const cardMargin = 6.0;
@@ -7,3 +8,6 @@ const cardPaddingInner = 8.0;
 const cardMinHeight = 60.0;
 const listTileInnerPadding = 8.0;
 
+//Card Heights
+const cardContentMinHeight = 40.0;
+const cardContentMaxHeight = 568.0;
