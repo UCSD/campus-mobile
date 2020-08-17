aa4ba9a8825ba0c9ae865ac9d204678728542c63
Kevin De Silva <kevindj01@gmail.com>
2020-08-17 08:52:19 -0700

    Added dyamic webcard resizing to new branch and removal of print statements

--- lib/ui/cards/student_info/student_info_card.dart
+++ lib/ui/cards/student_info/student_info_card.dart
@@ -4,6 +4,7 @@ import 'package:campus_mobile_experimental/core/data_providers/user_data_provide
 import 'package:campus_mobile_experimental/ui/reusable_widgets/card_container.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:campus_mobile_experimental/ui/theme/app_layout.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -31,8 +32,9 @@ class _StudentInfoCardState extends State<StudentInfoCard> {
     );
   }
 
-  final _url =
-      "https://mobile.ucsd.edu/replatform/v1/qa/webview/student_info.html";
+  double _contentHeight = cardContentMinHeight;
+
+  final _url = "https://cwo-test.ucsd.edu/WebCards/student_info_kevin.html";
 
   @override
   void didChangeDependencies() {
@@ -56,23 +58,30 @@ class _StudentInfoCardState extends State<StudentInfoCard> {
     var tokenQueryString =
         "token=" + '${_userDataProvider.authenticationModel.accessToken}';
     var url = _url + "?" + tokenQueryString;
-    return Column(
-      children: <Widget>[
-        Flexible(
-            child: WebView(
-          javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: url,
-          onWebViewCreated: (controller) {
-            _webViewController = controller;
-          },
-          javascriptChannels: <JavascriptChannel>[
-            _printJavascriptChannel(context),
-          ].toSet(),
-        )),
-      ],
-    );
+    return Container(
+        height: _contentHeight,
+        child: WebView(
+            javascriptMode: JavascriptMode.unrestricted,
+            initialUrl: url,
+            onWebViewCreated: (controller) {
+              _webViewController = controller;
+            },
+            javascriptChannels: <JavascriptChannel>[
+              _printJavascriptChannel(context),
+            ].toSet(),
+            onPageFinished: (some) async {
+              double height = double.parse(await _webViewController
+                  .evaluateJavascript("document.documentElement.offsetHeight"));
+              setState(() {
+                if (height >= cardContentMinHeight &&
+                    height <= cardContentMaxHeight) {
+                  _contentHeight = double.parse(height.toString());
+                }
+              });
+            }));
   }
 
+  //Channel to obtain links and open them in new browser
   JavascriptChannel _printJavascriptChannel(BuildContext context) {
     return JavascriptChannel(
       name: 'CampusMobile',
