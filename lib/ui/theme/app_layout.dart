75fab56b2831c5c3b053eb2cb23f51cd1e6aabe7
Kevin De Silva <kevindj01@gmail.com>
2020-08-17 09:36:47 -0700

    added webcard dynamic height resizing

--- lib/ui/theme/app_layout.dart
+++ lib/ui/theme/app_layout.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 // Card Layout
 const cardMargin = 6.0;
 const cardPaddingInner = 8.0;
 const cardMinHeight = 60.0;
 const listTileInnerPadding = 8.0;
 
+//Card Heights
+const cardContentMinHeight = 40.0;
+const cardContentMaxHeight = 568.0;
