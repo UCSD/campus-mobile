351386327b3ae88a756b4e6f4ee1d36f148e1082
sssaiya <sssaiya@ucsd.edu>
2020-09-22 00:11:16 +0530

    Fix data loading issue

--- lib/core/data_providers/parking_data_provider.dart
+++ lib/core/data_providers/parking_data_provider.dart
@@ -9,7 +9,8 @@ import 'package:campus_mobile_experimental/core/constants/default_parking_consta
 class ParkingDataProvider extends ChangeNotifier {
   ParkingDataProvider() {
     ///DEFAULT STATES
-    _isLoading = false;
+    _isLoadingLots = false;
+    _isLoadingSpots = false;
 
     _parkingService = ParkingService();
     _spotTypesService = SpotTypesService();
@@ -18,7 +19,8 @@ class ParkingDataProvider extends ChangeNotifier {
   UserDataProvider _userDataProvider;
 
   ///STATES
-  bool _isLoading;
+  bool _isLoadingLots;
+  bool _isLoadingSpots;
   DateTime _lastUpdated;
   String _error;
   int selected_lots, selected_spots;
@@ -38,7 +40,7 @@ class ParkingDataProvider extends ChangeNotifier {
   /// FETCH PARKING LOT DATA AND SYNC THE ORDER IF USER IS LOGGED IN
   /// TODO: make sure to remove any lots the user has selected and are no longer available
   void fetchParkingLots() async {
-    _isLoading = true;
+    _isLoadingLots = true;
     selected_lots = 0;
     _error = null;
     notifyListeners();
@@ -77,12 +79,12 @@ class ParkingDataProvider extends ChangeNotifier {
       ///TODO: determine what error to show to the user
       _error = _parkingService.error;
     }
-    _isLoading = false;
+    _isLoadingLots = false;
     notifyListeners();
   }
 
   void fetchSpotTypes() async {
-    _isLoading = true;
+    _isLoadingSpots = true;
     selected_spots = 0;
     _error = null;
     notifyListeners();
@@ -116,7 +118,7 @@ class ParkingDataProvider extends ChangeNotifier {
       _error = _spotTypesService.error;
     }
 
-    _isLoading = false;
+    _isLoadingSpots = false;
     notifyListeners();
   }
 
@@ -128,7 +130,7 @@ class ParkingDataProvider extends ChangeNotifier {
     return List<ParkingModel>();
   }
 
-    SpotTypeModel get spotTypeModels {
+  SpotTypeModel get spotTypeModel {
     if (_spotTypeModel != null) {
       return _spotTypeModel;
     }
@@ -177,7 +179,7 @@ class ParkingDataProvider extends ChangeNotifier {
   }
 
   ///SIMPLE GETTERS
-  bool get isLoading => _isLoading;
+  bool get isLoading => _isLoadingSpots || _isLoadingLots;
   String get error => _error;
   DateTime get lastUpdated => _lastUpdated;
   Map<String, bool> get spotTypesState => _selectedSpotTypesState;
