832bf5f4182bd15411b09165f113677f9bd9ee43
Mihir Gupta <m08.in.us@gmail.com>
2020-10-16 11:55:19 -0700

    added client-side checking as well as submission successful screen

--- lib/ui/cards/scanner/scanner_view.dart
+++ lib/ui/cards/scanner/scanner_view.dart
@@ -1,5 +1,13 @@
+
+import 'package:campus_mobile_experimental/core/constants/app_constants.dart';
+import 'package:campus_mobile_experimental/core/data_providers/user_data_provider.dart';
+import 'package:campus_mobile_experimental/core/services/barcode_service.dart';
+import 'package:campus_mobile_experimental/ui/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_scandit_plugin/flutter_scandit_plugin.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
+
 
 class ScanditScanner extends StatefulWidget {
   @override
@@ -9,41 +17,49 @@ class ScanditScanner extends StatefulWidget {
 class _ScanditScannerState extends State<ScanditScanner> {
   String _message = '';
   ScanditController _controller;
-
+  bool hasScanned;
+  bool hasSubmitted;
+  bool didError;
+  String licenseKey;
+  BarcodeService _barcodeService = new BarcodeService();
+  UserDataProvider _userDataProvider;
+  set userDataProvider(UserDataProvider value) => _userDataProvider = value;
+  var ucsdAffiliation = "";
+  var accessToken = "";
+  String _barcode;
+  bool isLoading;
+  bool successfulSubmission;
 
   @override
   void initState() {
     super.initState();
+    hasScanned = false;
+    hasSubmitted = false;
+    didError = false;
+    successfulSubmission = false;
+    isLoading = false;
   }
 
   @override
   Widget build(BuildContext context) {
+    _userDataProvider = Provider.of<UserDataProvider>(context);
+    if(Theme.of(context).platform == TargetPlatform.iOS) {
+      licenseKey = "AcUfBiaXOVIQCeCwChcl05QwFCPbKhuZ5DFFHEdC7zjHZYV/XnS2CZJ5D9dnJcBTDW+Jh6RMOSJmXnJ8KywJjb9AHZNUU+j3MAZZX6hHx9fzc4UT4CAHEPRw2uVAIDBuKD8ozkMfw3qfOIanzJAM4oHLaUyHj9Qvxbh6eWOr/llZDzdB+mTTkUGW4ibe4XAhafCl7Y/pmyyS3KhREOJVIiMp8j0UoIjidTqBGq73dF9Ai2yXJKpAARXu4FAarFfWUoHIL2810iaDkpXhauSEv9KhCzWclhXFc/wD7kE9rfUYbq4gl0Wf3w7YTtvvaY+WeHYVvO4X+lDWn6GEf/hgRAPtyPLo2qTfD0TKxI/x7WoYw7rBFsRl43RrTbFb+a2ILhkp2A851a/xgzgv2VFFwKxZMRgwKb53JhWbF71zV5ZCvg8wi3MX502RnC6s+ljo3+EcJj3LDQ/1jIv8AH/9II2RWJKDPIx1WK4dojZiBYGPAp3cdgfatl6gx/PBFtJB2h+8No2KlzQRCRoY803Ir18GCQ7vJEevFEeQ/3X27vCrbyv/02hzMdmdSy3Im6CrH3WrVvrp+HioMCh6Yhao6c+hWNIBDvKgXUsMwQmVxtVg5TebF3GdJoW1rUDgMzXfRIBG+ApPecihGw37yjkL7FPLFMLytePm3h2a2HZJmrGs/1yZRhgVCynFGfzUa2rcL5ClXz7l7L4RW3GwP72fxwXjcNCwxOKe8chhGWJlOrKKKYwSkiQIg0d+I5B9YY7ryBFNHFzF0hYsRag7doW/oWclfbW2n/LLmn+Ict0bQd6BMJzjOh7R3e7R";
+    }
+    else if(Theme.of(context).platform == TargetPlatform.android) {
+      licenseKey = "AclfFCWXOrX/Dx8+gjAdWhARENDRBdFqYwEimBx1pFypQ/tp/Xw1/NF8nFvwSGY5HU6eGSJzr2q1VcyTiDhCX/F9edHkZ8goMRdY5Ml946UgJUAwiS2JwUAMEP4nNWZSBrv3PTwMHdnK7CYSJHMxvdHzf1bm6RTUOIDvgaVI5lZ4eeWXsnt9z6dZ6ZvUPRSOP5R+K2rjMubVQiupQ3ernL+X4f1rdXqLZ2ZEAcD5XzaoOyUqAnzGh0JPGsxAjFLeX/3BZmvYhGxdIyy2JdHFxBaiqfs4KiM2vTTy3/zhvOekk7uV17RSpSybWut/D5fRASYVlyuLOlOpAiQHqREmnFJIbRB9pPiav7rCdCIiIu/4lsz9Yjc1qumqf2WqyAuW3oY7nrgQRsrTi5QEIaaeeFDOmbubtLgHR5jf1HubVhtI17X4oHFa4rC7Zao7+uMGzSCdHezdWz9lTD01KAhX9J0oGqITjMgxzG+EY0bYvKZHxva0qJw2s/4p1RdcSgGCa1gNi7kRYrsqilM2HkSDMU5wQL2bbC7k0qlnKJUeBY58no9gV4lLQTKAg0nf2lp5zgxReQ+4e7WdndqOgZ8RFGTK0XiaL4/wKxju4IIW4VkirS7SeT60g+t6jRN9SF8ZqtVIev0ZhIAY+pe7phZRYIG8h/BAFcEW21TSNJVSUIfign1g6gnJj48eU5T8z7ynGmNsaIBb5CpeZehpHo98lxlFYXoDXaU3WVSufwb/tMSlq7yt67gE9Or4j/CTFIi3YBl8kNaTADU7nOuYGPZYOVNGRpLv9FdcnB2wCuG5";
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text("Barcode Scanner"),
       ),
-      body: Stack(
-        children: [
-            Scandit(
-                scanned: _handleBarcodeResult,
-                onError: (e) => setState(() => _message = e.message),
-                symbologies: [Symbology.CODE128, Symbology.DATA_MATRIX],
-                onScanditCreated: (controller) => _controller = controller,
-                licenseKey:
-                "AcUfBiaXOVIQCeCwChcl05QwFCPbKhuZ5DFFHEdC7zjHZYV/XnS2CZJ5D9dnJcBTDW+Jh6RMOSJmXnJ8KywJjb9AHZNUU+j3MAZZX6hHx9fzc4UT4CAHEPRw2uVAIDBuKD8ozkMfw3qfOIanzJAM4oHLaUyHj9Qvxbh6eWOr/llZDzdB+mTTkUGW4ibe4XAhafCl7Y/pmyyS3KhREOJVIiMp8j0UoIjidTqBGq73dF9Ai2yXJKpAARXu4FAarFfWUoHIL2810iaDkpXhauSEv9KhCzWclhXFc/wD7kE9rfUYbq4gl0Wf3w7YTtvvaY+WeHYVvO4X+lDWn6GEf/hgRAPtyPLo2qTfD0TKxI/x7WoYw7rBFsRl43RrTbFb+a2ILhkp2A851a/xgzgv2VFFwKxZMRgwKb53JhWbF71zV5ZCvg8wi3MX502RnC6s+ljo3+EcJj3LDQ/1jIv8AH/9II2RWJKDPIx1WK4dojZiBYGPAp3cdgfatl6gx/PBFtJB2h+8No2KlzQRCRoY803Ir18GCQ7vJEevFEeQ/3X27vCrbyv/02hzMdmdSy3Im6CrH3WrVvrp+HioMCh6Yhao6c+hWNIBDvKgXUsMwQmVxtVg5TebF3GdJoW1rUDgMzXfRIBG+ApPecihGw37yjkL7FPLFMLytePm3h2a2HZJmrGs/1yZRhgVCynFGfzUa2rcL5ClXz7l7L4RW3GwP72fxwXjcNCwxOKe8chhGWJlOrKKKYwSkiQIg0d+I5B9YY7ryBFNHFzF0hYsRag7doW/oWclfbW2n/LLmn+Ict0bQd6BMJzjOh7R3e7R" //'INSERT YOUR TEST KEY',
-            ),
-          Center(
-            child: Container(
-                width: 300,
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  border: Border.all(color: Colors.teal),
-                )),
-          ),
-          Center(child: Text(_message)),
-        ],
-      ),
+      body:
+       !hasScanned ?
+          renderScanner()
+        :
+          renderSubmissionView()
+
+      ,
       floatingActionButton: IconButton(
         onPressed: () {},
         icon: Container(),
@@ -51,29 +67,167 @@ class _ScanditScannerState extends State<ScanditScanner> {
     );
   }
 
-  void _handleBarcodeResult(BarcodeResult result) {
-    showDialog<void>(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Barcode scanned'),
-          content: Text('The barcode is ${result.data}'),
-          actions: [
-            FlatButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Stop'),
+  Widget renderScanner() {
+    return(
+        Stack(
+          children: [
+            Scandit(
+                scanned: _handleBarcodeResult,
+                onError: (e) => setState(() => _message = e.message),
+                symbologies: [Symbology.CODE128, Symbology.DATA_MATRIX],
+                onScanditCreated: (controller) => _controller = controller,
+                licenseKey: licenseKey
             ),
-            FlatButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                _controller.resumeBarcodeScanning();
-              },
-              child: const Text('Continue'),
+            Center(
+              child: Container(
+                  width: 300,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    border: Border.all(color: Colors.teal),
+                  )),
             ),
+            Center(child: Text(_message)),
           ],
-        );
-      },
+        )
     );
+  }
+
+  Widget renderSubmissionView() {
+    print("top of rendersubmission method");
+    if(isLoading) {
+      print("in isLoading here");
+      return (
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: SizedBox(
+                    height: 40,
+                    width: 40,
+                    child: CircularProgressIndicator()
+                ),
+
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:20.0),
+                child: Text("Submitting...please wait", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+              ),
+            ],
+          )
+      );
+    }
+    else if(successfulSubmission) {
+      return Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(32),
+              child: (
+                 Column(
+                   children: <Widget>[
+                     Icon(Icons.check_circle,
+                       color: Colors.green,
+                       size: 60),
+                     Padding(
+                       padding: const EdgeInsets.all(8.0),
+                       child: Text("Scan Submitted", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+                     ),
+                     Text("Scan sent at: " + DateTime.now().toString(), style: TextStyle(color: ColorPrimary)),
+                   ]
+                 )
+              ),
+            ),
+          ),
+          Align(alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Text("Next Steps:", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+              )),
+
+          ListView(
+          shrinkWrap: true,
+          children:<Widget>[
+           ListTile(title: Text(String.fromCharCode(0x2022) + " Proceed to the next step in the testing process")),
+           ListTile(title: Text(String.fromCharCode(0x2022) + " Results are usually available within 24-36 hours.")),
+           ListTile(title: Text(String.fromCharCode(0x2022) + " You can view your results by logging in to MyStudentChart.")),
+           ListTile(title: Text(String.fromCharCode(0x2022) + " If you are experiencing symptoms of COVID-19, stay in your residence and seek guidance from a healthcare provider.")),
+          ],
+        ),
+        ],
+      );
+    }
+    else if(didError) {
+      return(
+          Column(
+              children: <Widget>[
+                Text("Scan failed.")
+              ]
+          )
+      );
+    }
+    else {
+      return(
+          Column(
+              children: <Widget>[
+                Text("An error occurred, please try again later.")
+              ]
+          )
+      );
+    }
+  }
+
+  Map<String, dynamic> createUserData() {
+    print("in createuserdata");
+    this.setState(() {
+      ucsdAffiliation = _userDataProvider.authenticationModel.ucsdaffiliation;
+      accessToken = _userDataProvider.authenticationModel.accessToken;
+    });
+    print("affiliation: " + ucsdAffiliation.toString());
+    return {'barcode': _barcode, 'ucsdaffiliation': ucsdAffiliation};
+  }
+
+
+  Future<void> _handleBarcodeResult(BarcodeResult result) async {
+    print("SCANNED:" + result.data);
+    this.setState(() {
+      hasScanned = true;
+      _barcode = result.data;
+    });
+    print("hasScanned: " + hasScanned.toString());
+    var data = createUserData();
+    var headers = {
+      "Content-Type": "application/json",
+      'Authorization': 'Bearer ${accessToken}'
+    };
+    setState(() {
+      isLoading = true;
+    });
+    print(data);
+    var results = await _barcodeService.uploadResults(headers, data);
+    print(results);
+
+    if (results) {
+      this.setState(() {
+        isLoading = false;
+        didError = false;
+        successfulSubmission = true;
+      });
+    } else {
+      this.setState(() {
+        successfulSubmission = false;
+        didError = true;
+        isLoading = false;
+      });
+      if (_barcodeService.error.contains(ErrorConstants.invalidBearerToken)) {
+        await _userDataProvider.refreshToken();
+      } else {
+
+      }
+      //_submitted = true;
+    }
+
+
   }
 }
