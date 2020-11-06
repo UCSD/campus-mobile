97d179e623b7c8ec6c57c14453e66d763cb81958
Bryant, Charles <c3bryant@ucsd.edu>
2020-11-06 08:10:10 -0800

    Revert messaging topics API to v1

--- lib/core/services/messages.dart
+++ lib/core/services/messages.dart
@@ -7,7 +7,7 @@ class MessageService {
   final String mymessages_endpoint =
       'https://api-qa.ucsd.edu:8243/mp-mymessages/1.0.0/messages?start=';
   final String topics_endpoint =
-      'https://bvgjvzaakl.execute-api.us-west-2.amazonaws.com/dev/topics/v2?';
+      'https://bvgjvzaakl.execute-api.us-west-2.amazonaws.com/dev/topics?';
 
   bool _isLoading = false;
   DateTime _lastUpdated;
