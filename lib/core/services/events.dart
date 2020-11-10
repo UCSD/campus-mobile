daab8adb5c7a62a15670f7b4961df4995f183e96
Bryant, Charles <c3bryant@ucsd.edu>
2020-11-09 16:08:56 -0800

    Update events service

--- lib/core/services/events.dart
+++ lib/core/services/events.dart
@@ -3,7 +3,7 @@ import 'package:campus_mobile_experimental/core/models/events.dart';
 
 class EventsService {
   final String endpoint =
-      'https://api-qa.ucsd.edu:8243/campusevents/1.0.0/events/student-v2';
+      'https://9tqs71by9h.execute-api.us-west-2.amazonaws.com/qa/v1/events/student-v2';
 
   bool _isLoading = false;
   DateTime _lastUpdated;
@@ -11,9 +11,6 @@ class EventsService {
   List<EventModel> _data;
 
   final NetworkHelper _networkHelper = NetworkHelper();
-  final Map<String, String> headers = {
-    "accept": "application/json",
-  };
 
   EventsService() {
     fetchData();
@@ -24,41 +21,16 @@ class EventsService {
     _isLoading = true;
     try {
       /// fetch data
-      String _response =
-          await _networkHelper.authorizedFetch(endpoint, headers);
+      String _response = await _networkHelper.fetchData(endpoint);
 
       /// parse data
-      final data = eventModelFromJson(_response);
+      final data = eventsModelFromJson(_response);
       _isLoading = false;
       _data = data;
       return true;
     } catch (e) {
-      if (e.response != null && e.response.statusCode == 401) {
-        if (await getNewToken()) {
-          return await fetchData();
-        }
-      }
       _error = e.toString();
-      print(_error);
       _isLoading = false;
-      return false;
-    }
-  }
-
-  Future<bool> getNewToken() async {
-    final String tokenEndpoint = "https://api-qa.ucsd.edu:8243/token";
-    final Map<String, String> tokenHeaders = {
-      "content-type": 'application/x-www-form-urlencoded',
-      "Authorization":
-          "Basic djJlNEpYa0NJUHZ5akFWT0VRXzRqZmZUdDkwYTp2emNBZGFzZWpmaWZiUDc2VUJjNDNNVDExclVh"
-    };
-    try {
-      var response = await _networkHelper.authorizedPost(
-          tokenEndpoint, tokenHeaders, "grant_type=client_credentials");
-      headers["Authorization"] = "Bearer " + response["access_token"];
-      return true;
-    } catch (e) {
-      _error = e.toString();
       return false;
     }
   }
