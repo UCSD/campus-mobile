4e8ec11e1d12fe6142021df4893f8ef489865b12
Luke Dutta <lukedutta9@gmail.com>
2021-02-10 09:39:15 -0800

    add functionality for opening maps apps

--- lib/ui/map/directions_button.dart
+++ lib/ui/map/directions_button.dart
@@ -1,7 +1,9 @@
 import 'package:campus_mobile_experimental/core/providers/map.dart';
+import 'package:campus_mobile_experimental/app_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DirectionsButton extends StatelessWidget {
   const DirectionsButton({
@@ -22,7 +24,39 @@ class DirectionsButton extends StatelessWidget {
       ),
       backgroundColor: Colors.white,
       onPressed: () {
+        String locationQuery =
+            Provider.of<MapsDataProvider>(context, listen: false)
+            .searchBarController
+            .text;
+        if (locationQuery.isNotEmpty) {
+          getDirections(context);
+        } else {
+          Navigator.pushNamed(context, RoutePaths.MapSearch);
+        }
       },
     );
+  }
+
+  Future<void> getDirections(BuildContext context) async {
+    LatLng currentPin = Provider
+        .of<MapsDataProvider>(context, listen: false)
+        .markers
+        .values
+        .toList()[0].position;
+    double lat = currentPin.latitude;
+    double lon = currentPin.longitude;
+
+    String googleUrl =
+        'https://www.google.com/maps/dir/?api=1&destination=$lat,$lon&travelmode=walking';
+    String appleUrl =
+        'http://maps.apple.com/?daddr=$lat,$lon&dirflag=w';
+    if (await canLaunch(googleUrl)) {
+      await launch(googleUrl);
+    }
+    else if (await canLaunch(appleUrl)) {
+      await launch(appleUrl);
+    } else {
+      throw 'Could not launch $googleUrl';
+    }
   }
 }
