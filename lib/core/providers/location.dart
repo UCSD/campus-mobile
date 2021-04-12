37dc72f069a1ad4396f45e060be363b39bb98e00
Peter Gonzalez <p8gonzal@ucsd.edu>
2021-04-12 12:45:13 -0700

    Reverted location provider, solving fix

--- lib/core/providers/location.dart
+++ lib/core/providers/location.dart
@@ -1,42 +1,38 @@
 import 'dart:async';
 
 import 'package:campus_mobile_experimental/core/models/location.dart';
-import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
-class LocationDataProvider extends ChangeNotifier {
+class LocationDataProvider {
   final Location _locationService = new Location();
   bool _permission = false;
   String error;
 
   StreamController<Coordinates> _locationController =
-      StreamController<Coordinates>.broadcast();
+  StreamController<Coordinates>.broadcast();
   Stream<Coordinates> get locationStream => _locationController.stream;
 
   LocationDataProvider() {
+    locationStream;
     _init();
   }
 
   _init() async {
-    await _requestPermissions();
-    if (_permission) {
-      /// create the settings for location access
-      await _locationService.changeSettings(
-          accuracy: LocationAccuracy.high, distanceFilter: 100);
-    }
-    notifyListeners();
-  }
+    /// create the settings for location access
+    await _locationService.changeSettings(
+        accuracy: LocationAccuracy.high, distanceFilter: 100);
 
-  _requestPermissions() async {
-    PermissionStatus hasPermission;
-    //check if permission is granted
-    hasPermission = await _locationService.hasPermission();
-    if (hasPermission == PermissionStatus.denied) {
-      hasPermission = await _locationService.requestPermission();
-      if (hasPermission == PermissionStatus.granted) {
-        _permission = true;
+    /// check to see if gps service is enabled on device
+    bool serviceStatus = await _locationService.serviceEnabled();
+    if (!serviceStatus) {
+      /// check to see if permission has been granted to the app
+      _permission = await _locationService.requestService();
+      if (_permission) {
         _enableListener();
       }
+    } else {
+      _permission = true;
+      _enableListener();
     }
   }
 
