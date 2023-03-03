fc8d2b91ccb05de6a4f008303d1df9fa6e64ffa8
zaashraf <zaashraf@ucsd.edu>
2023-03-03 15:30:49 -0800

    Uses Cached Network Image package to display image or silhouette

--- lib/core/services/employee_id.dart
+++ lib/core/services/employee_id.dart
@@ -2,18 +2,16 @@ import 'dart:async';
 
 import 'package:campus_mobile_experimental/app_networking.dart';
 import 'package:campus_mobile_experimental/core/models/employee_id.dart';
+import 'package:dio/dio.dart';
 
 class EmployeeIdService {
   final String myEmployeeProfileApiUrl =
       'https://api-qa.ucsd.edu:8243/staff/my/v1/profile';
-
   bool _isLoading = false;
   DateTime? _lastUpdated;
   String? _error;
   EmployeeIdModel _employeeIdModel = EmployeeIdModel();
-
   final NetworkHelper _networkHelper = NetworkHelper();
-
   Future<bool> fetchEmployeeIdProfile(Map<String, String> headers) async {
     _error = null;
     _isLoading = true;
@@ -21,17 +19,19 @@ class EmployeeIdService {
       /// fetch data
       String _response = await _networkHelper.authorizedFetch(
           myEmployeeProfileApiUrl, headers);
-
       _employeeIdModel = employeeIdModelFromJson(_response);
       _isLoading = false;
       return true;
     } catch (e) {
-      _error = e.toString();
-      _isLoading = false;
+      if (e is DioError && e.type == DioErrorType.receiveTimeout) {
+
+      } else {
+        _error = e.toString();
+        _isLoading = false;
+      }
       return false;
     }
   }
-
   String? get error => _error;
   EmployeeIdModel get employeeIdModel => _employeeIdModel;
   bool get isLoading => _isLoading;
