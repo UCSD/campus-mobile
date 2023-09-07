6e3e48ab5d716be5883732c0c91e9e7ebcc99c1c
mliu6723 <mliu6723@gmail.com>
2023-09-07 11:35:14 -0700

    fixing errors after manually testing on codemagic

--- scripts/codemagic-ci/post-publish.sh
+++ scripts/codemagic-ci/post-publish.sh
@@ -23,14 +23,12 @@ echo $ENV_VARS > env-vars.json
 
 echo "Find build artifacts"
 dsymPath=$(find $CM_BUILD_DIR/build/ios/archive/Runner.xcarchive -name "*.dSYM" | head -1)
-if [[ -z ${dsymPath} ]]
-then
+if [[ -z ${dsymPath} ]]; then
   echo "No debug symbols were found, skip publishing to Firebase Crashlytics"
 else
   echo "Publishing debug symbols from $dsymPath to Firebase Crashlytics"
-  ls -d -- ios/Pods/*
   $CM_BUILD_DIR/ios/Pods/FirebaseCrashlytics/upload-symbols \
-    -gsp ios/Runner/GoogleService-Info.plist -p ios $dsymPath
+    -gsp $CM_BUILD_DIR/ios/Runner/GoogleService-Info.plist -p ios $dsymPath
 fi
 
 node ./build-notifier.js
