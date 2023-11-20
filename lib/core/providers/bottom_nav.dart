91862c30d6f0e4fcff4acd3508ef5aa946923a0a
virajl <116128339+virajl@users.noreply.github.com>
2023-11-20 12:08:30 -0800

    Remove comments

--- lib/core/providers/bottom_nav.dart
+++ lib/core/providers/bottom_nav.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-// class BottomNavigationBarProvider with ChangeNotifier {
-//   int _currentIndex = 0;
-//
-//   int get currentIndex => _currentIndex;
-//   set currentIndex(int index) {
-//     _currentIndex = index;
-//     notifyListeners();
-//   }
-// }
 ValueNotifier<int> _currentIndex = ValueNotifier(0);
 ValueNotifier<int> useBottomNavigationBar() {
   useListenable(_currentIndex);
