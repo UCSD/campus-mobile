27c25636888bbb115109c369ad966385863e63fc
virajl <116128339+virajl@users.noreply.github.com>
2023-11-21 15:48:24 -0800

    revert podfile.lock

--- lib/core/providers/scanner_message.dart
+++ lib/core/providers/scanner_message.dart
@@ -4,8 +4,7 @@ import 'package:campus_mobile_experimental/core/services/scanner_message.dart';
 import 'package:flutter/material.dart';
 
 class ScannerMessageDataProvider extends ChangeNotifier {
-  ScannerMessageDataProvider()
-  {
+  ScannerMessageDataProvider() {
     ///DEFAULT STATES
     _isLoading = false;
 
