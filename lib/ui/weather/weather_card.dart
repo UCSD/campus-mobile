ab8e510c9fba23abd5be2b4b564216864e70a46f
Parth Shinde <48342852+ParthShinde04@users.noreply.github.com>
2024-02-07 11:03:19 -0800

    added footer to weather card

--- lib/ui/weather/weather_card.dart
+++ lib/ui/weather/weather_card.dart
@@ -3,6 +3,7 @@ import 'package:campus_mobile_experimental/core/models/weather.dart';
 import 'package:campus_mobile_experimental/core/providers/cards.dart';
 import 'package:campus_mobile_experimental/core/providers/weather.dart';
 import 'package:campus_mobile_experimental/ui/common/card_container.dart';
+import 'package:campus_mobile_experimental/core/utils/webview.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,17 +14,48 @@ const String WEATHER_ICON_BASE_URL =
 class WeatherCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CardContainer(
-      active: Provider.of<CardsDataProvider>(context).cardStates![cardId],
-      hide: () => Provider.of<CardsDataProvider>(context, listen: false)
-          .toggleCard(cardId),
-      reload: () => Provider.of<WeatherDataProvider>(context, listen: false)
-          .fetchWeather(),
-      isLoading: Provider.of<WeatherDataProvider>(context).isLoading,
-      titleText: CardTitleConstants.titleMap[cardId],
-      errorText: Provider.of<WeatherDataProvider>(context).error,
-      child: () => buildCardContent(
-          Provider.of<WeatherDataProvider>(context).weatherModel!),
+    return Stack(
+      children: [
+        CardContainer(
+          active: Provider.of<CardsDataProvider>(context).cardStates![cardId],
+          hide: () => Provider.of<CardsDataProvider>(context, listen: false)
+              .toggleCard(cardId),
+          reload: () => Provider.of<WeatherDataProvider>(context, listen: false)
+              .fetchWeather(),
+          isLoading: Provider.of<WeatherDataProvider>(context).isLoading,
+          titleText: CardTitleConstants.titleMap[cardId],
+          errorText: Provider.of<WeatherDataProvider>(context).error,
+          child: () => buildCardContent(
+              Provider.of<WeatherDataProvider>(context).weatherModel!),
+          footer: buildFooter(),
+        ),
+        // webview openlink()
+      ],
+    );
+  }
+
+  Widget buildFooter() {
+    return Container(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () => openLink('https://www.example.com'),
+              child: Text(
+                "Weather attribution",
+                style: TextStyle(
+                  color: Colors.blue, // Make it look like a link
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            Text("Powered by UCSD"),
+          ],
+        ),
+      ),
     );
   }
 
