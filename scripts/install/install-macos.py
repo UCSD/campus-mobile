92b80048d062493b4ee40652b83a6b3019606970
Bryant, Charles <c3bryant@ucsd.edu>
2024-10-07 16:27:42 -0700

    Add xcode command line tools installation step and tidy up

--- scripts/install/install-macos.py
+++ scripts/install/install-macos.py
@@ -13,7 +13,7 @@ avd_name = ""  # Global variable to store the name of the Android Virtual Device
 
 
 ##############################################################################################################
-# 0/10  Main
+# Main
 ##############################################################################################################
 
 def main():
@@ -23,54 +23,81 @@ def main():
  | |   / _` | '_ ` _ \| '_ \| | | / __| | |\/| |/ _ \| '_ \| | |/ _ \  | || '_ \/ __| __/ _` | | |/ _ \ '__|
  | |__| (_| | | | | | | |_) | |_| \__ \ | |  | | (_) | |_) | | |  __/  | || | | \__ \ || (_| | | |  __/ |   
   \____\__,_|_| |_| |_| .__/ \__,_|___/ |_|  |_|\___/|_.__/|_|_|\___| |___|_| |_|___/\__\__,_|_|_|\___|_|   
-                      |_|                                                                                   
+					  |_|                                                                                   
 """)
 	print("Welcome to the Campus Mobile (MacOS) Installer.")
 	print("\nThe installer will perform the following actions:")
-	print("  1. Install Homebrew")
-	print("  2. Install Git")
-	print("  3. Install Java")
-	print("  4. Install VS Code")
-	print("  5. Install Flutter")
-	print("  6. Install Android Tools")
-	print("  7. Create & Launch Android Emulator")
-	print("  8. Install Campus Mobile")
-	print("  9. Configure Campus Mobile")
-	print(" 10. Build & Run Campus Mobile")
+	print("  1. Install Xcode Command Line Tools")
+	print("  2. Install Homebrew")
+	print("  3. Install Git")
+	print("  4. Install Java")
+	print("  5. Install VS Code")
+	print("  6. Install Flutter")
+	print("  7. Install Android Tools")
+	print("  8. Create & Launch Android Emulator")
+	print("  9. Install Campus Mobile")
+	print(" 10. Configure Campus Mobile")
+	print(" 11. Build & Run Campus Mobile")
 
 	proceed = input("Press 'Y' to continue with the installation, any other key to exit: ").strip().upper()
 	if proceed == "Y":
-		print("\nStarting installation... Please be patient as this process may take up to 10 minutes.\n")
-		backup_zshrc() # 0/10
-		install_homebrew() # 1/10
-		install_git() # 2/10
-		install_java() # 3/10
-		install_vs_code() # 4/10
-		install_flutter() # 5/10
-		install_android_tools() # 6/10
-		create_android_emulator() # 7/10
-		install_campus_mobile() # 8/10
-		configure_campus_mobile() # 9/10
-		run_campus_mobile() # 10/10
+		print("\nStarting installation... Please be patient as this process may take up to 15 minutes.\n")
+		backup_zshrc() # 0/11
+		install_xcode_command_line_tools() # 1/11
+		# install_homebrew() # 2/11
+		# install_git() # 3/11
+		# install_java() # 4/11
+		# install_vs_code() # 5/11
+		# install_flutter() # 6/11
+		# install_android_tools() # 7/11
+		# create_android_emulator() # 8/11
+		# install_campus_mobile() # 9/11
+		# configure_campus_mobile() # 10/11
+		# run_campus_mobile() # 11/11
 	else:
 		print("Installation canceled.")
 		sys.exit(0)
 
 
 ##############################################################################################################
-# 1/10  Install Homebrew
+# 1/11  Install Xcode Command Line Tools
+##############################################################################################################
+def install_xcode_command_line_tools():
+    # Check if Xcode Command Line Tools need to be installed
+	if not pkg_installed('com.apple.pkg.CLTools_Executables'):
+		prompt_until_xcode_tools_installed()
+	print(" 1/11  Xcode Command Line Tools installation complete.")
+
+def prompt_until_xcode_tools_installed():
+	print("\n  *** USER ACTION REQUIRED ***")
+	print("  1. Locate the installer pop-up window that appears.")
+	print("  2. Click 'Install' to begin the installation of Xcode Command Line Tools.")
+
+	# Start the installation process in the background
+	subprocess.Popen("xcode-select --install", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+	try:
+		input("\nAfter completing the installation, press any key to continue (or Control-C to exit).")
+	except KeyboardInterrupt:
+		print("Installation interrupted by user. Exiting...")
+		sys.exit(0)
+
+	install_xcode_command_line_tools()
+
+
+##############################################################################################################
+# 2/11  Install Homebrew
 ##############################################################################################################
 def install_homebrew():
 	if is_installed('brew'):
 		subprocess.run(['brew', 'upgrade'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-		print(" 1/10  Homebrew upgrade complete.")
+		print(" 2/11  Homebrew upgrade complete.")
 	else:
 		subprocess.run('echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-		print(" 1/10  Homebrew installation complete.")
+		print(" 2/11  Homebrew installation complete.")
 
 
 ##############################################################################################################
-# 2/10  Install Git
+# 3/11  Install Git
 ##############################################################################################################
 def install_git():
 	if not is_installed('git'):
@@ -86,11 +113,11 @@ def install_git():
 		user_email = input("Enter your Git user email: ")
 		subprocess.run(['git', 'config', '--global', 'user.email', user_email])
 	
-	print(" 2/10  Git installation complete.")
+	print(" 3/11  Git installation complete.")
 
 
 ##############################################################################################################
-# 3/10  Install Java
+# 4/11  Install Java
 ##############################################################################################################
 def install_java():
 	if not check_openjdk_version():
@@ -102,7 +129,7 @@ def install_java():
 
 		# Add JAVA_HOME and its bin directory to the PATH
 		add_path_to_zshrc([f'{java_home}', f'{java_home}/bin'])
-	print(" 3/10  Java installation complete.")
+	print(" 4/11  Java installation complete.")
 
 def check_openjdk_version(min_version=17):
 	try:
@@ -119,14 +146,14 @@ def check_openjdk_version(min_version=17):
 
 
 ##############################################################################################################
-# 4/10  Install VS Code
+# 5/11  Install VS Code
 ##############################################################################################################
 def install_vs_code():
 	if not is_installed('code'):
 		subprocess.run(['brew', 'install', '--cask', 'visual-studio-code'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
 	# Install extensions
 	install_vs_code_extensions()
-	print(" 4/10  VS Code installation complete.")
+	print(" 5/11  VS Code installation complete.")
 
 # Function to install VS Code extensions
 def install_vs_code_extensions():
@@ -142,7 +169,7 @@ def install_vs_code_extensions():
 
 
 ##############################################################################################################
-# 5/10  Install Flutter
+# 6/11  Install Flutter
 ##############################################################################################################
 def install_flutter():
 	# Try to determine the Flutter binary path dynamically
@@ -159,7 +186,7 @@ def install_flutter():
 			os.makedirs(flutter_dir, exist_ok=True)
 			subprocess.run(['git', 'clone', 'https://github.com/flutter/flutter.git', '-b', '3.7.3', flutter_dir], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
 		add_flutter_to_path(flutter_dir)
-	print(" 5/10  Flutter installation complete.")
+	print(" 6/11  Flutter installation complete.")
 
 def add_flutter_to_path(flutter_dir):
 	flutter_bin_path = os.path.join(flutter_dir, 'bin')
@@ -177,7 +204,7 @@ def check_and_switch_flutter_version(flutter_bin_path):
 
 
 ##############################################################################################################
-# 6/10  Install Android tools
+# 7/11  Install Android tools
 ##############################################################################################################
 def install_android_tools():
 	try:
@@ -212,14 +239,14 @@ def install_android_tools():
 		add_path_to_zshrc([android_cmdline_tools, platform_tools, android_emulator, platforms, build_tools])
 		add_export_to_zshrc("ANDROID_SDK_ROOT", android_home)
 
-		print(" 6/10  Android Tools installation complete.")
+		print(" 7/11  Android Tools installation complete.")
 	except subprocess.CalledProcessError as e:
 		print(f"Failed to install Android tools: {e}")
 		sys.exit(1)
 
 
 ##############################################################################################################
-# 7/10  Create & Launch Android Emulator
+# 8/11  Create & Launch Android Emulator
 ##############################################################################################################
 def create_android_emulator():
 	global avd_name
@@ -249,7 +276,7 @@ def create_android_emulator():
 		print("Emulator failed to start within the timeout period. Please try again.")
 		sys.exit(1)
 	
-	print(" 7/10  Android Emulator creation complete.")
+	print(" 8/11  Android Emulator creation complete.")
 
 def get_available_avd_name(base_name):
 	counter = 1
@@ -266,7 +293,7 @@ def avd_exists(name):
 
 
 ##############################################################################################################
-# 8/10  Install Campus Mobile
+# 9/11  Install Campus Mobile
 ##############################################################################################################
 def install_campus_mobile():
 	github_username = get_github_username()
@@ -291,7 +318,7 @@ def install_campus_mobile():
 		set_git_remote('upstream', upstream_url)
 		subprocess.run(['git', 'fetch', 'upstream'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
 
-	print(" 8/10  Campus Mobile installation complete.")
+	print(" 9/11  Campus Mobile installation complete.")
 
 def set_git_remote(remote_name, url):
 	# Set or update a git remote
@@ -319,7 +346,7 @@ def configure_origin(origin_url, upstream_url):
 
 
 ##############################################################################################################
-# 9/10  Configure Campus Mobile
+# 10/11  Configure Campus Mobile
 ##############################################################################################################
 def configure_campus_mobile():
 	downloads_path = os.path.expanduser('~/Downloads')
@@ -350,6 +377,7 @@ def configure_campus_mobile():
 				print("\nError: Not all required files are in the ~/Downloads folder. Please check and try again.")
 		
 		except KeyboardInterrupt:
+			print("Installation interrupted by user. Exiting...")
 			sys.exit(0)
 		except subprocess.CalledProcessError:
 			pass
@@ -364,10 +392,11 @@ def configure_campus_mobile():
 			try:
 				input("\nPress any key to continue (or Control-C to exit).")
 			except KeyboardInterrupt:
+				print("Installation interrupted by user. Exiting...")
 				sys.exit(0)
 
 	clean_up_config_installer(installer_path)
-	print(" 9/10  Campus Mobile configuration complete.")
+	print(" 10/11  Campus Mobile configuration complete.")
 
 def verify_and_extract(downloads_path, zip_files, installer_path):
 	# Check if all zip files are present
@@ -471,10 +500,10 @@ def update_task_json_device_id(device_id, project_path):
 
 
 ##############################################################################################################
-# 10/10  Run Campus Mobile
+# 11/11  Run Campus Mobile
 ##############################################################################################################
 def run_campus_mobile():
-	print("10/10  Campus Mobile build & run in progress...")
+	print("11/11  Campus Mobile build & run in progress...")
 
 	# Open the project in VS Code
 	subprocess.run(['code', project_path], check=True)
@@ -489,7 +518,7 @@ def run_campus_mobile():
 - Git remote origin: {get_git_remote_origin()}
 
   If this is incorrect, set the correct remote using:
-    git remote set-url origin https://github.com/<Your-GitHub-Username>/campus-mobile.git
+	git remote set-url origin https://github.com/<Your-GitHub-Username>/campus-mobile.git
 *********************************************************************************
 """)
 
@@ -556,6 +585,10 @@ def add_export_to_zshrc(var_name, value):
 def is_installed(command):
 	return subprocess.run(['command', '-v', command], capture_output=True).returncode == 0
 
+def pkg_installed(package_name):
+	result = subprocess.run(['pkgutil', '--pkgs=' + package_name], capture_output=True, text=True)
+	return bool(result.stdout.strip())
+
 def get_android_system_image():
 	architecture = platform.machine()
 	if 'aarch64' in architecture or 'arm64' in architecture:
@@ -567,18 +600,18 @@ def get_android_system_image():
 		sys.exit(1)
 
 def get_github_username():
-    default_username = "Your_GitHub_Username"
-    email_result = subprocess.run(['git', 'config', 'user.email'], capture_output=True, text=True)
-    git_email = email_result.stdout.strip()
-    if '@' in git_email:
-        return git_email.split('@')[0]
-    return default_username
+	default_username = "Your_GitHub_Username"
+	email_result = subprocess.run(['git', 'config', 'user.email'], capture_output=True, text=True)
+	git_email = email_result.stdout.strip()
+	if '@' in git_email:
+		return git_email.split('@')[0]
+	return default_username
 
 def get_git_remote_origin():
-    result = subprocess.run(['git', 'remote', 'get-url', 'origin'], capture_output=True, text=True)
-    if result.returncode == 0:
-        return result.stdout.strip()
-    return "Not set"
+	result = subprocess.run(['git', 'remote', 'get-url', 'origin'], capture_output=True, text=True)
+	if result.returncode == 0:
+		return result.stdout.strip()
+	return "Not set"
 
 # Initialize the script
 if __name__ == "__main__":
