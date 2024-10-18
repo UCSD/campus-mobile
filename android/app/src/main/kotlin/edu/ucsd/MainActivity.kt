1a9c9432e7dd1dd45feac9d9d7a9e749b174568f
Bryant, Charles <c3bryant@ucsd.edu>
2024-10-18 12:22:00 -0700

    Revert "Merge pull request #1932 from mliu6723/podcast_feature"
    This reverts commit c966e1b2137e4904c1ca0e69f92c5771c489bd51, reversing
    changes made to fff5465444ce2d3a6e27bd77ffa68444f9506ca6.

--- android/app/src/main/kotlin/edu/ucsd/MainActivity.kt
+++ android/app/src/main/kotlin/edu/ucsd/MainActivity.kt
@@ -1,12 +1,7 @@
 package edu.ucsd
 
 import io.flutter.embedding.android.FlutterActivity
-import com.ryanheise.audioservice.AudioServicePlugin
-import io.flutter.embedding.engine.FlutterEngine
-import android.content.Context
 
 class MainActivity: FlutterActivity() {
-    override fun provideFlutterEngine(context:Context):FlutterEngine {
-        return AudioServicePlugin.getFlutterEngine(context);
-    }
+
 }
