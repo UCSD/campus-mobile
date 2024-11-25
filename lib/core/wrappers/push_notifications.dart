498d4258a6f39d148f5e82a26ad4eb0fb53f7cc7
klortiz13 <kortizalvarado@ucsd.edu>
2024-11-25 09:33:18 -0800

    finished core

--- lib/core/wrappers/push_notifications.dart
+++ lib/core/wrappers/push_notifications.dart
@@ -7,8 +7,7 @@ class PushNotificationWrapper extends StatefulWidget {
   final Widget? child;
 
   @override
-  _PushNotificationWrapperState createState() =>
-      _PushNotificationWrapperState();
+  _PushNotificationWrapperState createState() => _PushNotificationWrapperState();
 }
 
 class _PushNotificationWrapperState extends State<PushNotificationWrapper> {
