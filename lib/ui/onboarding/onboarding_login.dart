f3e11342fd80904c406ea5077a719bb3d2de1f78
morebytes <22108504+morebytes@users.noreply.github.com>
2025-03-05 14:09:33 -0800

    Re-implemented the accessibility fix to work on iOS devices

--- lib/ui/onboarding/onboarding_login.dart
+++ lib/ui/onboarding/onboarding_login.dart
@@ -32,228 +32,210 @@ class _OnboardingLoginState extends State<OnboardingLogin> {
 
   @override
   Widget build(BuildContext context) =>
-    AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        systemNavigationBarColor: Colors.black,
-        statusBarIconBrightness: Brightness.dark,
-        statusBarBrightness: Brightness.dark,
-        systemNavigationBarIconBrightness: Brightness.dark,
-      ),
+    SafeArea(
       child: Scaffold(
         backgroundColor: lightOnboardingScreen,
         body: _userDataProvider.isLoading
-            ? const Center(
-                child: const CircularProgressIndicator(
-                  valueColor: const AlwaysStoppedAnimation<Color>(darkAccentColor),
-                ),
-              )
-            : SingleChildScrollView(
-                  child: Container(
-                    height: _screenHeight,
-                    decoration: const BoxDecoration(
-                      image: const DecorationImage(
-                        image: const AssetImage("assets/images/login-background.png"),
-                        fit: BoxFit.cover, // Ensure the image covers the entire screen
-                      ),
+          ? const Center(
+              child: const CircularProgressIndicator(
+                valueColor: const AlwaysStoppedAnimation<Color>(darkAccentColor),
+              ),
+            )
+          : SingleChildScrollView(
+                child: Container(
+                  height: _screenHeight,
+                  decoration: const BoxDecoration(
+                    image: const DecorationImage(
+                      image: const AssetImage("assets/images/login-background.png"),
+                      fit: BoxFit.cover, // Ensure the image covers the entire screen
                     ),
-                    child: _buildLoginWidget(),
                   ),
-               // ),
-              )
+                  child: _buildLoginWidget(),
+                ),
+            )
       )
     );
 
   Widget _buildLoginWidget() =>
-    SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // Align items at the top
-          crossAxisAlignment: CrossAxisAlignment.center, // Center items horizontally
-          children: <Widget>[
-            const Spacer(flex: 3),
-
-            // UCSD Logo
-            Flexible(
-              flex: 0,
-              child: FractionallySizedBox(
-                widthFactor: 0.78055556, //0.76986301,
-                child: Image.asset(
-                  'assets/images/UCSanDiegoLogo-Blue.png',
-                ),
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Align items at the top
+        crossAxisAlignment: CrossAxisAlignment.center, // Center items horizontally
+        children: <Widget>[
+          const Spacer(flex: 3),
+          // UCSD Logo
+          Flexible(
+            flex: 0,
+            child: FractionallySizedBox(
+              widthFactor: 0.78055556, //0.76986301,
+              child: Image.asset(
+                'assets/images/UCSanDiegoLogo-Blue.png',
               ),
             ),
-            const SizedBox(height: 44.0),
-
-            // Welcome Heading
-            const Flexible(
-              flex: 0,
-              child: const FractionallySizedBox(
-                  widthFactor: 0.725,
-                  child: const Text(
-                    "WELCOME TO THE UC SAN DIEGO MOBILE APP",
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontFamily: "Refrigerator Deluxe",
-                      fontSize: 34.5,
-                      height: 0.9333, // line height: 20px
-                      fontWeight: FontWeight.w900,
-                      color: const Color(0xFF182B49), // Subheading color
-                    ),
-                  )),
-            ),
-            const SizedBox(height: 12.5),
-
-            // Description Text
-            const Flexible(
-              flex: 0,
-              child: const FractionallySizedBox(
-                widthFactor: 0.64722222,
+          ),
+          const SizedBox(height: 44.0),
+          // Welcome Heading
+          const Flexible(
+            flex: 0,
+            child: const FractionallySizedBox(
+                widthFactor: 0.725,
                 child: const Text(
-                  // TODO: the font here seems a bit light. Might need to swap that out later
-                  "Your personalized gateway to campus life, events, news and more.",
+                  "WELCOME TO THE UC SAN DIEGO MOBILE APP",
                   textAlign: TextAlign.center,
                   style: const TextStyle(
-                    fontFamily: 'Brix Sans',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 18,
-                    letterSpacing: 2.2,
-                    height: 1.2778, // line height: ~26px
-                    color: const Color(0xFF182B49), // Foreground body color
-                  ),
-                ),
-              )
-            ),
-            const SizedBox(height: 35.0),
-
-            // UCSD Email Input
-            _buildEmailField(),
-            const SizedBox(height: 18.5),
-
-            // Password Input
-            _buildPasswordField(),
-            const SizedBox(height: 20.0),
-
-            // SIGN IN and Forgot Password?
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFFFCD00), // Yellow Button
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    elevation: 0.0,
-                    fixedSize: () {
-                      // calculate width using scaling ratio, then calculate height from width using aspect ratio
-                      final width = _screenWidth * 0.24111111;
-                      final height = width / 2.29268293;
-                      return Size(width, height);
-                    }(),
-                  ),
-                  child: Semantics(
-                    button: true,
-                    hint:
-                        'press to login with your information inputted in above textfields',
-                    child: const Text(
-                      'SIGN IN',
-                      style: const TextStyle(
-                        fontFamily: 'Brix Sans',
-                        fontSize: 15.0,
-                        height: 1.195,
-                        color: Colors.black, // Text color on button
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  onPressed: _userDataProvider.isLoading
-                      ? null
-                      : () {
-                          _userDataProvider
-                              .manualLogin(_emailTextFieldController.text,
-                                  _passwordTextFieldController.text)
-                              .then((isLoggedIn) async {
-                            if (isLoggedIn) {
-                              Navigator.pushNamedAndRemoveUntil(context,
-                                  RoutePaths.OnboardingInitial, (_) => false);
-                            } else {
-                              showAlertDialog(context);
-                            }
-                          });
-                        },
-                ),
-
-                // the spacer in between sign in and forgot password
-                SizedBox(width: _screenWidth * 0.202),
-
-                GestureDetector(
-                  child: Semantics(
-                    hint:
-                        'press to be redirected to the UCSD Password reset page',
-                    child: const Text(
-                      'Forgot Password?',
-                      style: const TextStyle(
-                        color: const Color(0xFF00629B),
-                        height: 1.42857143,
-                        //fontFamily: "Source Sans Pro" // this doesn't seem to be doing anything, so disabling for now
-                      ), // Light Blue
-                    ),
-                  ),
-                  onTap: () async {
-                    try {
-                      /// TODO: Update link to redirect to password reset
-                      String link =
-                          'https://acms.ucsd.edu/students/accounts-and-passwords/index.html';
-                      await launch(link, forceSafariVC: true);
-                    } catch (e) {
-                      // an error occurred, do nothing
-                    }
-                  },
-                ),
-              ],
-            ),
-
-            ///////////////////////// Footer ////////////////////////////
-            const Spacer(flex: 2),
-            // Not affiliated text
-            const Text(
-              "Not affiliated with UC San Diego?",
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 15.5,
-                height: 1.42, // line height: 20px
-                color: const Color(0xFF5D5E60), // Foreground body color
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            // Skip This Step
-            GestureDetector(
-              child: Semantics(
-                hint:
-                    'press to skip the login process and use this app as a visitor',
-                child: const Text(
-                  "SKIP THIS STEP",
-                  style: const TextStyle(
+                    fontFamily: "Refrigerator Deluxe",
+                    fontSize: 34.5,
+                    height: 0.9333, // line height: 20px
+                    fontWeight: FontWeight.w900,
                     color: const Color(0xFF182B49), // Subheading color
-                    fontFamily: 'Brix Sans',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 16.5,
-                    height: 1.195, // line height: 16.73px
-                    decoration: TextDecoration.underline,
                   ),
+                )),
+          ),
+          const SizedBox(height: 12.5),
+          // Description Text
+          const Flexible(
+            flex: 0,
+            child: const FractionallySizedBox(
+              widthFactor: 0.64722222,
+              child: const Text(
+                // TODO: the font here seems a bit light. Might need to swap that out later
+                "Your personalized gateway to campus life, events, news and more.",
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontFamily: 'Brix Sans',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 18,
+                  letterSpacing: 2.2,
+                  height: 1.2778, // line height: ~26px
+                  color: const Color(0xFF182B49), // Foreground body color
                 ),
               ),
-              onTap: () async {
-                Navigator.pushNamedAndRemoveUntil(
-                    context, RoutePaths.OnboardingInitial, (_) => false);
-              },
+            )
+          ),
+          const SizedBox(height: 35.0),
+          // UCSD Email Input
+          _buildEmailField(),
+          const SizedBox(height: 18.5),
+          // Password Input
+          _buildPasswordField(),
+          const SizedBox(height: 20.0),
+          // SIGN IN and Forgot Password?
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFFFCD00), // Yellow Button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  elevation: 0.0,
+                  fixedSize: () {
+                    // calculate width using scaling ratio, then calculate height from width using aspect ratio
+                    final width = _screenWidth * 0.24111111;
+                    final height = width / 2.29268293;
+                    return Size(width, height);
+                  }(),
+                ),
+                child: Semantics(
+                  button: true,
+                  hint:
+                      'press to login with your information inputted in above textfields',
+                  child: const Text(
+                    'SIGN IN',
+                    style: const TextStyle(
+                      fontFamily: 'Brix Sans',
+                      fontSize: 15.0,
+                      height: 1.195,
+                      color: Colors.black, // Text color on button
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                onPressed: _userDataProvider.isLoading
+                    ? null
+                    : () {
+                        _userDataProvider
+                            .manualLogin(_emailTextFieldController.text,
+                                _passwordTextFieldController.text)
+                            .then((isLoggedIn) async {
+                          if (isLoggedIn) {
+                            Navigator.pushNamedAndRemoveUntil(context,
+                                RoutePaths.OnboardingInitial, (_) => false);
+                          } else {
+                            showAlertDialog(context);
+                          }
+                        });
+                      },
+              ),
+              // the spacer in between sign in and forgot password
+              SizedBox(width: _screenWidth * 0.202),
+              GestureDetector(
+                child: Semantics(
+                  hint:
+                      'press to be redirected to the UCSD Password reset page',
+                  child: const Text(
+                    'Forgot Password?',
+                    style: const TextStyle(
+                      color: const Color(0xFF00629B),
+                      height: 1.42857143,
+                      //fontFamily: "Source Sans Pro" // this doesn't seem to be doing anything, so disabling for now
+                    ), // Light Blue
+                  ),
+                ),
+                onTap: () async {
+                  try {
+                    /// TODO: Update link to redirect to password reset
+                    String link =
+                        'https://acms.ucsd.edu/students/accounts-and-passwords/index.html';
+                    await launch(link, forceSafariVC: true);
+                  } catch (e) {
+                    // an error occurred, do nothing
+                  }
+                },
+              ),
+            ],
+          ),
+          ///////////////////////// Footer ////////////////////////////
+          const Spacer(flex: 2),
+          // Not affiliated text
+          const Text(
+            "Not affiliated with UC San Diego?",
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 15.5,
+              height: 1.42, // line height: 20px
+              color: const Color(0xFF5D5E60), // Foreground body color
             ),
-
-            const Spacer()
-          ],
-        ),
-      )
+          ),
+          const SizedBox(height: 12.0),
+          // Skip This Step
+          GestureDetector(
+            child: Semantics(
+              hint:
+                  'press to skip the login process and use this app as a visitor',
+              child: const Text(
+                "SKIP THIS STEP",
+                style: const TextStyle(
+                  color: const Color(0xFF182B49), // Subheading color
+                  fontFamily: 'Brix Sans',
+                  fontWeight: FontWeight.w700,
+                  fontSize: 16.5,
+                  height: 1.195, // line height: 16.73px
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            onTap: () async {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, RoutePaths.OnboardingInitial, (_) => false);
+            },
+          ),
+          
+          const SizedBox(height: 16.0),
+          const Spacer()
+        ],
+      ),
     );
 
   // TODO: change the font for the password field (it's a lighter gray than what is currently there)
