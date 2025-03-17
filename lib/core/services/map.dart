6e3d72a04fa009701eb19ed0cf1ca7acddd44676
morebytes <tibodrov@ucsd.edu>
2025-03-17 14:51:04 -0700

    Trying out a fix for a possible syntax bug

--- lib/core/services/map.dart
+++ lib/core/services/map.dart
@@ -18,7 +18,7 @@ class MapSearchService {
       /// fetch data
       String? _response = await NetworkHelper.fetchData(
           dotenv.get('MAP_BASE_ENDPOINT') + '?query=' + location + '&region=0');
-      if (_response != 'null') {
+      if (_response != null) {
         /// parse data
         final data = mapSearchModelFromJson(_response!);
         _results = data;
