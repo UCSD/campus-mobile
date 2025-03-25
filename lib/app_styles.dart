dedc17a904d89c5c56441308978020d42a8ff9a6
Wu, Alex <a6wu@ucsd.edu>
2025-03-24 21:14:48 -0700

    update styling for busyness manage locations view

--- lib/app_styles.dart
+++ lib/app_styles.dart
@@ -205,6 +205,8 @@ const TextStyle linkTextLight = TextStyle(
     decoration: TextDecoration.underline,
     color: linkColorLight);
 
+const Color toggleActiveColor = Color(0xFF109B00);
+
 // Theme agnostic styles
 const agnosticDisabled = Color(0xFF8A8A8A);
 
