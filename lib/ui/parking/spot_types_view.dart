e1ad5a5928fafffe1b5a956d6181823442a1c5cf
Wu, Alex <a6wu@ucsd.edu>
2025-03-26 13:03:55 -0700

    use custom AlertDialog and add check for max spots selected

--- lib/ui/parking/spot_types_view.dart
+++ lib/ui/parking/spot_types_view.dart
@@ -1,6 +1,9 @@
+import 'package:campus_mobile_experimental/app_constants.dart';
 import 'package:campus_mobile_experimental/core/models/spot_types.dart';
 import 'package:campus_mobile_experimental/core/providers/parking.dart';
+import 'package:campus_mobile_experimental/core/providers/user.dart';
 import 'package:campus_mobile_experimental/ui/common/HexColor.dart';
+import 'package:campus_mobile_experimental/ui/common/alert_dialog_widget.dart';
 import 'package:campus_mobile_experimental/ui/common/container_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,20 +18,25 @@ class SpotTypesView extends StatefulWidget {
 class _SpotTypesViewState extends State<SpotTypesView> {
   /// PROVIDERS
   late ParkingDataProvider spotTypesDataProvider;
+  late UserDataProvider _userDataProvider;
 
   @override
   Widget build(BuildContext context) {
     spotTypesDataProvider = Provider.of<ParkingDataProvider>(context);
+    _userDataProvider = Provider.of<UserDataProvider>(context);
     return ContainerView(child: createListWidget(context));
   }
 
-  Widget createListWidget(BuildContext context) => ListView(children: createList(context));
+  Widget createListWidget(BuildContext context) =>
+      ListView(children: createList(context));
 
   List<Widget> createList(BuildContext context) {
     var selectedSpots = 0;
     List<Widget> list = [];
     for (Spot data in spotTypesDataProvider.spotTypeModel!.spots!) {
-      if (Provider.of<ParkingDataProvider>(context).spotTypesState[data.spotKey]! == true) {
+      if (Provider.of<ParkingDataProvider>(context)
+              .spotTypesState[data.spotKey]! ==
+          true) {
         selectedSpots++;
       }
       var iconColor = HexColor(data.color);
@@ -54,9 +62,30 @@ class _SpotTypesViewState extends State<SpotTypesView> {
                       ))),
         title: Text(data.name),
         trailing: Switch(
-          value: Provider.of<ParkingDataProvider>(context).spotTypesState[data.spotKey]!,
-          onChanged: (_) {
-            spotTypesDataProvider.toggleSpotSelection(data.spotKey, selectedSpots);
+          value: Provider.of<ParkingDataProvider>(context)
+              .spotTypesState[data.spotKey]!,
+          onChanged: (bool isOn) {
+            if (isOn &&
+                spotTypesDataProvider.selectedSpots >
+                    ParkingDataProvider.MAX_SELECTED_SPOTS) {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialogWidget(
+                    type: MessageTypeConstants.ERROR,
+                    icon: Icons.block_flipped,
+                    title: ParkingConstants.spotMaxTitle,
+                    description: ParkingConstants.spotMaxDesc,
+                    onClose: () {
+                      Navigator.of(context).pop();
+                    },
+                  );
+                },
+              );
+            } else {
+              spotTypesDataProvider.toggleSpotSelection(
+                  data.spotKey, selectedSpots);
+            }
           },
           // activeColor: Theme.of(context).buttonColor,
           activeColor: Theme.of(context).colorScheme.background,
