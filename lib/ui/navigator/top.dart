d11a1a5e43190dd7319d0db5dc0280a6ec41aa76
Bryant, Charles <c3bryant@ucsd.edu>
2025-03-26 22:12:32 -0700

    Revert changes to top.dart

--- lib/ui/navigator/top.dart
+++ lib/ui/navigator/top.dart
@@ -1,121 +1,129 @@
+import 'package:campus_mobile_experimental/app_constants.dart';
+import 'package:campus_mobile_experimental/app_styles.dart';
+import 'package:campus_mobile_experimental/core/providers/bottom_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:campus_mobile_experimental/app_styles.dart';
-import 'package:campus_mobile_experimental/app_constants.dart';
-import 'package:campus_mobile_experimental/core/providers/bottom_nav.dart';
 
 class CMAppBar extends StatelessWidget {
+  CMAppBar({
+    this.title,
+    this.doneButton,
+    this.notificationsFilterButton,
+  });
+
   final String? title;
   final bool? doneButton;
   final bool? notificationsFilterButton;
-
-  CMAppBar({this.title, this.doneButton, this.notificationsFilterButton});
 
   @override
   Widget build(BuildContext context) {
     if (doneButton == true) {
       return PreferredSize(
-        preferredSize: Size.fromHeight(50),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios, color: lightTextColor),
-            onPressed: () => Navigator.pop(context),
-          ),
-          elevation: 0,
-          backgroundColor: ColorPrimary,
-          foregroundColor: lightTextColor,
-          centerTitle: true,
-          title: Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: title == null
-                ? Image.asset(
-              'assets/images/UCSanDiegoLogo-nav.png',
-              fit: BoxFit.contain,
-              height: 28,
-            )
-                : Text(title!, style: appBarTitleStyle),
-          ),
-          actions: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 8, right: 20),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  foregroundColor: darkButtonColor,
-                ),
-                child: Text('Done'),
-                onPressed: () {
-                  Provider.of<BottomNavigationBarProvider>(context, listen: false)
-                      .currentIndex = NavigatorConstants.HomeTab;
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                      RoutePaths.BottomNavigationBar, (Route<dynamic> route) => false);
-                  Provider.of<CustomAppBar>(context, listen: false)
-                      .changeTitle(CustomAppBar().appBar.title);
-                },
+          preferredSize: Size.fromHeight(50),
+          child: AppBar(
+              elevation: 0,
+              backgroundColor: ColorPrimary,
+              foregroundColor: lightTextColor,
+              primary: true,
+              centerTitle: true,
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: title == null
+                    ? Image.asset(
+                        'assets/images/UCSanDiegoLogo-nav.png',
+                        fit: BoxFit.contain,
+                        height: 28,
+                      )
+                    : Text(
+                        title!,
+                        style: appBarTitleStyle,
+                      ),
               ),
-            )
-          ],
-          systemOverlayStyle: SystemUiOverlayStyle.light,
-        ),
-      );
+              actions: <Widget>[
+                Padding(
+                    padding: EdgeInsets.only(bottom: 8, right: 20),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: darkButtonColor,
+                      ),
+                      child: Text(
+                        'Done',
+                      ),
+                      onPressed: () {
+                        // Set tab bar index to the Home tab
+                        Provider.of<BottomNavigationBarProvider>(context,
+                                listen: false)
+                            .currentIndex = NavigatorConstants.HomeTab;
+
+                        // Navigate to Home tab
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                            RoutePaths.BottomNavigationBar,
+                            (Route<dynamic> route) => false);
+
+                        // change the appBar title to the ucsd logo
+                        Provider.of<CustomAppBar>(context, listen: false)
+                            .changeTitle(CustomAppBar().appBar.title);
+                      },
+                    ))
+              ],
+              systemOverlayStyle: SystemUiOverlayStyle.light));
     } else if (notificationsFilterButton == true) {
       return PreferredSize(
-        preferredSize: Size.fromHeight(50),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios, color: lightTextColor),
-            onPressed: () => Navigator.pop(context),
-          ),
-          elevation: 0,
-          backgroundColor: ColorPrimary,
-          foregroundColor: lightTextColor,
-          centerTitle: true,
-          title: Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: title == null
-                ? Image.asset(
-              'assets/images/UCSanDiegoLogo-nav.png',
-              fit: BoxFit.contain,
-              height: 28,
-            )
-                : Text(title!, style: appBarTitleStyle),
-          ),
-          actions: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: IconButton(
-                icon: Icon(Icons.filter_list_outlined, color: lightTextColor),
-                onPressed: () => Navigator.pushNamed(context, RoutePaths.NotificationsFilter),
+          preferredSize: Size.fromHeight(50),
+          child: AppBar(
+              elevation: 0,
+              backgroundColor: ColorPrimary,
+              foregroundColor: lightTextColor,
+              primary: true,
+              centerTitle: true,
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: title == null
+                    ? Image.asset(
+                        'assets/images/UCSanDiegoLogo-nav.png',
+                        fit: BoxFit.contain,
+                        height: 28,
+                      )
+                    : Text(
+                        title!,
+                        style: appBarTitleStyle,
+                      ),
               ),
-            )
-          ],
-          systemOverlayStyle: SystemUiOverlayStyle.light,
-        ),
-      );
+              actions: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: IconButton(
+                    icon: Icon(Icons.filter_list_outlined),
+                    onPressed: () {
+                      Navigator.pushNamed(
+                          context, RoutePaths.NotificationsFilter);
+                    },
+                  ),
+                )
+              ],
+              systemOverlayStyle: SystemUiOverlayStyle.light));
     } else {
       return PreferredSize(
         preferredSize: Size.fromHeight(50),
         child: AppBar(
-          automaticallyImplyLeading: false,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios, color: lightTextColor),
-            onPressed: () => Navigator.pop(context),
-          ),
           elevation: 0,
           backgroundColor: ColorPrimary,
           foregroundColor: lightTextColor,
+          primary: true,
           centerTitle: true,
           title: Padding(
             padding: const EdgeInsets.only(bottom: 8.0),
             child: title == null
                 ? Image.asset(
-              'assets/images/UCSanDiegoLogo-nav.png',
-              fit: BoxFit.contain,
-              height: 28,
-            )
-                : Text(title!, style: appBarTitleStyle),
+                    'assets/images/UCSanDiegoLogo-nav.png',
+                    fit: BoxFit.contain,
+                    height: 28,
+                  )
+                : Text(
+                    title!,
+                    style: appBarTitleStyle,
+                  ),
           ),
           systemOverlayStyle: SystemUiOverlayStyle.light,
         ),
@@ -129,12 +137,13 @@ class CustomAppBar extends ChangeNotifier {
     makeAppBar();
   }
 
+  /// STATES
   String? title;
   bool? doneButton;
   bool? notificationsFilterButton;
   late CMAppBar appBar;
 
-  void makeAppBar() {
+  makeAppBar() {
     appBar = CMAppBar(
       title: title,
       doneButton: doneButton,
@@ -142,10 +151,17 @@ class CustomAppBar extends ChangeNotifier {
     );
   }
 
-  void changeTitle(String? newTitle, {bool done = false, bool notification = false}) {
+  changeTitle(String? newTitle, {done = false, notification = false}) {
     title = RouteTitles.titleMap[newTitle];
     doneButton = done;
     notificationsFilterButton = notification;
+
+    // if (newTitle == "Notifications") {
+    //   notificationsFilterButton = true;
+    // } else {
+    //   notificationsFilterButton = false;
+    // }
+
     makeAppBar();
   }
 }
