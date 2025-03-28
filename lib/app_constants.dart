f40978519860d20fc481f669ac9f30e530ea46f5
klortiz13 <kris.ortiz13alv@gmail.com>
2025-03-27 21:04:25 -0700

    added alertDialog to parking structure view

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -139,7 +139,8 @@ class ParkingConstants {
   static const spotMaxDesc =
       'The maximum number of parking spots allowed is three. Please remove some spots to add more.';
   static const lotMaxTitle = 'Maximum parking lots reached';
-  static const lotMaxDesc = 'You have reached the maximum number of lots (10) that can be selected. Please deselect some lots before adding more.';
+  static const lotMaxDesc =
+      'You have reached the maximum number of lots (10) that can be selected. Please deselect some lots before adding more.';
 }
 
 class Plugins {
