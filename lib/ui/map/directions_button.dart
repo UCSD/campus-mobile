3915181103aaba6fd16878921a384e8b447a1ca5
Kriz Ortiz <kortizalvarado@ucsd.edu>
2025-05-10 15:34:03 -0700

    removed spaces

--- lib/ui/map/directions_button.dart
+++ lib/ui/map/directions_button.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
-
 import '../../app_styles.dart';
 
 class DirectionsButton extends StatelessWidget {
--- lib/ui/map/my_location_button.dart
+++ lib/ui/map/my_location_button.dart
@@ -2,7 +2,6 @@ import 'package:campus_mobile_experimental/core/providers/map.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
-
 import '../../app_styles.dart';
 
 class MyLocationButton extends StatelessWidget {
