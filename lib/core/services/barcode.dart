2759d7a0d62077ad3330e961abaa9bfee12c012b
Kriz Ortiz <kortizalvarado@ucsd.edu>
2025-08-25 20:54:02 -0700

    Added 401 catch block to barcode.dart

--- lib/core/services/barcode.dart
+++ lib/core/services/barcode.dart
@@ -22,6 +22,9 @@ class BarcodeService {
     } catch (e) {
       /// if the authorized fetch failed we know we have to refresh the
       /// token for this service
+      if (e.toString().contains("401")) {
+        if (await NetworkHelper.getNewToken(headers)) return await uploadResults(headers, body);
+      }
       _error = e.toString();
       return false;
     } finally {
