fa2806bb8cb331a841ffde29b3f8aa65fa9e25df
Kriz Ortiz <kortizalvarado@ucsd.edu>
2025-08-25 21:03:32 -0700

    Added 401 catch block to the employee_id.dart

--- lib/core/services/employee_id.dart
+++ lib/core/services/employee_id.dart
@@ -22,6 +22,12 @@ class EmployeeIdService {
       _employeeIdModel = employeeIdModelFromJson(_response);
       return true;
     } catch (e) {
+      /// if the authorized fetch failed we know we have to refresh the
+      /// token for this service
+      if (e.toString().contains("401")) {
+        if (await NetworkHelper.getNewToken(headers))
+          return await fetchEmployeeIdProfile(headers);
+      }
       _error = e.toString();
       return false;
     } finally {
