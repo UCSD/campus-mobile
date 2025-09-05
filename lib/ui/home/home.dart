da7a1597e01134857edbd7bf6a050556399d7b31
Kriz Ortiz <kortizalvarado@ucsd.edu>
2025-09-05 10:02:40 -0700

    Removed changeTitle call from home.dart

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -165,7 +165,6 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     initUniLinks();
     _connectivityProvider = Provider.of<InternetConnectivityProvider>(context);
-    Provider.of<CustomAppBar>(context).changeTitle(null);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: cardMargin, vertical: 0.0),
       child: ListView(
