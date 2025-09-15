7c3a30d6ff2b3d3b9c419b6497314b58f09a6288
Bryant, Charles <c3bryant@ucsd.edu>
2025-09-15 16:14:54 -0700

    Fix dining card exception caused by dining filter

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -82,7 +82,7 @@ class DiningConstants {
     "Triton Cash",
     "Dining Dollars",
     "Apple/Google Pay",
-    "MasterCard, Visa",
+    "MasterCard/Visa",
     "American Express",
     "Cash",
     "Other",
