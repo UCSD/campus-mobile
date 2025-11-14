b7477e4e33aec4879048045bdf11ccc5913cf7db
klortiz13 <kortizalvarado@ucsd.edu>
2025-11-13 19:25:34 -0800

    Triggering first work to ensure second runs as well

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'package:campus_mobile_experimental/app_constants.dart';
 import 'package:campus_mobile_experimental/app_provider.dart';
-import 'package:campus_mobile_experimental/app_router.dart'
-    as campusMobileRouter;
+import 'package:campus_mobile_experimental/app_router.dart' as campusMobileRouter;
 import 'package:campus_mobile_experimental/app_styles.dart';
 import 'package:campus_mobile_experimental/core/models/authentication.dart';
 import 'package:campus_mobile_experimental/core/models/user_profile.dart';
@@ -32,8 +31,7 @@ void main() async {
     final mapsImplementation = GoogleMapsFlutterPlatform.instance;
     if (mapsImplementation is GoogleMapsFlutterAndroid) {
       WidgetsFlutterBinding.ensureInitialized();
-      await mapsImplementation
-          .initializeWithRenderer(AndroidMapRenderer.latest);
+      await mapsImplementation.initializeWithRenderer(AndroidMapRenderer.latest);
     }
 
     // dotenv loading
@@ -59,7 +57,7 @@ Future<void> initializeHive() async {
 Future<void> initializeApp() async {
   final prefs = await SharedPreferences.getInstance();
 
-  // TODO: fix this. We don't need two different persistent flags... - November 2025
+  // TODO: fix this. We don't need two different persistent flags...
   if (prefs.getBool('first_run') ?? true) {
     await clearSecuredStorage();
     await clearHiveStorage();
@@ -76,12 +74,11 @@ Future<void> clearSecuredStorage() async {
   await storage.deleteAll();
 }
 
-// TODO: refactor this to load multiple futures in one statement - November 2025
+// TODO: refactor this to load multiple futures in one statement
 Future<void> clearHiveStorage() async {
   await (await Hive.openBox(DataPersistence.cardStates)).deleteFromDisk();
   await (await Hive.openBox(DataPersistence.cardOrder)).deleteFromDisk();
-  await (await Hive.openBox(DataPersistence.AuthenticationModel))
-      .deleteFromDisk();
+  await (await Hive.openBox(DataPersistence.AuthenticationModel)).deleteFromDisk();
   await (await Hive.openBox(DataPersistence.UserProfileModel)).deleteFromDisk();
 }
 
@@ -149,17 +146,13 @@ class CampusMobile extends StatelessWidget {
         debugShowCheckedModeBanner: true,
         title: 'UC San Diego',
         theme: lightTheme.copyWith(
-          colorScheme:
-              lightTheme.colorScheme.copyWith(secondary: darkAccentColor),
+          colorScheme: lightTheme.colorScheme.copyWith(secondary: darkAccentColor),
         ),
         darkTheme: darkTheme.copyWith(
-          colorScheme:
-              darkTheme.colorScheme.copyWith(secondary: lightAccentColor),
+          colorScheme: darkTheme.colorScheme.copyWith(secondary: lightAccentColor),
         ),
         themeMode: ThemeMode.system,
-        initialRoute: showOnboardingScreen
-            ? RoutePaths.OnboardingLogin
-            : RoutePaths.BottomNavigationBar,
+        initialRoute: showOnboardingScreen ? RoutePaths.OnboardingLogin : RoutePaths.BottomNavigationBar,
         onGenerateRoute: campusMobileRouter.Router.generateRoute,
         navigatorObservers: [observer],
         builder: (context, child) {
