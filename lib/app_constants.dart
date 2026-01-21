0025312e3f8204629d6531e3572e56395283e631
angelawu236 <anw041@ucsd.edu>
2026-01-21 11:16:23 -0800

    added first ave, south, scholars as default parking lots

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -68,12 +68,7 @@ class RouteTitles {
 }
 
 class ParkingDefaults {
-  static const DEFAULT_LOTS = [
-    "Athena",
-    "Gilman",
-    "Hopkins",
-    "Theatre District",
-  ];
+  static const DEFAULT_LOTS = ["Athena", "Gilman", "Hopkins", "Theatre District", "South", "Scholars", "First Ave"];
   static const DEFAULT_SPOTS = ["S", "B", "A"];
 }
 
