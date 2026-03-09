4ff51defb005a92e8a9397e2e8784eee14beef5f
aleyu0 <83614302+aleyu0@users.noreply.github.com>
2026-03-09 11:36:18 -0700

    Handle null selectedOccuspaceLocations and preserve location view state across availability refresh

--- lib/core/providers/availability.dart
+++ lib/core/providers/availability.dart
@@ -28,6 +28,8 @@ class AvailabilityDataProvider extends ChangeNotifier {
     Map<String, AvailabilityModel> newMapOfLots = {};
 
     if (await _availabilityService.fetchData()) {
+      Map<String?, bool> newLocationViewState = {};
+
       /// setting the LocationViewState based on user data
       for (AvailabilityModel model in _availabilityService.data) {
         String curName = model.name;
@@ -37,15 +39,20 @@ class AvailabilityDataProvider extends ChangeNotifier {
 
         /// if the user is logged out and has not put any preferences,
         /// show all locations by default
-        final bool hasNoSelectedLocations = userDataProvider.userProfileModel.selectedOccuspaceLocations!.isEmpty;
+        final selectedLocations = userDataProvider.userProfileModel.selectedOccuspaceLocations ?? [];
+        final bool hasNoSelectedLocations = selectedLocations.isEmpty;
+        
         if (hasNoSelectedLocations)
-          _locationViewState[curName] = true;
-
-        /// otherwise, LocationViewState should be true for all selectedOccuspaceLocations
+          newLocationViewState[curName] = _locationViewState[curName] ?? true;
         else {
-          _locationViewState[curName] = userDataProvider.userProfileModel.selectedOccuspaceLocations!.contains(curName);
+          newLocationViewState[curName] = _locationViewState[curName] ??
+              userDataProvider.userProfileModel.selectedOccuspaceLocations!.contains(curName);
         }
       }
+
+      // After the loop, assign it:
+      _locationViewState = newLocationViewState;
+    
 
       /// replace old list of lots with new one
       _availabilityModels = newMapOfLots;
