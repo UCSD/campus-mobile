b91f3d74c392dd80239993c92adb947f0fa37ac9
angelawu236 <anw041@ucsd.edu>
2026-04-14 11:14:11 -0700

    B.2

--- lib/ui/news/news_list.dart
+++ lib/ui/news/news_list.dart
@@ -74,14 +74,16 @@ class NewsList extends StatelessWidget {
   }
 
   Widget buildNewsTile(Item newsItem, BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(
-          context,
-          RoutePaths.NEWS_DETAIL_VIEW,
-          arguments: newsItem,
-        );
-      },
+    return Semantics(
+      link: true,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(
+            context,
+            RoutePaths.NEWS_DETAIL_VIEW,
+            arguments: newsItem,
+          );
+        },
       child: Container(
         padding: EdgeInsets.all(8.0),
         child: IntrinsicHeight(
@@ -143,6 +145,7 @@ class NewsList extends StatelessWidget {
             ],
           ),
         ),
+      ),
       ),
     );
   }
