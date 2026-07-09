766412b3922052a2bc86a2093737763b5aeaf759
klortiz13 <kortizalvarado@ucsd.edu>
2026-07-09 14:04:41 -0700

    fixed existing bug (dates were not displayed correctly in details page)

--- lib/ui/events/events_detail_view.dart
+++ lib/ui/events/events_detail_view.dart
@@ -7,6 +7,7 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:campus_mobile_experimental/app_styles.dart';
+import 'package:campus_mobile_experimental/ui/events/event_tile.dart';
 
 class EventDetailView extends StatelessWidget {
   const EventDetailView({Key? key, required this.data}) : super(key: key);
@@ -32,12 +33,16 @@ class EventDetailView extends StatelessWidget {
           child: Row(
             children: [
               // Event Date
-              EventDateContainer(
-                date: DateFormat("MMM d y").format(
-                  isMultiDay(data.startDate, data.endDate)
-                      ? getDisplayDate(data.startDate, data.endDate)
-                      : data.startDate.toLocal(),
-                ),
+              Builder(
+                builder: (context) {
+                  final df = DateFormat("MMM d y");
+                  final localStart = data.startDate.toLocal();
+                  final localEnd = data.endDate.toLocal();
+                  final startDate = df.format(localStart);
+                  final endDate = df.format(localEnd);
+                  final dateDisplay = startDate == endDate ? startDate : '$startDate - $endDate';
+                  return StartEndDateContainer(date: dateDisplay);
+                }
               ),
 
               // Event Title
@@ -120,25 +125,6 @@ class EventDetailView extends StatelessWidget {
     );
   }
 
-  DateTime getDisplayDate(DateTime start, DateTime end) {
-    final now = DateTime.now();
-    final localStart = start.toLocal();
-    final localEnd = end.toLocal();
-
-    // If today is within the event window
-    final bool isNotBeforeStart = !now.isBefore(localStart);
-    final bool isNotAfterEnd = !now.isAfter(localEnd);
-    if (isNotBeforeStart && isNotAfterEnd) return now;
-
-    // Otherwise, show the earlier of the two future dates
-    return localStart.isBefore(localEnd) ? localStart : localEnd;
-  }
-
-  bool isMultiDay(DateTime start, DateTime end) {
-    final s = start.toLocal();
-    final e = end.toLocal();
-    return s.year != e.year || s.month != e.month || s.day != e.day;
-  }
 }
 
 // CREATE EVENT IMAGE
@@ -177,66 +163,6 @@ class EventImage extends StatelessWidget {
   }
 }
 
-// CREATE START DATE CONTAINER
-class EventDateContainer extends StatelessWidget {
-  final String date;
-  const EventDateContainer({Key? key, required this.date}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    String semanticDate = date;
-    final Map<String, String> months = {
-      'Jan': 'January',
-      'Feb': 'February',
-      'Mar': 'March',
-      'Apr': 'April',
-      'May': 'May',
-      'Jun': 'June',
-      'Jul': 'July',
-      'Aug': 'August',
-      'Sep': 'September',
-      'Oct': 'October',
-      'Nov': 'November',
-      'Dec': 'December'
-    };
-    months.forEach((key, value) {
-      semanticDate = semanticDate.replaceAll(key, value);
-    });
-
-    return Semantics(
-      container: true,
-      label: 'When: $semanticDate',
-      child: ExcludeSemantics(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Month
-            Text(date.split(' ')[0].toUpperCase(),
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Theme.of(context).brightness == Brightness.light ? lightPrimaryColor : Colors.white,
-                  fontWeight: FontWeight.w400,
-                )),
-            // Day
-            Text(date.split(' ')[1].toUpperCase(),
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Theme.of(context).brightness == Brightness.light ? lightPrimaryColor : Colors.white,
-                  fontWeight: FontWeight.w500,
-                )),
-            // Year
-            Text(date.split(' ')[2].toUpperCase(),
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Theme.of(context).brightness == Brightness.light ? lightPrimaryColor : Colors.white,
-                  fontWeight: FontWeight.w400,
-                )),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 // CREATE EVENT TITLE
 class EventTitle extends StatelessWidget {
