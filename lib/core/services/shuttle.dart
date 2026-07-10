3899524b76b8a3f72655ab09768f5f49280263ec
klortiz13 <kortizalvarado@ucsd.edu>
2026-07-10 12:59:06 -0700

    a

--- lib/core/services/shuttle.dart
+++ lib/core/services/shuttle.dart
@@ -57,6 +57,11 @@ class ShuttleService {
 
       /// parse data
       final arrivingData = getArrivingShuttles(_response);
+      // print arriving data to terminal in pink (one line per arrival with reset)
+      // for (var arrival in arrivingData) {
+      //   print(
+      //       '\x1B[35mArrivingShuttle(stopId: ${arrival.routeStopId}, routeId: ${arrival.routeId}, routeName: ${arrival.routeName}, routeColor: ${arrival.routeColor}, minutesToArrival: ${arrival.secondsToArrival / 60})\x1B[0m');
+      // }
       return arrivingData;
     } catch (e) {
       /// if the authorized fetch failed we know we have to refresh the
